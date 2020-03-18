693f68eb528dde84ac7510c3f2646e491ef08e58
David Strauss <david@davidstrauss.net>
2020-03-17 17:08:17 -0700

    Snapshot (for now)

--- run.py
+++ run.py
@@ -35,9 +35,23 @@ def get_snapshot(date, province_state, country_region):
     full_snapshot = pd.read_csv(snapshot_filename)
     filtered_snapshot = full_snapshot[(full_snapshot["Province/State"] == province_state) & (full_snapshot["Country/Region"] == country_region)]
     pprint.pprint(filtered_snapshot)
-    return filtered_snapshot
 
-def forecast_region(province_state, country_region):
+    confirmed = 0
+    deaths = 0
+    recovered = 0
+
+    try:
+        row = filtered_snapshot.iloc[0]
+        confirmed = row.at['Confirmed']
+        deaths = row.at['Deaths']
+        recovered = row.at['Recovered']
+    except IndexError as e:
+        pass
+
+    return {'confirmed': confirmed, 'deaths': deaths, 'recovered': recovered}
+
+
+def forecast_region(province_state, country_region, iterations):
     logging.info('Building results for {} in {}'.format(province_state, country_region))
     pop = get_population(province_state, country_region)
     beds = get_beds(province_state, country_region)
@@ -49,7 +63,8 @@ def forecast_region(province_state, country_region):
 
     snapshots = []
 
-    snapshot_date = datetime.date.today() - datetime.timedelta(days=1)
+    today = datetime.date.today() - datetime.timedelta(days=1)
+    snapshot_date = today
 
     snapshots.append(get_snapshot(snapshot_date, province_state, country_region))
 
@@ -59,5 +74,30 @@ def forecast_region(province_state, country_region):
 
     pprint.pprint(snapshots)
 
+    cols = ['Note',
+            'Date',
+            'Effective R0',
+            'Beginning Susceptible',
+            'New Infected',
+            'Current Infected',
+            'Recovered or Died',
+            'Ending Susceptible (Right Axis)',
+            'Actual Reported (Hospitalized?) (Current Trends)',
+            'Predicted Hospitalized (Current Trends)',
+            'Cumulative Infected (Right Axis)',
+            'Cumulative Deaths'
+            'Available Hospital Beds'
+            'Estimated Actual Chance of Infection'
+            'Predicted Chance of Infection'
+            'Cumulative Predicted Chance of Infection (Right Axis)'
+            'R0',
+            '% Susceptible']
 
-forecast_region('California', 'US')
+    forecast = pd.DataFrame(columns=cols)
+
+    for i in range(0, iterations):
+        snapshot_date += datetime.timedelta(days=model_interval)
+
+
+
+forecast_region('California', 'US', 50)
