59c7fbe8a83cf0d29950cf456cccf962e279feed
Chris Kelly <ghop02@gmail.com>
2020-03-23 22:27:23 -0400

    Moving data out of python package

--- tests/covid_data_model_test.py
+++ tests/covid_data_model_test.py
@@ -3,8 +3,8 @@ from covid_data_model import run
 
 
 def test_run_simulation():
-    beds = pd.read_csv("covid_data_model/data/beds.csv")
-    populations = pd.read_csv("covid_data_model/data/populations.csv")
-    full_timeseries = pd.read_csv("covid_data_model/data/timeseries.csv")
+    beds = pd.read_csv("data/beds.csv")
+    populations = pd.read_csv("data/populations.csv")
+    full_timeseries = pd.read_csv("data/timeseries.csv")
     country = "USA"
     run.main(beds, populations, full_timeseries, country)
