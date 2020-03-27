40f9fbe3ac92652a2400336f283a3dd6c1adb0be
Greg Soltis <gsoltis@gmail.com>
2020-03-27 15:44:30 -0700

    Add a sanity check for runs of run.py (#38)
    * Add a sanity check for validating runs of run.py
    
    * Update to run subprocess properly and not trigger a model import
    
    * quality build_params constants
    
    * Fix import

---
+++ libs/build_params.py
@@ -0,0 +1,120 @@
+'''
+Constants to use for a build. In a separate file to avoid
+auto-importing a dataset when we don't necessarily need to.
+'''
+
+import datetime
+
+r0 = 2.4
+
+INTERVENTIONS = [
+    None,  # No Intervention
+    {  # Flatten the Curve
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 4, 20): 1.1,
+        datetime.date(2020, 5, 22): 0.8,
+        datetime.date(2020, 6, 23): r0
+    },
+    {  # Full Containment
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 3, 31): 0.3,
+        datetime.date(2020, 4, 28): 0.2,
+        datetime.date(2020, 5,  6): 0.1,
+        datetime.date(2020, 5, 10): 0.35,
+        datetime.date(2020, 5, 18): r0
+    },
+    {  # @TODO: Model w/ FlatteningTheCurve (2 wk delay)
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 4, 20): 1.1,
+        datetime.date(2020, 5, 22): 0.8,
+        datetime.date(2020, 6, 23): r0
+    },
+    {  # @TODO: Model w/ FlatteningTheCurve (1 mo delay)
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 4, 20): 1.1,
+        datetime.date(2020, 5, 22): 0.8,
+        datetime.date(2020, 6, 23): r0
+    },
+    {  # @TODO: Full Containment (1 wk dly)
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 3, 31): 0.3,
+        datetime.date(2020, 4, 28): 0.2,
+        datetime.date(2020, 5,  6): 0.1,
+        datetime.date(2020, 5, 10): 0.35,
+        datetime.date(2020, 5, 18): r0
+    },
+    {  # @TODO: Full Containment (2 wk dly)
+        datetime.date(2020, 3, 23): 1.3,
+        datetime.date(2020, 3, 31): 0.3,
+        datetime.date(2020, 4, 28): 0.2,
+        datetime.date(2020, 5,  6): 0.1,
+        datetime.date(2020, 5, 10): 0.35,
+        datetime.date(2020, 5, 18): r0
+    },
+    {  # Social Distancing
+        datetime.date(2020, 3, 23): 1.7,
+        datetime.date(2020, 6, 23): r0
+    },
+]
+
+OUTPUT_DIR = 'results/test'
+
+# Dict to transform longhand state names to abbreviations
+US_STATE_ABBREV =  {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands': 'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
