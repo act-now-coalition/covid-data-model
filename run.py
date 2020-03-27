612d44a88388d35732e34948f1398665aa250ffe
Chris Kelly <ghop02@gmail.com>
2020-03-27 18:23:24 -0400

    Revert run.py changes

--- run.py
+++ run.py
@@ -1,38 +1,22 @@
-#!/usr/bin/env python
-"""
-Run Covid data model.
-
-Execution:
-    ./run.py run-model [JHU|CDS] [--output-dir]
-
-"""
 import logging
-import pprint
+logging.basicConfig(level=logging.INFO)
 
 import datetime
 import time
 import simplejson
-import click
-import copy
-import os.path
-
 from libs.CovidTimeseriesModel import CovidTimeseriesModel
 from libs.CovidDatasets import CDSDataset
-from libs.CovidDatasets import JHUDataset
 
-_logger = logging.getLogger(__name__)
-
-
-def record_results(result, directory, name, num, pop):
-    vals = copy.copy(result)
+def record_results(res, directory, name, num, pop):
+    import copy
+    import os.path
+    vals = copy.copy(res)
     # Format the date in the manner the front-end expects
-    vals['Date'] = result['Date'].apply(lambda d: "{}/{}/{}".format(d.month, d.day, d.year))
+    vals['Date'] = res['Date'].apply(lambda d: "{}/{}/{}".format(d.month, d.day, d.year))
     # Set the population
     vals['Population'] = pop
     # Write the results to the specified directory
-    path = os.path.join(directory, name.upper() + '.' + str(num) + '.json').format(name)
-    _logger.debug(f'Writing path: {path}')
-    with open(path, 'w') as out:
+    with open( os.path.join(directory, name.upper() + '.' + str(num) + '.json').format(name), 'w') as out:
         simplejson.dump(vals[[
                 'Date',
                 'R',
@@ -55,19 +39,21 @@ def record_results(result, directory, name, num, pop):
                 '% Susceptible'
             ]].values.tolist(), out, ignore_nan=True)
 
-def model_state(dataset, country, state, interventions=None):
+def model_state(country, state, interventions=None):
+    ## Constants
+    start_time = time.time()
     HOSPITALIZATION_RATE = .0727
     HOSPITALIZED_CASES_REQUIRING_ICU_CARE = .1397
     TOTAL_INFECTED_PERIOD = 12
     MODEL_INTERVAL = 4
     r0 = 2.4
-    POP = dataset.get_population_by_country_state(country, state)
-
+    Dataset = CDSDataset(filter_past_date=datetime.date(2020, 3, 19))
+    POP = Dataset.get_population_by_country_state(country, state)
     # Pack all of the assumptions and parameters into a dict that can be passed into the model
     MODEL_PARAMETERS = {
         # Pack the changeable model parameters
-        'timeseries': dataset.get_timeseries_by_country_state(country, state, MODEL_INTERVAL),
-        'beds': dataset.get_beds_by_country_state(country, state),
+        'timeseries': Dataset.get_timeseries_by_country_state(country, state, MODEL_INTERVAL),
+        'beds': Dataset.get_beds_by_country_state(country, state),
         'population': POP,
         'projection_iterations': 24, # Number of iterations into the future to project
         'r0': r0,
@@ -81,7 +67,7 @@ def model_state(dataset, country, state, interventions=None):
         'hospital_capacity_change_daily_rate': 1.05,
         'max_hospital_capacity_factor': 2.07,
         'initial_hospital_bed_utilization': .6,
-        'model_interval': MODEL_INTERVAL, # In days
+        'model_interval': 4, # In days
         'total_infected_period': 12, # In days
         'rolling_intervals_for_current_infected': int(round(TOTAL_INFECTED_PERIOD / MODEL_INTERVAL, 0)),
     }
@@ -139,42 +125,14 @@ INTERVENTIONS = [
     },
 ]
 
-
-@click.group()
-def main():
-    pass
-
-
-@main.command()
-@click.argument('dataset_name', type=click.Choice(['JHU', 'CDS']), default="CDS")
-@click.option(
-    '--output-dir', default='results/test', help="Model results output directory"
-)
-def run_model(dataset_name, output_dir):
-    # filter_past_date = datetime.date(2020, 3, 19)
-    filter_past_date = None
-    if dataset_name == 'JHU':
-        dataset = JHUDataset(filter_past_date=filter_past_date)
-    elif dataset_name == 'CDS':
-        dataset = CDSDataset(filter_past_date=filter_past_date)
-
-    _logger.info(
-        f'Running model on {dataset.__class__.__name__}. Saving output to {output_dir}'
-    )
-    for state in dataset.get_all_states_by_country('USA'):
-        for i in range(0, len(INTERVENTIONS)):
-            _logger.info(f"Running intervention {i} for {state}")
-            intervention = INTERVENTIONS[i]
-            model_output = model_state(dataset, 'USA', state, intervention)
-            record_results(
-                model_output,
-                output_dir,
-                state,
-                i,
-                dataset.get_population_by_country_state('USA', state)
-            )
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    main()
+Dataset = CDSDataset()
+for state in Dataset.get_all_states_by_country('USA'):
+    for i in range(0, len(INTERVENTIONS)):
+        intervention = INTERVENTIONS[i]
+        record_results(
+            model_state('USA', state, intervention),
+            'results/test',
+            state,
+            i,
+            Dataset.get_population_by_country_state('USA', state)
+        )
