ff1053117c517fced8b6e1f5604946a8221260a3
Chris Kelly <ghop02@gmail.com>
2020-03-27 19:35:38 -0400

    Adding test that runs model on one state

--- test/placeholder_test.py
+++
@@ -1,7 +0,0 @@
-
-# This is just a placeholder test file. Once there are real tests, this should
-# be deleted.
-
-
-def test_placeholder():
-    pass
---
+++ test/run_cli_test.py
@@ -0,0 +1,28 @@
+import pathlib
+import pytest
+
+from click.testing import CliRunner
+
+import run
+from libs import build_params
+
+
+@pytest.mark.parametrize('dataset_name', ['JHU', 'CDS'])
+def test_runs_all_interventions_on_one_state(tmpdir, dataset_name):
+    runner = CliRunner()
+
+    runner.invoke(run.main, [
+        'run-model', dataset_name, '--state', 'MA', '--output-dir', tmpdir
+    ])
+
+    output_path = pathlib.Path(tmpdir)
+    output_paths = list(output_path.iterdir())
+
+    assert len(output_paths) == len(build_params.INTERVENTIONS)
+    for path in output_paths:
+        assert path.read_bytes()
+
+
+
+# TODO(chris): Would like to add a test that runs all the states on one intervention.
+# I don't think that we should run the entire generation in the unittests.
