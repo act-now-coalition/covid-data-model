f6059866367059ab79d646f7a0e8ec63980644bf
Greg Soltis <gsoltis@gmail.com>
2020-03-26 17:59:56 -0700

    Update dod.py to also generate states.csv (#29)
    * Start work on states.csv
    
    * generate states.csv
    
    * Include lat/lng
    
    * Include 'Point' hack
    
    * Filter to only the 50 states

--- dod.py
+++ dod.py
@@ -1,14 +1,34 @@
 import pandas as pd
 import numpy as np
+import requests
 import datetime
 import pprint
+from us_state_abbrev import us_state_abbrev
 
 # @TODO: Attempt today. If that fails, attempt yesterday.
 latest = datetime.date.today() - datetime.timedelta(days=1)
 
+interventions_url = 'https://raw.githubusercontent.com/covid-projections/covid-projections/master/src/assets/data/interventions.json'
+interventions = requests.get(interventions_url).json()
+interventions_df = pd.DataFrame(
+    list(interventions.items()),
+    columns=['state', 'intervention']
+)
+interventions_df.info()
+
+abbrev_df = pd.DataFrame(
+    list(us_state_abbrev.items()),
+    columns=['state', 'abbreviation']
+)
+abbrev_df.info()
+
+projections_df = pd.read_csv('projections_03-26-2020.csv')
+projections_df.info()
+
 url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(
     latest.strftime("%m-%d-%Y"))
 raw_df = pd.read_csv(url)
+
 raw_df.info()
 
 column_mapping = {"Province_State": "Province/State",
@@ -55,3 +75,42 @@ us_only = final_df[(final_df["Country/Region"] == "US")]
 pprint.pprint(us_only.head())
 
 us_only.to_csv("results/counties.csv")
+
+states_group = us_only.groupby(['Province/State'])
+states_agg = states_group.aggregate({
+    'Last Update': 'max',
+    'Confirmed': 'sum',
+    'Recovered': 'sum',
+    'Deaths': 'sum',
+    'Active': 'sum',
+    'Country/Region': 'first',
+    'Latitude': 'first',
+    'Longitude': 'first'
+    # People tested is currently null
+    #'People Tested': 'sum'
+})
+states_agg.info()
+states_abbrev = states_agg.merge(
+    abbrev_df, left_index=True, right_on='state', how='left'
+).merge(
+    # inner merge to filter to only the 50 states
+    interventions_df, left_on='abbreviation', right_on='state', how='inner'
+).merge(
+    projections_df, left_on='state_y', right_on='State', how='left'
+).drop(['abbreviation', 'state_y', 'State'], axis=1)
+state_col_remap = {
+    'state_x': 'Province/State',
+    'Projection1': '4-day Hospitalizations Prediction',
+    'Projection2': '8-day Hospitalizations Prediction',
+    'intervention': 'Intervention'
+}
+states_remapped = states_abbrev.rename(columns=state_col_remap)
+states_remapped.info()
+# TODO: filter out county-specific columns
+state_cols = cols + ['Intervention', '4-day Hospitalizations Prediction', '8-day Hospitalizations Prediction']
+states_final = pd.DataFrame(states_remapped, columns=state_cols)
+states_final['Shape'] = 'Point'
+states_final.info()
+states_final = states_final.fillna("<Null>")
+pprint.pprint(states_final.head())
+states_final.to_csv('results/states.csv')
