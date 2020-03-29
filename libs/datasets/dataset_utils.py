a8f6b7e665269e8ae772a7676f3517f9f5f7934c
Chris Kelly <ghop02@gmail.com>
2020-03-29 16:53:40 -0400

    Adding helpers around comparing two datasets

--- libs/datasets/dataset_utils.py
+++ libs/datasets/dataset_utils.py
@@ -71,3 +71,35 @@ def build_aggregate_county_data_frame(jhu_data_source, cds_data_source):
         cds_usa_data[cds_usa_data.date < '2020-03-22'],
         jhu_usa_data[jhu_usa_data.date >= '2020-03-22']
     ])
+
+
+def check_index_values_are_unique(data):
+    duplicates_results = data.index.duplicated()
+    duplicates = duplicates_results[duplicates_results == True]
+    if len(duplicates):
+        _logger.warning(f"Found {len(duplicates)} results.")
+
+
+
+def compare_datasets(base, other, group, first_name='first', other_name='second', values='cases'):
+    other = other.groupby(group).sum().reset_index().set_index(group)
+    base = base.groupby(group).sum().reset_index().set_index(group)
+    base['info'] = first_name
+    other['info'] = other_name
+    common = pd.concat([base, other])
+    all_combined = common.pivot_table(
+        index=['date', 'state'],
+        columns='info',
+        values=values
+    ).rename_axis(None, axis=1)
+    first_null = all_combined[first_name].isnull()
+    first_notnull = all_combined[first_name].notnull()
+    other_null = all_combined[other_name].isnull()
+    other_notnull = all_combined[other_name].notnull()
+
+    contains_both = all_combined[first_notnull & other_notnull]
+    matching = contains_both[contains_both[first_name] == contains_both[other_name]]
+    not_matching = contains_both[contains_both[first_name] != contains_both[other_name]]
+    not_matching['delta'] = contains_both[first_name] - contains_both[other_name]
+    not_matching['delta_ratio'] = (contains_both[first_name] - contains_both[other_name]) / contains_both[first_name]
+    return all_combined, matching, not_matching
