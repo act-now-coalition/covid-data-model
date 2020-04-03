bc5251c84489afc1b033dbdb7f6764930e45ca79
Chris Kelly <ghop02@gmail.com>
2020-04-03 02:47:54 -0400

    Merging nyc boroughs (#90)
    * fix parsing of county in census population data
    
    * Combining NYC counties
    
    * code formatting
    
    * Add a test and caught a bug!

--- test/data_validation_test.py
+++ test/data_validation_test.py
@@ -1,10 +1,12 @@
+import logging
+import pytest
+import pandas as pd
 from libs.datasets import JHUDataset
 from libs.datasets import CDSDataset
 from libs.datasets import dataset_utils
+from libs.datasets import custom_aggregations
 from libs.datasets.dataset_utils import AggregationLevel
 from libs import CovidDatasets
-import pytest
-import logging
 
 _logger = logging.getLogger(__name__)
 
@@ -75,5 +77,55 @@ def test_missing_state_in_generic_dataset(legacy_cls, new_cls, test_matching, te
         assert False
 
 
-def test_aggregation():
-    pass
+def default_timeseries_row(**updates):
+    data = {
+        'fips': '22083',
+        'aggregate_level': 'county',
+        'date': '2020-03-26 00:00:00',
+        'country': 'USA',
+        'state': 'LA',
+        'cases': 10.0,
+        'deaths': 1.0,
+        'recovered': 0.0,
+        'source': 'JHU',
+        'generated': False,
+        'county': 'Richland Parish'
+    }
+    data.update(updates)
+    return data
+
+
+@pytest.mark.parametrize("are_boroughs_zero", [True, False])
+def test_nyc_aggregation(are_boroughs_zero):
+
+    nyc_county_fips = custom_aggregations.NEW_YORK_COUNTY_FIPS
+    nyc_borough_fips = custom_aggregations.NYC_BOROUGH_FIPS[0]
+
+    nyc_cases = 10
+    borough_cases = 0 if are_boroughs_zero else 10
+    rows = [
+        default_timeseries_row(fips=nyc_county_fips, cases=nyc_cases),
+        default_timeseries_row(
+            fips=nyc_borough_fips, cases=borough_cases, deaths=borough_cases, recovered=borough_cases
+        ),
+        default_timeseries_row()
+    ]
+
+    df = pd.DataFrame(rows)
+
+    # Todo: figure out a better way to define these groups.
+    group = [
+        'date', 'source', 'country', 'aggregate_level', 'state', 'generated'
+    ]
+    result = custom_aggregations.update_with_combined_new_york_counties(
+        df, group, are_boroughs_zero=are_boroughs_zero
+    )
+    results = result.sort_values('fips').to_dict(orient='records')
+
+    assert len(results) == 2
+    nyc_result = results[1]
+
+    if are_boroughs_zero:
+        assert nyc_result['cases'] == nyc_cases
+    else:
+        assert nyc_result['cases'] == nyc_cases + borough_cases
