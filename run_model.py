6ceb67abcfea6c3cc11374a66799dfe7acfbb501
Greg Soltis <gsoltis@gmail.com>
2020-04-03 20:14:17 -0700

    Add git hash support to run_model.py, output a version file when the model is run

--- run_model.py
+++ run_model.py
@@ -4,8 +4,9 @@ import datetime
 import logging
 import click
 from libs import build_params
+from libs.datasets.dataset_utils import data_version
 import run
-print(run)
+
 WEB_DEPLOY_PATH = "../covid-projections/public/data"
 
 
@@ -26,21 +27,34 @@ def main():
     is_flag=True,
     help="Only runs the county summary if true.",
 )
-def run_county(state=None, deploy=False, summary_only=False):
+@click.option(
+    '--git-hash',
+    type=str,
+    help='''
+    | Git hash of the commit in covid-data-public to use.
+    | If provided, covid-data-public must have no pending changes.
+    | If omitted, the repository will be used as-is'''
+)
+def run_county(state=None, deploy=False, summary_only=False, git_hash=None):
     """Run county level model."""
-    min_date = datetime.datetime(2020, 3, 7)
-    max_date = datetime.datetime(2020, 7, 6)
+    with data_version(git_hash) as version:
+        min_date = datetime.datetime(2020, 3, 7)
+        max_date = datetime.datetime(2020, 7, 6)
 
-    output_dir = build_params.OUTPUT_DIR
-    if deploy:
-        output_dir = WEB_DEPLOY_PATH
+        output_dir = build_params.OUTPUT_DIR
+        if deploy:
+            output_dir = WEB_DEPLOY_PATH
 
-    if not summary_only:
-        run.run_county_level_forecast(
-            min_date, max_date, country="USA", state=state, output_dir=output_dir
-        )
-    run.build_county_summary(min_date, state=state, output_dir=output_dir)
-
+        if not summary_only:
+            run.run_county_level_forecast(
+                min_date, max_date, country="USA", state=state, output_dir=output_dir
+            )
+        run.build_county_summary(min_date, state=state, output_dir=output_dir)
+        # only write the version if we saved everything
+        if state is None and not summary_only:
+            version.write_file('counties', output_dir)
+        else:
+            logging.info('Skip version file because this is not a full run')
 
 @main.command("state")
 @click.option("--state", "-s")
@@ -49,18 +63,33 @@ def run_county(state=None, deploy=False, summary_only=False):
     is_flag=True,
     help="Output data files to public data directory in local covid-projections.",
 )
-def run_state(state=None, deploy=False):
+@click.option(
+    '--git-hash',
+    type=str,
+    help='''
+    | Git hash of the commit in covid-data-public to use.
+    | If provided, covid-data-public must have no pending changes.
+    | If omitted, the repository will be used as-is'''
+)
+def run_state(state=None, deploy=False, git_hash=None):
     """Run State level model."""
-    min_date = datetime.datetime(2020, 3, 7)
-    max_date = datetime.datetime(2020, 7, 6)
+    with data_version(git_hash) as version:
+        min_date = datetime.datetime(2020, 3, 7)
+        max_date = datetime.datetime(2020, 7, 6)
 
-    output_dir = build_params.OUTPUT_DIR
-    if deploy:
-        output_dir = WEB_DEPLOY_PATH
+        output_dir = build_params.OUTPUT_DIR
+        if deploy:
+            output_dir = WEB_DEPLOY_PATH
 
-    run.run_state_level_forecast(
-        min_date, max_date, country="USA", state=state, output_dir=output_dir
-    )
+        run.run_state_level_forecast(
+            min_date, max_date, country="USA", state=state, output_dir=output_dir
+        )
+        logging.info(f'Wrote output to {output_dir}')
+        # only write the version if we saved everything
+        if state is None :
+            version.write_file('states', output_dir)
+        else:
+            logging.info('Skip version file because this is not a full run')
 
 
 if __name__ == "__main__":
