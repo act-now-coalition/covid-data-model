fbcd2f29fb06e81681064e761c31831f625cc48d
Vincent Woo <me@vincentwoo.com>
2020-04-06 20:56:58 -0700

    Have deploy.sh build DoD artifacts as well
    Helps address #123, may conflict with a @goldblatt PR

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 PUBLIC_DATA_PATH=../covid-projections/public/data
 
-
 # Run State and County level models
 ./run_model.py state -o ${PUBLIC_DATA_PATH}
 ./run_model.py county -o ${PUBLIC_DATA_PATH}/county
 ./run_model.py county-summary -o ${PUBLIC_DATA_PATH}/county_summaries
 # Generate the latest state case summary data.
 ./run_data.py latest -o ${PUBLIC_DATA_PATH}/case_summary
+
+./deploy_dod_dataset.py -i ${PUBLIC_DATA_PATH} -o ${PUBLIC_DATA_PATH}
--- deploy_dod_dataset.py
+++ deploy_dod_dataset.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
 from io import BytesIO
 import boto3
+import click
 import os
 
 from libs.build_dod_dataset import get_usa_by_county_df, get_usa_by_states_df, get_usa_county_shapefile, get_usa_state_shapefile
@@ -7,13 +9,14 @@ from libs.build_dod_dataset import get_usa_by_county_with_projection_df
 
 class DatasetDeployer():
 
-    def __init__(self, key='filename.csv', body='a random data'):
+    def __init__(self, key='filename.csv', body='a random data', output_dir='.'):
         self.s3 = boto3.client('s3')
         # Supplied by ENV on AWS
         # BUCKET_NAME format is s3://{BUCKET_NAME}
         self.bucket_name = os.environ.get('BUCKET_NAME')
         self.key = key
         self.body = body
+        self.output_dir = output_dir
 
     def _persist_to_s3(self):
         """Persists specific data onto an s3 bucket.
@@ -33,7 +36,7 @@ class DatasetDeployer():
         """
         print('persisting {} to local'.format(self.key))
 
-        with open(self.key, 'wb') as f:
+        with open(os.path.join(self.output_dir, self.key), 'wb') as f:
             # hack to allow the local writer to take either bytes or a string
             # note this assumes that all strings are given in utf-8 and not,
             # like, ASCII
@@ -48,40 +51,40 @@ class DatasetDeployer():
             self._persist_to_local()
         return
 
-
-def deploy():
+@click.command()
+@click.option('--input', '-i', default='results', help='Input directory of state/county projections')
+@click.option('--output', '-o', default='', help='Output directory for artifacts')
+def deploy(input, output):
     """The entry function for invokation
 
     """
-    states_blob = {
-        'key': 'states.csv',
-        'body': get_usa_by_states_df().to_csv()
-        }
-    statesObj = DatasetDeployer(**states_blob)
-    statesObj.persist()
+    DatasetDeployer(
+        key='states.csv',
+        body=get_usa_by_states_df(input).to_csv(),
+        output_dir=output
+    ).persist()
 
-    counties_blob = {
-        'key': 'counties.csv',
-        'body': get_usa_by_county_with_projection_df().to_csv()
-        }
-    countiesObj = DatasetDeployer(**counties_blob)
-    countiesObj.persist()
+    DatasetDeployer(
+        key='counties.csv',
+        body=get_usa_by_county_with_projection_df(input).to_csv(),
+        output_dir=output
+    ).persist()
 
     states_shp = BytesIO()
     states_shx = BytesIO()
     states_dbf = BytesIO()
-    get_usa_state_shapefile(states_shp, states_shx, states_dbf)
-    DatasetDeployer(key='states.shp', body=states_shp.getvalue()).persist()
-    DatasetDeployer(key='states.shx', body=states_shx.getvalue()).persist()
-    DatasetDeployer(key='states.dbf', body=states_dbf.getvalue()).persist()
+    get_usa_state_shapefile(input, states_shp, states_shx, states_dbf)
+    DatasetDeployer(key='states.shp', body=states_shp.getvalue(), output_dir=output).persist()
+    DatasetDeployer(key='states.shx', body=states_shx.getvalue(), output_dir=output).persist()
+    DatasetDeployer(key='states.dbf', body=states_dbf.getvalue(), output_dir=output).persist()
 
     counties_shp = BytesIO()
     counties_shx = BytesIO()
     counties_dbf = BytesIO()
-    get_usa_county_shapefile(counties_shp, counties_shx, counties_dbf)
-    DatasetDeployer(key='counties.shp', body=counties_shp.getvalue()).persist()
-    DatasetDeployer(key='counties.shx', body=counties_shx.getvalue()).persist()
-    DatasetDeployer(key='counties.dbf', body=counties_dbf.getvalue()).persist()
+    get_usa_county_shapefile(input, counties_shp, counties_shx, counties_dbf)
+    DatasetDeployer(key='counties.shp', body=counties_shp.getvalue(), output_dir=output).persist()
+    DatasetDeployer(key='counties.shx', body=counties_shx.getvalue(), output_dir=output).persist()
+    DatasetDeployer(key='counties.dbf', body=counties_dbf.getvalue(), output_dir=output).persist()
 
     print('finished dod job')
 
