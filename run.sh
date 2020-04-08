6ad939e89e9fadf2695d24778b7a10aaec75e893
Michael Lehenbauer <mikelehen@gmail.com>
2020-04-08 11:28:42 -0700

    Add version.json to API output directory.
    * Also rename absPath to abs_path for consistency.

--- run.sh
+++ run.sh
@@ -15,8 +15,8 @@ prepare () {
     echo "Example: $0 ../covid-data-public/ ./api_results/"
     exit 1
   else
-    DATA_SOURCES_DIR="$(absPath $1)"
-    API_OUTPUT_DIR="$(absPath $2)"
+    DATA_SOURCES_DIR="$(abs_path $1)"
+    API_OUTPUT_DIR="$(abs_path $2)"
   fi
 
   if [ ! -d "${DATA_SOURCES_DIR}" ] ; then
@@ -70,11 +70,71 @@ execute() {
   mkdir -p "${DOD_DIR}"
   ./deploy_dod_dataset.py -i "${STATES_DIR}" -o "${DOD_DIR}"
 
+  echo ">>> Generating ${API_OUTPUT_DIR}/version.json"
+  generate_version_json
+
   echo ">>> All API Artifacts written to ${API_OUTPUT_DIR}"
 }
 
+# Generates a version.json file in the API_OUTPUT_DIR capturing the time
+# and state of all repos.
+function generate_version_json() {
+  local model_repo_json=$(get_repo_status_json)
+
+  pushd "${DATA_SOURCES_DIR}" > /dev/null
+  local data_repo_json=$(get_repo_status_json)
+  popd > /dev/null
+
+  local timestamp=$(iso_timestamp)
+
+  cat > "${API_OUTPUT_DIR}/version.json" << END
+{
+  "timestamp": "${timestamp}",
+  "covid-data-public": ${data_repo_json},
+  "covid-data-model": ${model_repo_json}
+}
+END
+}
+
+# Returns a { branch: ..., hash: ..., dirty: ... } JSON blob describing the
+# status of the repo we are currently in.
+function get_repo_status_json() {
+  local branch=$(get_repo_branch)
+  local hash=$(get_repo_commit_hash)
+  local dirty=$(get_repo_dirty)
+
+  echo "{ \"branch\": \"${branch}\", \"hash\": \"${hash}\", \"dirty\": ${dirty} }"
+}
+
+# Returns the name of the active branch or just "HEAD" if it's detached.
+function get_repo_branch() {
+  local branch_name=$(git symbolic-ref -q HEAD)
+  branch_name=${branch_name##refs/heads/}
+  branch_name=${branch_name:-HEAD}
+  echo "${branch_name}"
+}
+
+# Returns the SHA hash of the latest commit.
+function get_repo_commit_hash() {
+  git rev-parse --verify HEAD
+}
+
+# Returns true if there are any modified / untracked files in the repo.
+function get_repo_dirty() {
+  if [ -z "$(git status --porcelain)" ]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+}
+
+# Returns a UTC timestamp in ISO 8601 format.
+function iso_timestamp() {
+  date -u +%Y-%m-%dT%H:%M:%S%z
+}
+
 # Helper for getting absolute paths.
-function absPath() {
+function abs_path() {
   (
   cd "$(dirname $1)"
   echo "$PWD/$(basename $1)"
