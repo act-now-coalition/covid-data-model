56ef37b48904b24940fbbbf65f80612f6edb5592
Chris Kelly <ghop02@gmail.com>
2020-04-08 00:15:30 -0400

    moving core of dod dataset generation to its own pipeline

--- deploy_dod_dataset.py
+++ deploy_dod_dataset.py
@@ -1,58 +1,30 @@
 #!/usr/bin/env python
-from io import BytesIO
-import boto3
 import click
-import os
 import logging
 
 from libs.enums import Intervention
-from libs.validate_results import validate_states_df, validate_counties_df, validate_states_shapefile, validate_counties_shapefile
-from libs.build_dod_dataset import get_usa_by_county_with_projection_df, get_usa_by_states_df
-from libs.functions import generate_shapefiles
-from libs import dataset_deployer
+from libs.pipelines import dod_pipeline
 logger = logging.getLogger(__name__)
 PROD_BUCKET = "data.covidactnow.org"
 
 
 @click.command()
 @click.option('--run_validation', '-r', default=True, help='Run the validation on the deploy command')
-@click.option('--input', '-i', default='results', help='Input directory of state/county projections')
+@click.option('--input-file', '-i', default='results', help='Input directory of state/county projections')
 @click.option('--output', '-o', default='results/dod', help='Output directory for artifacts')
-def deploy(run_validation, input, output):
+def deploy(run_validation, input_file, output):
     """The entry function for invocation"""
 
-    for intervention_enum in list(Intervention):
-        logger.info(f"Starting to generate files for {intervention_enum.name}.")
+    for intervention in list(Intervention):
+        logger.info(f"Starting to generate files for {intervention.name}.")
 
-        states_key_name = f'states.{intervention_enum.name}'
-        states_df = get_usa_by_states_df(input, intervention_enum.value)
-        if run_validation:
-            validate_states_df(states_key_name, states_df)
-        dataset_deployer.upload_csv(states_key_name, states_df.to_csv(), output)
-
-        states_shp, states_shx, states_dbf = generate_shapefiles.get_usa_state_shapefile(
-            states_df
+        state_result, county_result = dod_pipeline.run_projections(
+            input_file, intervention, run_validation=run_validation
         )
-        if run_validation:
-            validate_states_shapefile(states_key_name, states_shp, states_shx, states_dbf)
-        dataset_deployer.deploy_shape_files(output, states_key_name, states_shp, states_shx, states_dbf)
-        logger.info(f"Generated state shape files for {intervention_enum.name}")
+        dod_pipeline.deploy_results(state_result, output)
+        dod_pipeline.deploy_results(county_result, output)
 
-        counties_key_name = f'counties.{intervention_enum.name}'
-        counties_df = get_usa_by_county_with_projection_df(input, intervention_enum.value)
-        if run_validation:
-            validate_counties_df(counties_key_name, counties_df)
-        dataset_deployer.upload_csv(counties_key_name, counties_df.to_csv(), output)
-
-        counties_shp, counties_shx, counties_dbf = generate_shapefiles.get_usa_county_shapefile(
-            counties_df
-        )
-        if run_validation:
-            validate_counties_shapefile(counties_key_name, counties_shp, counties_shx, counties_dbf)
-        dataset_deployer.deploy_shape_files(output, counties_key_name, counties_shp, counties_shx, counties_dbf)
-        logger.info(f"Generated counties shape files for {intervention_enum.name}")
-
-    print('finished dod job')
+    logger.info('finished dod job')
 
 
 if __name__ == "__main__":
@@ -67,6 +39,6 @@ if __name__ == "__main__":
     # triggering persistance to local
     python deploy_dod_dataset.py
     """
-    # pylint: disable=no-value-for-parameter
     logging.basicConfig(level=logging.INFO)
+    # pylint: disable=no-value-for-parameter
     deploy()
