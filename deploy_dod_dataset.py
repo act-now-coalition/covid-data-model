ec21f45d06ba91c86c59be9db933ff043f421d82
Chris Kelly <ghop02@gmail.com>
2020-04-08 19:32:43 -0400

    Generate shapefile byte buffers in generate_shapefiles (#155)
    * moving datset deployer out of deploy_dod_dataset
    
    * Generate shapefile byte buffers in generate_shapefiles
    
    * moving core of dod dataset generation to its own pipeline (#156)
    
    * moving core of dod dataset generation to its own pipeline
    
    * File -> dir

--- deploy_dod_dataset.py
+++ deploy_dod_dataset.py
@@ -1,60 +1,30 @@
 #!/usr/bin/env python
-from io import BytesIO
-import boto3
 import click
-import os
 import logging
 
 from libs.enums import Intervention
-from libs.validate_results import validate_states_df, validate_counties_df, validate_states_shapefile, validate_counties_shapefile
-from libs.build_dod_dataset import get_usa_by_county_with_projection_df, get_usa_by_states_df
-from libs.functions.generate_shapefiles import get_usa_county_shapefile, get_usa_state_shapefile
-from libs import dataset_deployer
+from libs.pipelines import dod_pipeline
 logger = logging.getLogger(__name__)
 PROD_BUCKET = "data.covidactnow.org"
 
 
 @click.command()
 @click.option('--run_validation', '-r', default=True, help='Run the validation on the deploy command')
-@click.option('--input', '-i', default='results', help='Input directory of state/county projections')
+@click.option('--input-dir', '-i', default='results', help='Input directory of state/county projections')
 @click.option('--output', '-o', default='results/dod', help='Output directory for artifacts')
-def deploy(run_validation, input, output):
+def deploy(run_validation, input_dir, output):
     """The entry function for invocation"""
 
-    for intervention_enum in list(Intervention):
-        logger.info(f"Starting to generate files for {intervention_enum.name}.")
+    for intervention in list(Intervention):
+        logger.info(f"Starting to generate files for {intervention.name}.")
 
-        states_key_name = f'states.{intervention_enum.name}'
-        states_df = get_usa_by_states_df(input, intervention_enum.value)
-        if run_validation:
-            validate_states_df(states_key_name, states_df)
-        dataset_deployer.upload_csv(states_key_name, states_df.to_csv(), output)
+        state_result, county_result = dod_pipeline.run_projections(
+            input_dir, intervention, run_validation=run_validation
+        )
+        dod_pipeline.deploy_results(state_result, output)
+        dod_pipeline.deploy_results(county_result, output)
 
-        states_shp = BytesIO()
-        states_shx = BytesIO()
-        states_dbf = BytesIO()
-        get_usa_state_shapefile(states_df, states_shp, states_shx, states_dbf)
-        if run_validation:
-            validate_states_shapefile(states_key_name, states_shp, states_shx, states_dbf)
-        dataset_deployer.deploy_shape_files(output, states_key_name, states_shp, states_shx, states_dbf)
-        logger.info(f"Generated state shape files for {intervention_enum.name}")
-
-        counties_key_name = f'counties.{intervention_enum.name}'
-        counties_df = get_usa_by_county_with_projection_df(input, intervention_enum.value)
-        if run_validation:
-            validate_counties_df(counties_key_name, counties_df)
-        dataset_deployer.upload_csv(counties_key_name, counties_df.to_csv(), output)
-
-        counties_shp = BytesIO()
-        counties_shx = BytesIO()
-        counties_dbf = BytesIO()
-        get_usa_county_shapefile(counties_df, counties_shp, counties_shx, counties_dbf)
-        if run_validation:
-            validate_counties_shapefile(counties_key_name, counties_shp, counties_shx, counties_dbf)
-        dataset_deployer.deploy_shape_files(output, counties_key_name, counties_shp, counties_shx, counties_dbf)
-        logger.info(f"Generated counties shape files for {intervention_enum.name}")
-
-    print('finished dod job')
+    logger.info('finished dod job')
 
 
 if __name__ == "__main__":
@@ -69,5 +39,6 @@ if __name__ == "__main__":
     # triggering persistance to local
     python deploy_dod_dataset.py
     """
+    logging.basicConfig(level=logging.INFO)
     # pylint: disable=no-value-for-parameter
     deploy()
