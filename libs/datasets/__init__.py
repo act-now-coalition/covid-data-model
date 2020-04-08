d0109ff1a99efa747fb1141bab1bfe9c9bf1957c
Chris Kelly <ghop02@gmail.com>
2020-04-08 00:34:43 -0400

    Adding Covid Tracking hospitalization numbers Data Source (#143)
    * Adding Covid Tracking hospitalization numbers
    
    * removing unused code + imports

--- libs/datasets/__init__.py
+++ libs/datasets/__init__.py
@@ -4,5 +4,6 @@ import enum
 from libs.datasets.sources.jhu_dataset import JHUDataset
 from libs.datasets.sources.nytimes_dataset import NYTimesDataset
 from libs.datasets.sources.dh_beds import DHBeds
+from libs.datasets.sources.covid_tracking_source import CovidTrackingDataSource
 from libs.datasets.sources.cds_dataset import CDSDataset
 from libs.datasets.sources.fips_population import FIPSPopulation
