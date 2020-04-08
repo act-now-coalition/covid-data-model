dce8a16fbe70697e609b470e0635694260f48c90
Susan Goldblatt <sgoldblatt@Susans-MacBook-Pro.local>
2020-04-08 15:55:24 -0400

    removing the poverty data set

--- libs/datasets/__init__.py
+++ libs/datasets/__init__.py
@@ -6,4 +6,3 @@ from libs.datasets.sources.nytimes_dataset import NYTimesDataset
 from libs.datasets.sources.dh_beds import DHBeds
 from libs.datasets.sources.cds_dataset import CDSDataset
 from libs.datasets.sources.fips_population import FIPSPopulation
-from libs.datasets.sources.poverty_dataset import PovertyPopulation
