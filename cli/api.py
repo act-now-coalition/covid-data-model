2eed2ee4ce1129c73ca1c6e539fa29a05112f70d
Chris Kelly <ghop02@gmail.com>
2020-04-10 00:01:14 -0400

    Update comment for api cli

--- cli/api.py
+++ cli/api.py
@@ -17,7 +17,7 @@ def main():
     '--output-dir', '-o', type=pathlib.Path, help="Output directory to save schemas in.", default="api/schemas"
 )
 def update_schemas(output_dir):
-
+    """Updates all public facing API schemas."""
     schemas = api.load_public_schemas()
     for schema in schemas:
         path = output_dir / f"{schema.__name__}.json"
