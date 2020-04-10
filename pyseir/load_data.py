9945c61ffdbf614b85e7667489f58283faf064fa
zhang-xinyu <xinyu.zhang@grandrounds.com>
2020-04-10 12:13:33 -0500

    add functions to load_data

--- pyseir/load_data.py
+++ pyseir/load_data.py
@@ -152,6 +152,47 @@ def load_county_metadata():
     """
     return pd.read_json(os.path.join(DATA_DIR, 'county_metadata.json'), dtype={'fips': 'str'})
 
+def load_county_metadata_by_state(state):
+    """
+    Generate a dataframe that contains county metadata aggregated at state
+    level.
+
+    Parameters
+    ----------
+    state: str or list(str)
+        Name of state to load the metadata for.
+
+    Returns
+    -------
+    state_metadata: pd.DataFrame
+    """
+
+    # aggregate into state level metadata
+    state_metadata = load_county_metadata()
+
+    if state:
+        state = [state] if not isinstance(state, list) else state
+        state_metadata = state_metadata[state_metadata.state.isin(state)]
+
+    density_measures = ['housing_density', 'population_density']
+    for col in density_measures:
+        state_metadata[col] = state_metadata[col] * state_metadata['total_population']
+
+    age_dist = state_metadata.groupby('state')['age_distribution'] \
+                             .apply(lambda l: np.stack(np.array(l)).sum(axis=0))
+    density_info = state_metadata.groupby('state').agg(
+        {'population_density': lambda x: sum(x),
+         'housing_density': lambda x: sum(x),
+         'total_population': lambda x: sum(x),
+         'fips': list})
+    age_bins = state_metadata[['state', 'age_bin_edges']].groupby('state').first()
+    state_metadata = pd.concat([age_dist, density_info, age_bins], axis=1)
+
+    for col in density_measures:
+        state_metadata[col] /= state_metadata['total_population']
+
+    return state_metadata
+
 
 def load_ensemble_results(fips, run_mode='default'):
     """
@@ -246,6 +287,44 @@ def load_new_case_data_by_fips(fips, t0):
     observed_new_deaths = county_case_data['deaths'].values[1:] - county_case_data['deaths'].values[:-1]
     return times_new, observed_new_cases, observed_new_deaths
 
+def load_new_case_data_by_state(state, t0):
+    """
+    Get data for new cases at state level.
+
+    Parameters
+    ----------
+    state: str
+        State full name.
+    t0: datetime
+        Datetime to offset by.
+
+    Returns
+    -------
+    times: array(float)
+        List of float days since t0 for the case and death counts below
+    observed_new_cases: array(int)
+        Array of new cases observed each day.
+    observed_new_deaths: array(int)
+        Array of new deaths observed each day.
+    """
+    _state_fips = load_county_metadata()[['state', 'fips']]
+    _state_fips = _state_fips[_state_fips.state == state].drop_duplicates()
+
+    # set long enough lists to record new cases and deaths count
+    observed_new_cases_state = np.zeros(360)
+    observed_new_deaths_state = np.zeros(360)
+    times_new_state = np.zeros(360)
+    for fips in _state_fips['fips']:
+        times, observed_new_cases, observed_new_deaths = \
+            load_new_case_data_by_fips(fips, t0)
+        observed_new_cases_state[times.values] += observed_new_cases
+        observed_new_deaths_state[times.values] += observed_new_deaths
+        times_new_state[times.values] = times.values
+    observed_new_cases_state = observed_new_cases_state[times_new_state > 0]
+    observed_new_deaths_state = observed_new_deaths_state[times_new_state > 0]
+    times_new_state = times_new_state[times_new_state > 0]
+
+    return times_new_state, observed_new_cases_state, observed_new_deaths_state
 
 def load_hospital_data():
     """
