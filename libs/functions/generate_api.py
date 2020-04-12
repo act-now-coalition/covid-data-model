3b79b5ffe12d1fa55ccd95d5870e7ad13d40c0bf
Susan Goldblatt <suzgoldblatt@gmail.com>
2020-04-12 14:50:03 -0400

    Data Pipeline for top 100 counties (#192)
    Adding a data pipeline for the top 100 counties. Creating the start of the API schema. Will have a follow up PR to put this into S3 bucket!

---
+++ libs/functions/generate_api.py
@@ -0,0 +1,41 @@
+from api.can_predictions import (
+    CANPredictionAPIRow,
+    CANPredictionAPI,
+    _Projections,
+    _HospitalBeds,
+)
+from libs.datasets import results_schema as rc
+
+
+def _get_date_or_none(panda_date_or_none):
+    """ Projection Null value is a string NULL so if this date value is a string,
+     make it none. Otherwise convert to the python datetime. Example 
+     of this being null is when there is no bed shortfall, the shortfall dates is none """
+    if isinstance(panda_date_or_none, str):
+        return None
+    return panda_date_or_none.to_pydatetime()
+
+
+def generate_api_for_projection(projection):
+    api_results = []
+
+    for index, county_row in projection.iterrows():
+        peak_date = _get_date_or_none(county_row[rc.PEAK_HOSPITALIZATIONS])
+        shortage_start_date = _get_date_or_none(county_row[rc.HOSPITAL_SHORTFALL_DATE])
+        _hospital_beds = _HospitalBeds(
+            peakDate=peak_date,
+            shortageStartDate=shortage_start_date,
+            peakShortfall=county_row[rc.PEAK_HOSPITALIZATION_SHORTFALL],
+        )
+        _projections = _Projections(
+            hospitalBeds=_hospital_beds
+        )
+        county_result = CANPredictionAPIRow(
+            stateName=county_row[rc.STATE],
+            countyName=county_row[rc.COUNTY],
+            fips=county_row[rc.FIPS],
+            lastUpdatedDate=county_row[rc.LAST_UPDATED],
+            projections=_projections,
+        )
+        api_results.append(county_result)
+    return CANPredictionAPI(data=api_results)
