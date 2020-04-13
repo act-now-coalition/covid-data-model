99184dceb84cce1e929120b3e37ff4bed40c4b98
Susan Goldblatt <suzgoldblatt@gmail.com>
2020-04-13 12:22:18 -0400

    formatting the updated date to be iso format (#213)
    * formatting the updated date to be iso format
    
    * use dashes not underscores
    
    Co-authored-by: Susan Goldblatt <sgoldblatt@Susans-MacBook-Pro.local>

--- libs/functions/generate_api.py
+++ libs/functions/generate_api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from api.can_predictions import (
     CANPredictionAPIRow,
     CANPredictionAPI,
@@ -7,14 +8,24 @@ from api.can_predictions import (
 from libs.datasets import results_schema as rc
 
 
+def _format_date(input_date):
+    if not input_date: 
+        raise Exception("Can't format a date that doesn't exist")
+    if isinstance(input_date, str): 
+        # note if this is already in iso format it will be grumpy. maybe use dateutil
+        datetime_obj = datetime.strptime(input_date, '%m/%d/%Y %H:%M')  
+        return datetime_obj.isoformat()
+    if isinstance(input_date, datetime): 
+        return input_date.isoformat()
+    raise Exception("Invalid date type when converting to api")
+
 def _get_date_or_none(panda_date_or_none):
     """ Projection Null value is a string NULL so if this date value is a string,
      make it none. Otherwise convert to the python datetime. Example 
      of this being null is when there is no bed shortfall, the shortfall dates is none """
     if isinstance(panda_date_or_none, str):
         return None
-    return panda_date_or_none.to_pydatetime()
-
+    return _format_date(panda_date_or_none.to_pydatetime())
 
 def generate_api_for_projection(projection):
     api_results = []
@@ -34,7 +45,7 @@ def generate_api_for_projection(projection):
             stateName=county_row[rc.STATE],
             countyName=county_row[rc.COUNTY],
             fips=county_row[rc.FIPS],
-            lastUpdatedDate=county_row[rc.LAST_UPDATED],
+            lastUpdatedDate=_format_date(county_row[rc.LAST_UPDATED]),
             projections=_projections,
         )
         api_results.append(county_result)
