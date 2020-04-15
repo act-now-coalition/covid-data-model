713ff12619b92d12adb69e11e72105ceaa0f396a
Simon Frid <simon.frid@gmail.com>
2020-04-14 22:11:41 -0700

    meta parallelize

--- pyseir/__init__.py
+++ pyseir/__init__.py
@@ -1,3 +1,37 @@
 import pathlib
+import os, math
+
+import multiprocessing.pool
+
 DATA_DIR = pathlib.Path(__file__).parent.parent / 'data'
 OUTPUT_DIR = pathlib.Path(__file__).parent.parent / 'output'
+
+# from multiprocessing import Pool
+
+def divide_up_pool():
+    """Divides up a large pool of cpus to be able to run parallelized runs, also in parallel.
+
+    The first integer is the total available for a given stats/county.
+    The latter integer is the number of states to run at the same time.
+
+    Returns:
+        (tuple[int, int]) -- a tuple of
+    """
+    total = os.cpu_count()
+    if total < 10:
+        return total, 1
+    return math.floor(total/4), 4
+
+
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class Pool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
