4e22cdfa69a5be61af76916ca1b792e620e16f16
Susan Goldblatt <sgoldblatt@Susans-MacBook-Pro.local>
2020-04-16 19:49:06 -0400

    more fixes

--- libs/enums.py
+++ libs/enums.py
@@ -9,8 +9,21 @@ class Intervention(enum.Enum):
     NO_INTERVENTION = 0
     FLATTEN = 1
     # FULL_CONTAINMENT = 2 # you are cancelled
-    SOCIAL_DISTANCING = 2
-    CURRENT = 3  # look at what the state is and get the file for that
+    SOCIAL_DISTANCING = 3
+    CURRENT = 4  # look at what the state is and get the file for that
+    INFERRED = 5 # given the previous pattern, how do we predict going forward
+
+    @classmethod
+    def from_webui_data_adaptor(cls, label):
+        if label == "suppression_policy__no_intervention": 
+            return cls.NO_INTERVENTION
+        elif label == "suppression_policy__flatten_the_curve":
+            return cls.FLATTEN
+        elif label == "suppression_policy__inferred": 
+            return cls.INFERRED
+        elif label == "suppression_policy__social_distancing":
+            return cls.SOCIAL_DISTANCING
+        raise Exception(f"Unexpected WebUI Data Adaptor label: {label}")
 
     @classmethod
     def from_str(cls, label):
