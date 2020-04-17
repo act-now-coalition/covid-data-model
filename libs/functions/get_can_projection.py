c2c34fb14f87994865062c09a8663284de0da755
Susan Goldblatt <suzgoldblatt@gmail.com>
2020-04-16 22:28:18 -0400

    Revert "return enum instead of the value (#250)" (#255)
    This reverts commit 23966253c8ed99c907f365373f8f6590c588d995.

--- libs/functions/get_can_projection.py
+++ libs/functions/get_can_projection.py
@@ -15,9 +15,9 @@ def _get_interventions_for_state(state):
 
 
 def _get_intervention(intervention, state):
-    if intervention == Intervention.CURRENT:
+    if intervention == Intervention.CURRENT.value:
         state_intervention = _get_interventions_for_state(state)
-        return Intervention.from_str(state_intervention)
+        return Intervention.from_str(state_intervention).value
     return intervention
 
 
