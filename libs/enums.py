5a717e3f07972b37b41cfd2d3868437d8cae6548
Susan Goldblatt <suzgoldblatt@gmail.com>
2020-04-16 22:28:18 -0400

    Revert "return enum instead of the value (#250)" (#255)
    This reverts commit 23966253c8ed99c907f365373f8f6590c588d995.

--- libs/enums.py
+++ libs/enums.py
@@ -4,34 +4,13 @@ import enum
 # TODO: This should maybe be unique per state.
 UNKNOWN_FIPS = "99999"
 
+
 class Intervention(enum.Enum):
     NO_INTERVENTION = 0
-    FLATTEN = 1 # on the webiste, strictDistancingNow
+    FLATTEN = 1
     # FULL_CONTAINMENT = 2 # you are cancelled
-    SOCIAL_DISTANCING = 3 # weak distancingNow on the website
+    SOCIAL_DISTANCING = 3
     CURRENT = 4  # look at what the state is and get the file for that
-    INFERRED = 5 # given the previous pattern, how do we predict going forward
-
-    @classmethod
-    def county_supported_interventions(cls): 
-        return [
-            Intervention.NO_INTERVENTION,
-            Intervention.FLATTEN,
-            Intervention.SOCIAL_DISTANCING,
-            Intervention.CURRENT
-        ]
-
-    @classmethod
-    def from_webui_data_adaptor(cls, label):
-        if label == "suppression_policy__no_intervention": 
-            return cls.NO_INTERVENTION
-        elif label == "suppression_policy__flatten_the_curve":
-            return cls.FLATTEN
-        elif label == "suppression_policy__inferred": 
-            return cls.INFERRED
-        elif label == "suppression_policy__social_distancing":
-            return cls.SOCIAL_DISTANCING
-        raise Exception(f"Unexpected WebUI Data Adaptor label: {label}")
 
     @classmethod
     def from_str(cls, label):
