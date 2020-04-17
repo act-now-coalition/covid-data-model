b175b2e4b74978fbcc262e8f07f89d457c45cb7e
Susan Goldblatt <suzgoldblatt@gmail.com>
2020-04-17 14:38:58 -0400

    Use Enum Value to generate pyseir model files  (#260)
    * Revert "Revert "return enum instead of the value (#250)" (#255)"
    
    This reverts commit 8cadd30f582057db346ad7fb942023f8813dbb69.
    
    * Fixing the pyseir file generation to use enum values
    
    Co-authored-by: Susan Goldblatt <sgoldblatt@Susans-MacBook-Pro.local>

--- libs/enums.py
+++ libs/enums.py
@@ -4,13 +4,35 @@ import enum
 # TODO: This should maybe be unique per state.
 UNKNOWN_FIPS = "99999"
 
-
 class Intervention(enum.Enum):
     NO_INTERVENTION = 0
-    FLATTEN = 1
-    # FULL_CONTAINMENT = 2 # you are cancelled
-    SOCIAL_DISTANCING = 3
+    FLATTEN = 1 # on the webiste, strictDistancingNow
+    # FULL_CONTAINMENT = 2 # you are cancelled, but reusing this enum value
+    SOCIAL_DISTANCING = 3 # weak distancingNow on the website
     CURRENT = 4  # look at what the state is and get the file for that
+    # We are using enum 2 for consistency with the website 
+    INFERRED = 2 # given the previous pattern, how do we predict going forward
+
+    @classmethod
+    def county_supported_interventions(cls): 
+        return [
+            Intervention.NO_INTERVENTION,
+            Intervention.FLATTEN,
+            Intervention.SOCIAL_DISTANCING,
+            Intervention.CURRENT
+        ]
+
+    @classmethod
+    def from_webui_data_adaptor(cls, label):
+        if label == "suppression_policy__no_intervention": 
+            return cls.NO_INTERVENTION
+        elif label == "suppression_policy__flatten_the_curve":
+            return cls.FLATTEN
+        elif label == "suppression_policy__inferred": 
+            return cls.INFERRED
+        elif label == "suppression_policy__social_distancing":
+            return cls.SOCIAL_DISTANCING
+        raise Exception(f"Unexpected WebUI Data Adaptor label: {label}")
 
     @classmethod
     def from_str(cls, label):
