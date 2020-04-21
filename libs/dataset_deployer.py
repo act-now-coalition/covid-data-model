396a1ff39cadec9566340543cc53668e093ed904
Simon Frid <simon.frid@gmail.com>
2020-04-20 18:23:11 -0700

    remove boto session

--- libs/dataset_deployer.py
+++ libs/dataset_deployer.py
@@ -1,41 +1,21 @@
 import os
 import io
 import logging
-import boto3
 
 _logger = logging.getLogger(__name__)
 
 
 class DatasetDeployer(object):
-    """Common deploy operations for persisting files to s3 or local folder.
+    """Common deploy operations for persisting files to a local folder.
 
-    Deploys to S3 if the BUCKET_NAME environment variable is set.
     """
 
     def __init__(self, key="filename.csv", body="a random data", output_dir="."):
-        self.s3 = boto3.client("s3")
-        # Supplied by ENV on AWS
-        # BUCKET_NAME format is s3://{BUCKET_NAME}
-        self.bucket_name = os.environ.get("BUCKET_NAME")
         self.key = key
         self.body = body
         self.output_dir = output_dir
 
-    def _persist_to_s3(self):
-        """Persists specific data onto an s3 bucket.
-        This method assumes versioned is handled on the bucket itself.
-        """
-        _logger.info(f"persisting {self.key} to s3")
-
-        response = self.s3.put_object(
-            Bucket=self.bucket_name, Key=self.key, Body=self.body, ACL="public-read"
-        )
-        return response
-
     def _persist_to_local(self):
-        """Persists specific data onto an s3 bucket.
-        This method assumes versioned is handled on the bucket itself.
-        """
         _logger.info(f"persisting {self.key} to local")
 
         with open(os.path.join(self.output_dir, self.key), "wb") as f:
@@ -46,14 +26,8 @@ class DatasetDeployer(object):
                 self.body.encode("UTF-8") if isinstance(self.body, str) else self.body
             )
 
-        pass
-
     def persist(self):
-        if self.bucket_name:
-            self._persist_to_s3()
-        else:
-            self._persist_to_local()
-        return
+        self._persist_to_local()
 
 
 def upload_csv(key_name: str, csv: str, output_dir: str):
@@ -67,7 +41,7 @@ def upload_csv(key_name: str, csv: str, output_dir: str):
     _logger.info(f"Generated csv for {key_name}")
 
 
-def upload_json(key_name, json: str, output_dir: str): 
+def upload_json(key_name, json: str, output_dir: str):
     DatasetDeployer(f"{key_name}.json", json, output_dir).persist()
     _logger.info(f"Generated json for {key_name}")
 
