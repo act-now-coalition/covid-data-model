6ad864669547a3317ad788b206fdf9f95d9f9706
Eric Carlson <eric.carlson@grandrounds.com>
2020-04-24 17:13:52 -0600

    Add run_all to test

--- test/test_pyseir_end_to_end_test.py
+++ test/test_pyseir_end_to_end_test.py
@@ -1,15 +1,14 @@
 import os
 import json
 import numpy as np
-from pyseir.cli import _run_all
-from pyseir.utils import get_run_artifact_path, RunArtifact 
+from pyseir import cli
+from pyseir.utils import get_run_artifact_path, RunArtifact
 import libs.datasets.can_model_output_schema as schema
-
 
 
 def test__pyseir_end_to_end():
     # This covers a lot of edge cases.
-    #_run_all(state='montana')
+    cli._run_all(state='montana')
     path = get_run_artifact_path('29189', RunArtifact.WEB_UI_RESULT).replace('__INTERVENTION_IDX__', '2')
     assert os.path.exists(path)
 
