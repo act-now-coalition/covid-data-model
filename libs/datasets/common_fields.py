734008bd1eefd36fbbb1976de44fe178e24e6f95
Chris Kelly <ghop02@gmail.com>
2020-04-23 22:48:23 -0400

    Filling in fips and standardizing state name (#304)

--- libs/datasets/common_fields.py
+++ libs/datasets/common_fields.py
@@ -6,3 +6,9 @@ class CommonFields(object):
     # Column for FIPS code. Right now a column containing fips data may be
     # county fips (a length 5 string) or state fips (a length 2 string).
     FIPS = "fips"
+
+    # 2 letter state abbreviation, i.e. MA
+    STATE = "state"
+
+    # Full state name, i.e. Massachusetts
+    STATE_FULL_NAME = "state_full_name"
