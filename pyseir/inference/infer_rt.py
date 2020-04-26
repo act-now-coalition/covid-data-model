9f7d2e84051c1ff0ad10a5c79da963ccb3ef5448
Simon Frid <simon.frid@gmail.com>
2020-04-26 12:35:48 -0700

    only what's needed

--- pyseir/inference/infer_rt.py
+++ pyseir/inference/infer_rt.py
@@ -10,7 +10,6 @@ from pyseir import load_data
 from pyseir.utils import AggregationLevel, TimeseriesType
 from pyseir.utils import get_run_artifact_path, RunArtifact
 from pyseir.parameters.parameter_ensemble_generator import ParameterEnsembleGenerator
-from pandarallel import pandarallel
 
 
 class RtInferenceEngine:
@@ -450,17 +449,10 @@ def run_state(state, states_only=False):
         df = load_data.load_county_metadata()
         all_fips = df[df['state'].str.lower() == state_obj.name.lower()].fips
 
-
-        rt_inferences = all_fips.map(lambda x: RtInferenceEngine.run_for_fips(x)).tolist()
-        # print(rt_inferences.shape)
         # Something in here doesn't like multiprocessing...
-        # p = Pool(2)
-        # rt_inferences = list(map(RtInferenceEngine.run_for_fips, all_fips))
-        # p.close()
+        rt_inferences = all_fips.map(lambda x: RtInferenceEngine.run_for_fips(x)).tolist()
 
         for fips, rt_inference in zip(all_fips, rt_inferences):
-            # print(fips)
-            # print(rt_isnference)
             county_output_file = get_run_artifact_path(fips, RunArtifact.RT_INFERENCE_RESULT)
             if rt_inference is not None:
                 rt_inference.to_json(county_output_file)
