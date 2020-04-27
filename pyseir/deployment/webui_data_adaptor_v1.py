eef7a441ae34bc36373b5978688293ee2176463c
Eric Carlson <eric.carlson@grandrounds.com>
2020-04-27 13:31:47 -0600

    Comments checkpoint

--- pyseir/deployment/webui_data_adaptor_v1.py
+++ pyseir/deployment/webui_data_adaptor_v1.py
@@ -50,57 +50,57 @@ class WebUIDataAdaptorV1:
         self.df_whitelist = load_data.load_whitelist()
         self.df_whitelist = self.df_whitelist[self.df_whitelist['inference_ok'] == True]
 
-    def backfill_output_model_fips(self, fips, t0, final_beds, output_model):
-        """
-        Add backfilled hospitalization, case, amd deaths data.
-
-        Parameters
-        ----------
-        fips: str
-            State or county fips code.
-        t0: datetime
-            Start time for the simulation.
-        final_beds: total number of beds.
-            Number of beds after scaling.
-        output_model: dict
-            Output model to impute.
-
-        Returns
-        -------
-        backfill: str
-            Backfill dataframe.
-        """
-        backfill_to_date = date(2020, 3, 3)   # @TODO: Parameterize
-        hospitalization_rate = 0.04           # @TODO: Parameterize
-        intervals_to_backfill = math.ceil((t0.date() - backfill_to_date).days / self.output_interval_days)
-        backfill_offsets = range(-intervals_to_backfill * self.output_interval_days, 0, self.output_interval_days)
-
-        backfill = pd.DataFrame()
-        backfill['days'] = backfill_offsets
-        backfill['date'] = [(t0 + timedelta(days=t)) for t in backfill_offsets]
-        backfill['date'] = backfill['date'].dt.normalize()
-        backfill['beds'] = final_beds
-
-        if len(fips) == 5:
-            actual_timeseries = self.county_timeseries[(self.county_timeseries['fips'] == fips)]
-        else:
-            actual_timeseries = self.state_timeseries[(self.state_timeseries['state'] == self.state_abbreviation)]
-
-        backfill = pd.merge(backfill, actual_timeseries[['date', 'cases', 'deaths']], on='date', how='left')
-
-        # TODO this is fragile because of the backfill hospitalization date
-        #      alignment and/or 4 day discontinuity. Luckily it is also invisible on non-log-scales.
-        # Account for two cases
-        #   (i) hospital admissions available.
-        #   (ii) Not available, so cases are imputed...
-        # We can just read the initial conditions infected and hospitalized to rescale the case data to match.
-        backfill['all_infected'] =( output_model['all_infected'][0] * backfill['cases'] / backfill['cases'].max()).fillna(0)
-        backfill['all_hospitalized'] = np.multiply(backfill['all_infected'], hospitalization_rate).fillna(0)
-
-        backfill['dead'] = backfill['deaths'].fillna(0)
-        backfill['date'] = backfill['date'].dt.strftime('%m/%d/%y')
-
-        return backfill
+    # def backfill_output_model_fips(self, fips, t0, final_beds, output_model):
+    #     """
+    #     Add backfilled hospitalization, case, amd deaths data.
+    #
+    #     Parameters
+    #     ----------
+    #     fips: str
+    #         State or county fips code.
+    #     t0: datetime
+    #         Start time for the simulation.
+    #     final_beds: total number of beds.
+    #         Number of beds after scaling.
+    #     output_model: dict
+    #         Output model to impute.
+    #
+    #     Returns
+    #     -------
+    #     backfill: str
+    #         Backfill dataframe.
+    #     """
+    #     backfill_to_date = date(2020, 3, 3)   # @TODO: Parameterize
+    #     hospitalization_rate = 0.04           # @TODO: Parameterize
+    #     intervals_to_backfill = math.ceil((t0.date() - backfill_to_date).days / self.output_interval_days)
+    #     backfill_offsets = range(-intervals_to_backfill * self.output_interval_days, 0, self.output_interval_days)
+    #
+    #     backfill = pd.DataFrame()
+    #     backfill['days'] = backfill_offsets
+    #     backfill['date'] = [(t0 + timedelta(days=t)) for t in backfill_offsets]
+    #     backfill['date'] = backfill['date'].dt.normalize()
+    #     backfill['beds'] = final_beds
+    #
+    #     if len(fips) == 5:
+    #         actual_timeseries = self.county_timeseries[(self.county_timeseries['fips'] == fips)]
+    #     else:
+    #         actual_timeseries = self.state_timeseries[(self.state_timeseries['state'] == self.state_abbreviation)]
+    #
+    #     backfill = pd.merge(backfill, actual_timeseries[['date', 'cases', 'deaths']], on='date', how='left')
+    #
+    #     # TODO this is fragile because of the backfill hospitalization date
+    #     #      alignment and/or 4 day discontinuity. Luckily it is also invisible on non-log-scales.
+    #     # Account for two cases
+    #     #   (i) hospital admissions available.
+    #     #   (ii) Not available, so cases are imputed...
+    #     # We can just read the initial conditions infected and hospitalized to rescale the case data to match.
+    #     backfill['all_infected'] =( output_model['all_infected'][0] * backfill['cases'] / backfill['cases'].max()).fillna(0)
+    #     backfill['all_hospitalized'] = np.multiply(backfill['all_infected'], hospitalization_rate).fillna(0)
+    #
+    #     backfill['dead'] = backfill['deaths'].fillna(0)
+    #     backfill['date'] = backfill['date'].dt.strftime('%m/%d/%y')
+    #
+    #     return backfill
 
     def map_fips(self, fips):
         """
@@ -116,12 +116,14 @@ class WebUIDataAdaptorV1:
         else:
             population = self.population_data.get_state_level('USA', state=self.state_abbreviation)
 
+        #TODO: Normalize model to hospitalization data.
+
         logging.info(f'Mapping output to WebUI for {self.state}, {fips}')
         pyseir_outputs = load_data.load_ensemble_results(fips)
 
         policies = [key for key in pyseir_outputs.keys() if key.startswith('suppression_policy')]
 
-        all_hospitalized_today = None
+        #all_hospitalized_today = None
         try:
             fit_results = load_inference_result(fips)
 
@@ -146,74 +148,58 @@ class WebUIDataAdaptorV1:
 
             # Hospitalizations need to be rescaled by the inferred factor to
             # match observations for display purposes.
-            if suppression_policy == 'suppression_policy__inferred' and fit_results:
-                t0 = datetime.fromisoformat(fit_results['t0_date'])
-                now_idx = int((datetime.today() - datetime.fromisoformat(fit_results['t0_date'])).days)
-                total_hosps = output_for_policy['HGen']['ci_50'][now_idx] + output_for_policy['HICU']['ci_50'][now_idx]
-                hosp_fraction = all_hospitalized_today / total_hosps
+            #if suppression_policy == 'suppression_policy__inferred' and fit_results:
+            t0 = datetime.fromisoformat(fit_results['t0_date'])
+            now_idx = int((datetime.today() - datetime.fromisoformat(fit_results['t0_date'])).days)
+            total_hosps = output_for_policy['HGen']['ci_50'][now_idx] + output_for_policy['HICU']['ci_50'][now_idx]
 
-            else:
-                t0 = datetime.today()
-                hosp_fraction = 1.
+            load_data.load_hospitalization_data_by_state()
+
+            hosp_fraction = all_hospitalized_today / total_hosps
+
+            # else:
+            #     t0 = datetime.today()
+            #     hosp_fraction = 1.
 
             t_list = output_for_policy['t_list']
             t_list_downsampled = range(0, int(max(t_list)), self.output_interval_days)
-            # Col 0 "index days since simulation start"
+
             output_model[schema.DAY_NUM] = t_list_downsampled
-            # Col 1  Actual time-series.
             output_model[schema.DATE] = [(t0 + timedelta(days=t)).date().strftime('%m/%d/%y') for t in t_list_downsampled]
-            # Col 2 "t"
             output_model[schema.TOTAL] = population
-            # Col 3 "b"
             output_model[schema.TOTAL_SUSCEPTIBLE] = np.interp(t_list_downsampled, t_list, output_for_policy['S']['ci_50'])
-            # Col 4 "c"
             output_model[schema.EXPOSED] = np.interp(t_list_downsampled, t_list, output_for_policy['E']['ci_50'])
-            # Col 5 "d"
             output_model[schema.INFECTED] = np.interp(t_list_downsampled, t_list, np.add(output_for_policy['I']['ci_50'], output_for_policy['A']['ci_50'])) # Infected + Asympt.
-            # Col 6 ("e")
             output_model[schema.INFECTED_A] = output_model[schema.INFECTED]
-            # Col 7 ("f")
             output_model[schema.INFECTED_B] = hosp_fraction * np.interp(t_list_downsampled, t_list, output_for_policy['HGen']['ci_50']) # Hosp General
-            # Col 8 ("g")
             output_model[schema.INFECTED_C] = hosp_fraction * np.interp(t_list_downsampled, t_list, output_for_policy['HICU']['ci_50']) # Hosp ICU
-            # Col 9 # General + ICU beds. don't include vent here because they are also counted in ICU
+            # General + ICU beds. don't include vent here because they are also counted in ICU
             output_model[schema.ALL_HOSPITALIZED] = np.add(output_model[schema.INFECTED_B], output_model[schema.INFECTED_C])
-            # Col 10
             output_model[schema.ALL_INFECTED] = output_model[schema.INFECTED]
-            # Col 11
             output_model[schema.DEAD] = np.interp(t_list_downsampled, t_list, output_for_policy['total_deaths']['ci_50'])
-            # Col 12
             final_beds = np.mean(output_for_policy['HGen']['capacity'])
             output_model[schema.BEDS] = final_beds
-            # Col 13
             output_model[schema.CUMULATIVE_INFECTED] = np.interp(t_list_downsampled, t_list, np.cumsum(output_for_policy['total_new_infections']['ci_50']))
 
             if fit_results:
-                # Col 14
                 output_model[schema.Rt] = np.interp(t_list_downsampled, t_list, fit_results['eps'] * fit_results['R0'] * np.ones(len(t_list)))
-                # Col 15
                 output_model[schema.Rt_ci90] = np.interp(t_list_downsampled, t_list, 2 * fit_results['eps_error'] * fit_results['R0'] * np.ones(len(t_list)))
             else:
                 output_model[schema.Rt] = 0
                 output_model[schema.Rt_ci90] = 0
 
-            # Col 16
             output_model[schema.CURRENT_VENTILATED] = hosp_fraction * np.interp(t_list_downsampled, t_list, output_for_policy['HVent']['ci_50'])
-            # Col 17
             output_model[schema.POPULATION] = population
 
-            # Col 18 (previously "m")
             output_model[schema.ICU_BED_CAPACITY] = np.mean(output_for_policy['HICU']['capacity'])
-            # Col 19 (previously "n")
             output_model[schema.VENTILATOR_CAPACITY] = np.mean(output_for_policy['HVent']['capacity'])
 
-            # Record the current number of hospitalizations in order to rescale the inference results.
-            all_hospitalized_today = output_model[schema.ALL_HOSPITALIZED][0]
-
+            # # Record the current number of hospitalizations in order to rescale the inference results.
+            # all_hospitalized_today = output_model[schema.ALL_HOSPITALIZED][0]
             # Don't backfill inferences
-            if suppression_policy != 'suppression_policy__inferred':
-                backfill = self.backfill_output_model_fips(fips, t0, final_beds, output_model)
-                output_model = pd.concat([backfill, output_model])[output_model.columns].reset_index(drop=True)
+            # if suppression_policy != 'suppression_policy__inferred':
+            #     backfill = self.backfill_output_model_fips(fips, t0, final_beds, output_model)
+            #     output_model = pd.concat([backfill, output_model])[output_model.columns].reset_index(drop=True)
 
             # Truncate date range of output.
             output_dates = pd.to_datetime(output_model['date'])
@@ -224,7 +210,6 @@ class WebUIDataAdaptorV1:
             try:
                 rt_results = load_Rt_result(fips)
                 rt_results.index = rt_results['Rt_MAP_composite'].index.strftime('%m/%d/%y')
-
                 merged = output_model.merge(rt_results[['Rt_MAP_composite', 'Rt_ci95_composite']],
                     right_index=True, left_on='date', how='left')
 
