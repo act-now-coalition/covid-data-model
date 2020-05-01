5ec4e2ddd9cf46c320732bf62178de1c9bdecbc0
Chris Kelly <ghop02@gmail.com>
2020-05-01 01:35:28 -0400

    Fix testing timeseries data pulling in county data as well

--- api/can_api_definition.py
+++ api/can_api_definition.py
@@ -188,9 +188,32 @@ class CovidActNowStateTimeseries(CovidActNowStateSummary):
 
         return rows
 
+    @pydantic.validator('timeseries')
+    def check_timeseries_one_row_per_date(cls, rows, values):
+        dates_in_row = len(set(row.date for row in rows))
+        if len(rows) != dates_in_row:
+
+            raise ValueError(
+                "Number of rows does not match number of dates: "
+                f"{len(rows)} vs. {dates_in_row}"
+            )
+
+        return rows
+
 
 class CovidActNowCountyTimeseries(CovidActNowCountySummary):
     timeseries: List[CANPredictionTimeseriesRow] = pydantic.Field(...)
+
+    @pydantic.validator('timeseries')
+    def check_timeseries_one_row_per_date(cls, rows, values):
+        dates_in_row = len(set(row.date for row in rows))
+        if len(rows) != dates_in_row:
+
+            raise ValueError(
+                "Number of rows does not match number of dates: "
+                f"{len(rows)} vs. {dates_in_row}"
+            )
+        return rows
 
 
 class CovidActNowCountiesAPI(pydantic.BaseModel):
