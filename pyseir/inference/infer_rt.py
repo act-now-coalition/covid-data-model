6f867e1f2ea242e21da6deb0709a323eb398402b
Chris Kelly <ghop02@gmail.com>
2020-05-06 18:09:08 -0400

    Catch error and add sentry alert

--- pyseir/inference/infer_rt.py
+++ pyseir/inference/infer_rt.py
@@ -1,6 +1,7 @@
 import math
 from datetime import datetime, timedelta
 import numpy as np
+import sentry_sdk
 import logging
 import pandas as pd
 from scipy import stats as sps
@@ -484,8 +485,12 @@ class RtInferenceEngine:
 
     @classmethod
     def run_for_fips(cls, fips):
-        engine = cls(fips)
-        return engine.infer_all()
+        try:
+            engine = cls(fips)
+            return engine.infer_all()
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            return None
 
 
 def run_state(state, states_only=False):
