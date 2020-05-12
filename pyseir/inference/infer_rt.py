c8814f7fab124dfd22f983040fa26af038c4f4f5
tashwoods <tashwoods@gmail.com>
2020-05-12 16:35:20 -0500

    fix inferred rt calculation

--- pyseir/inference/infer_rt.py
+++ pyseir/inference/infer_rt.py
@@ -190,12 +190,11 @@ class RtInferenceEngine:
                              .mean(std=self.kernel_std)\
                              .round()
 
-        zeros = smoothed.index[smoothed.eq(0)]
+        nonzeros = [idx for idx, val in enumerate(smoothed) if val!= 0]
         if len(zeros) == 0:
             idx_start = 0
         else:
-            last_zero = zeros.max()
-            idx_start = smoothed.index.get_loc(last_zero) + 1
+            idx_start = nonzeros[1]
         smoothed = smoothed.iloc[idx_start:]
         original = timeseries.loc[smoothed.index]
 
@@ -321,7 +320,7 @@ class RtInferenceEngine:
 
         return dates[start_idx:], times[start_idx:], posteriors
 
-    def infer_all(self, plot=False, shift_deaths=0):
+    def infer_all(self, plot=True, shift_deaths=0):
         """
         Infer R_t from all available data sources.
 
@@ -429,6 +428,10 @@ class RtInferenceEngine:
                                  alpha=.4, color='darkseagreen')
                 plt.scatter(df_all.index, df_all['Rt_MAP__new_hospitalizations'],
                             alpha=1, s=25, color='darkseagreen', label='New Hospitalizations', marker='d')
+            if 'Rt_MAP_composite' in df_all:
+                plt.scatter(df_all.index, df_all['Rt_MAP_composite'],
+                            alpha=1, s=25, color='yellow', label='Inferred $R_{t}$ Web', marker='d')
+            print('HERE')
 
             plt.hlines([1.0], *plt.xlim(), alpha=1, color='g')
             plt.hlines([1.1], *plt.xlim(), alpha=1, color='gold')
