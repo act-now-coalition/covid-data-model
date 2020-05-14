79ea00269d1b5d0e77a53d8b497c86fe6ba6fe0b
tashwoods <tashwoods@gmail.com>
2020-05-13 22:15:19 -0500

    updating comparison plots

--- raw_data_QA/check_nyt_raw_data.py
+++ raw_data_QA/check_nyt_raw_data.py
@@ -21,13 +21,10 @@ def aggregate_df(df, args):
 
 def get_state(df, state, args):
   df_new = df.loc[df[args.state_name] == state]
-  df_new[args.updated_date_name] = df[[args.date_name]]
+  #df_new[args.updated_date_name] = df[[args.date_name]]
   return df_new
 
 def get_rmse(df1, df2, var):
-  #df1_length = len(df1.index)
-  #truncated_df2 = df2.head(df1_length)
-  #truncated_df2 = get_equal_len_df(df1, df2)
   rmse = np.sqrt(mean_squared_error(df1[[var]], df2[[var]]))
   return round(rmse,2)
 
@@ -36,33 +33,44 @@ def get_equal_len_df(df1, df2):
   truncated_df1 = df1.head(df2_length)
   return truncated_df1
 
-def get_percent_diff(df1, df2, var):
-  #diff = ((df1[[var]] - df2[[var]])/ df1[[var]])
-  diff2 = []
+def get_compare_metrics(df1, df2, var):
+  diff = []
   z_scores = []
   for i in range(len(df1.index)):
     var1 = df1[var].iloc[i]
-    var2 = df2[var].iloc[i]
-    if var1 == 0 and var2 == 0:
+    var2 = df2[var].iloc[i] 
+    larger_var = max(var1, var2)
+    smaller_var = min(var1, var2)
+    if smaller_var == 0 and larger_var == 0:
       p_diff = 0
-      z_scores.append(0)
-    else:
-      p_diff = (var1 - var2)/var1
       z_score = 0
-    diff2.append(p_diff)
-  return diff2
+    #elif smaller_var == 0 and larger_var != 0:
+    #  p_diff = (larger_var - smaller_var)/ smaller_var
+    #  z_score = (larger_var - smaller_var) / smal
+    else:
+      p_diff = (larger_var - smaller_var)/larger_var
+      err = np.sqrt(larger_var)
+      z_score = (larger_var - smaller_var)/err #to be added
+    z_scores.append(z_score)
+    diff.append(p_diff)
+  return diff, z_scores
 
 def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name, args, state):
   #truncated df2 and df3 to df1 (this assumes df1 is the shortest)
   truncated_df2 = get_equal_len_df(df2, df1)
   truncated_df3 = get_equal_len_df(df3, df1)
+
+  #This uses df2 as the reference!!!!!!!
   rmse2 = get_rmse(df1, truncated_df2, var)
   rmse3 = get_rmse(truncated_df2, truncated_df3, var)
   if rmse2 > 0.1 or rmse3 > 0.1:
-    diff_df2 = get_percent_diff(df1, truncated_df2, var)
-    diff_df3 = get_percent_diff(df1, truncated_df3, var)
-    plt.plot(df1.index.values, diff_df2, color = 'green')
+    diff_df2, z_df2 = get_compare_metrics(df1, truncated_df2, var)
+    diff_df3, z_df3 = get_compare_metrics(df1, truncated_df3, var)
+    '''
+    #plt.plot(df1.index.values, diff_df2, color = 'green')
     #plt.plot(df1.index.values, diff_df3, color = 'yellow')
+    plt.plot(df1.index.values, z_df2, color = 'green')
+    plt.plot(df1.index.values, z_df3, color = 'yellow')
     plt.xlabel('Date')
     plt.ylabel('Percent Difference')
     plt.savefig(args.output_dir + '/' + state + '_percentdiff.pdf')
@@ -71,7 +79,6 @@ def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name, args, state):
     plt.xlabel(args.updated_date_name)
     plt.ylabel(var)
     print('HERE')
-    #percent_diff1 = get_percent_diff(df1, df2, var)
     plt.plot(df1.index.values, df1[var], color = 'orange', label = df1_name + ' NYT, RMSE: ' + str(rmse2), markersize = 15, marker = '.', alpha = 0.5)
     plt.plot(df2.index.values, df2[var],  color = 'blue', label = df2_name + ' NYT, RMSE: ' + str(rmse3), markersize = 8, marker = '*', alpha = 0.5)
     plt.plot(df3.index.values, df3[var], color = 'purple', label = df3_name + ' NYT: RMSE: ' + str(rmse3), markersize = 8, marker = 'd', alpha = 0.5)
@@ -80,6 +87,39 @@ def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name, args, state):
     plt.legend(loc = 'upper left')
     plt.grid(True)
     plt.savefig(args.output_dir + '/' + state + '_raw_' +  var + '_compare.png', bbox_inches='tight')
+    plt.close('all')
+    '''
+
+    fig, ax = plt.subplots(3,1, sharex = True)
+    fig.subplots_adjust(hspace = 0)
+    markersize1 = 8
+    markersize2 = 10
+    markersize3 = 4
+    markerstyle1 = '.'
+    markerstyle2 = '.'
+    markerstyle3 = '.'
+    color1 = 'blue'
+    color2 = 'orange'
+    color3 = 'purple'
+    ax[0].plot(df1.index.values, df1[var], color = color1, label = df1_name + ' NYT, RMSE: ' + str(rmse2), markersize = markersize1, marker = markerstyle1, alpha = 0.5)
+    ax[0].plot(df3.index.values, df3[var], color = color3, label = df3_name + ' NYT: RMSE: ' + str(rmse3), markersize = markersize3, marker = markerstyle3, alpha = 0.5)
+    ax[0].plot(df2.index.values, df2[var],  color = color2, label = df2_name + ' NYT', markersize = markersize2, marker = markerstyle2, alpha = 0.5)
+    ax[0].set(ylabel = var)
+    ax[0].grid(True)
+
+    ax[1].plot(df1.index.values, diff_df2, color = color2, markersize = markersize2, marker = markerstyle2, alpha = 0.5)
+    ax[1].plot(df1.index.values, diff_df3, color = color3, markersize = markersize3, marker = markerstyle3, alpha = 0.5)
+    ax[1].set(ylabel = 'Percent Difference')
+    ax[1].grid(True)
+
+    ax[2].plot(df1.index.values, z_df2, color = color2, markersize = markersize2, marker = markerstyle2, alpha = 0.5)
+    ax[2].plot(df1.index.values, z_df3, color = color3, markersize = markersize3, marker = markerstyle3, alpha = 0.5)
+    ax[2].set(ylabel='Z Score')
+    ax[2].grid(True)
+    plt.xticks(rotation=30)
+    ax[0].legend(loc = 'upper left')
+    #plt.grid(True)
+    plt.savefig(args.output_dir + '/' + state + '_compare.pdf', bbox_inches = 'tight')
     plt.close('all')
 
 def compare_county_state_plot(var, df1, df2, df1_name, df2_name, args, state):
@@ -113,7 +153,7 @@ if __name__ == '__main__':
 
   #remove previous output directory if it exists
   if os.path.isdir(args.output_dir):
-    subprocess.Popen("rm -r " + args.output_dir, stdout=subprocess.PIPE)
+    subprocess.Popen("rm -r ./" + args.output_dir, stdout=subprocess.PIPE)
   os.mkdir(args.output_dir)
   if 'All' in args.states:
     #could add start and end date here Natasha
@@ -140,8 +180,8 @@ if __name__ == '__main__':
     df3_state_ag = aggregate_df(df3_state, args)
 
 
-    make_plot('new_cases', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args, state)
-    make_plot('new_deaths', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args, state)
+    make_plot('cases', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args, state)
+    make_plot('deaths', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args, state)
     #compare_county_state_plot('new_cases', df_state_ag, df_county_ag, 'County Sum', 'State', args, state)
     #compare_county_state_plot('new_deaths', df_state_ag, df_county_ag, 'County Sum', 'State', args, state)
 
