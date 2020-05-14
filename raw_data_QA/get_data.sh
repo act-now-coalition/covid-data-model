80b035c76e4d2941bb4e878dfb910f6c48d0e79b
tashwoods <tashwoods@gmail.com>
2020-05-14 14:46:05 -0500

    grabbing hash from production website and linked cached covid-data-public/data for us-counties

--- raw_data_QA/get_data.sh
+++ raw_data_QA/get_data.sh
@@ -1,6 +1,21 @@
-rm -r data
-mkdir data
-#Commit hashes to compare
+#Remove old data directory if it exists and make new one
+OUTPUT_DIR="data/"
+rm -r $OUTPUT_DIR
+mkdir $OUTPUT_DIR
+
+
+#Extract git hash of current prod version on website
+#Get json file from current production site
+WEBSITE_PROD="https://raw.githubusercontent.com/covid-projections/covid-projections/master/src/assets/data/data_url.json"
+PROD_VERSION_FILE="version.json"
+PROD_FILE=$OUTPUT_DIR"prod.version"
+HASH_FILE=$OUTPUT_DIR"hashinfo.txt"
+curl $WEBSITE_PROD > $PROD_FILE
+json_prod=$(grep -o 'http[^"]*' $PROD_FILE)$PROD_VERSION_FILE
+curl $json_prod > $HASH_FILE
+COVID_DATA_PUBLIC_MASTER_PROD=$(python get_hash.py $HASH_FILE)
+
+#The current prod site does not have the counties csv counties cached so we will use old ones here
 COVID_DATA_PUBLIC_MASTER_OLD="6f24dcb9a6b2203572c7509e506182be62117a38"
 COVID_DATA_PUBLIC_MASTER_NOW="f2fb45e3e467fd3737865b3aef29e282b22f03d6"
 
