3f54e791994c92c4eb9ce3412c03668828d0b76d
Chris Kelly <ghop02@gmail.com>
2020-05-14 23:23:55 -0400

    Run black on pyseir to 100 char length lines (#409)
    * Run black on pyseir to 100 char length lines
    
    * run black on pyseir again
    
    * Remove pyseir exclusion

--- pyseir/inference/whitelist_generator.py
+++ pyseir/inference/whitelist_generator.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from pyseir.utils import get_run_artifact_path, RunArtifact
 from pandarallel import pandarallel
 
-VISIBIBLE_PROGRESS_BAR = os.environ.get('PYSEIR_VERBOSITY')=='True'
+VISIBIBLE_PROGRESS_BAR = os.environ.get("PYSEIR_VERBOSITY") == "True"
 pandarallel.initialize(progress_bar=VISIBIBLE_PROGRESS_BAR)
 
 
@@ -28,12 +28,10 @@ class WhitelistGenerator:
         Minimum number of nonzero death datapoints in the time series to allow
         display.
     """
+
     def __init__(
-            self,
-            total_cases=50,
-            total_deaths=0,
-            nonzero_case_datapoints=5,
-            nonzero_death_datapoints=0):
+        self, total_cases=50, total_deaths=0, nonzero_case_datapoints=5, nonzero_death_datapoints=0
+    ):
         self.county_metadata = load_data.load_county_metadata()
         self.df_whitelist = None
 
@@ -50,27 +48,30 @@ class WhitelistGenerator:
         -------
         df: whitelist
         """
-        logging.info('Generating county level whitelist...')
+        logging.info("Generating county level whitelist...")
 
         # parallel load and compute
         df_candidates = self.county_metadata.fips.parallel_apply(_whitelist_candidates_per_fips)
 
         # join extra data
         df_candidates = df_candidates.merge(
-            self.county_metadata[['fips', 'state','county']],
-            left_on='fips', right_on='fips', how='inner')
+            self.county_metadata[["fips", "state", "county"]],
+            left_on="fips",
+            right_on="fips",
+            how="inner",
+        )
 
-        df_whitelist = df_candidates[['fips', 'state', 'county']]
-        df_whitelist.loc[:, 'inference_ok'] = (
-                  (df_candidates.nonzero_case_datapoints >= self.nonzero_case_datapoints)
-                & (df_candidates.nonzero_death_datapoints >= self.nonzero_death_datapoints)
-                & (df_candidates.total_cases >= self.total_cases)
-                & (df_candidates.total_deaths >= self.total_deaths)
+        df_whitelist = df_candidates[["fips", "state", "county"]]
+        df_whitelist.loc[:, "inference_ok"] = (
+            (df_candidates.nonzero_case_datapoints >= self.nonzero_case_datapoints)
+            & (df_candidates.nonzero_death_datapoints >= self.nonzero_death_datapoints)
+            & (df_candidates.total_cases >= self.total_cases)
+            & (df_candidates.total_deaths >= self.total_deaths)
         )
 
         output_path = get_run_artifact_path(
-            fips='06', # Dummy fips since not used here...
-            artifact=RunArtifact.WHITELIST_RESULT)
+            fips="06", artifact=RunArtifact.WHITELIST_RESULT  # Dummy fips since not used here...
+        )
         df_whitelist.to_json(output_path)
 
         return df_whitelist
@@ -78,13 +79,14 @@ class WhitelistGenerator:
 
 def _whitelist_candidates_per_fips(fips):
     times, observed_new_cases, observed_new_deaths = load_data.load_new_case_data_by_fips(
-        fips, t0=datetime(day=1, month=1, year=2020))
+        fips, t0=datetime(day=1, month=1, year=2020)
+    )
 
     record = dict(
         fips=fips,
         total_cases=observed_new_cases.sum(),
         total_deaths=observed_new_deaths.sum(),
         nonzero_case_datapoints=np.sum(observed_new_cases > 0),
-        nonzero_death_datapoints=np.sum(observed_new_deaths > 0)
+        nonzero_death_datapoints=np.sum(observed_new_deaths > 0),
     )
     return pd.Series(record)
