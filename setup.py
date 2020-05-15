40c7211ddbcae74bacb3be5bd11073c2de1a08d0
Chris Kelly <ghop02@gmail.com>
2020-05-14 21:01:23 -0400

    Excluding pyseir + fixing other lint files

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyseir",
-    version='0.2',
-    description=(''),
-    url='https://github.com/covid-projections/covid-data-model/',
-    author='covidactnow.com',
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
+    version="0.2",
+    description=(""),
+    url="https://github.com/covid-projections/covid-data-model/",
+    author="covidactnow.com",
+    install_requires=["Click",],
+    entry_points="""
         [console_scripts]
         pyseir=pyseir.cli:entry_point
-    ''',
+    """,
     package_data={
-              # Package data suffixes to include when installing from git:
-              '': ['*.txt', '*.rst', '*.yaml', '*.csv']
-      },
+        # Package data suffixes to include when installing from git:
+        "": ["*.txt", "*.rst", "*.yaml", "*.csv"]
+    },
     packages=find_packages(),
 )
