17ae77d12385b61457834190d8946699c4913d47
tashwoods <tashwoods@gmail.com>
2020-05-14 22:46:08 -0500

    auto format get_hash.py

--- raw_data_QA/get_hash.py
+++ raw_data_QA/get_hash.py
@@ -1,9 +1,7 @@
 import json, sys
 
-if __name__ == '__main__':
-  inputfile = sys.argv[1]
-  f = open(inputfile)
-  data = json.load(f)
-  print(data['covid-data-public']['hash'])
-
-
+if __name__ == "__main__":
+    inputfile = sys.argv[1]
+    f = open(inputfile)
+    data = json.load(f)
+    print(data["covid-data-public"]["hash"])
