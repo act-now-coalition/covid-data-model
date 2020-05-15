b09a53397a68e589b9868f5747a69f5b7c4d84e7
Chris Kelly <ghop02@gmail.com>
2020-05-14 21:09:21 -0400

    adding black linter (#399)
    * adding black linter
    
    * Excluding pyseir + fixing other lint files

--- cli/run_counties_api.py
+++ cli/run_counties_api.py
@@ -12,27 +12,16 @@ PROD_BUCKET = "data.covidactnow.org"
 
 @click.command("deploy-counties-api")
 @click.option(
-    "--disable-validation",
-    "-dv",
-    is_flag=True,
-    help="Run the validation on the deploy command",
+    "--disable-validation", "-dv", is_flag=True, help="Run the validation on the deploy command",
 )
 @click.option(
-    "--input-dir",
-    "-i",
-    default="results",
-    help="Input directory of county projections",
+    "--input-dir", "-i", default="results", help="Input directory of county projections",
 )
 @click.option(
-    "--output",
-    "-o",
-    default="results/output/counties",
-    help="Output directory for artifacts",
+    "--output", "-o", default="results/output/counties", help="Output directory for artifacts",
 )
 @click.option(
-    "--summary-output",
-    default="results/output",
-    help="Output directory for county summaries.",
+    "--summary-output", default="results/output", help="Output directory for county summaries.",
 )
 def deploy_counties_api(disable_validation, input_dir, output, summary_output):
     """The entry function for invocation"""
@@ -44,28 +33,19 @@ def deploy_counties_api(disable_validation, input_dir, output, summary_output):
 
     for intervention in list(Intervention):
         county_result = api_pipeline.run_projections(
-            input_dir,
-            AggregationLevel.COUNTY,
-            intervention,
-            run_validation=not disable_validation,
+            input_dir, AggregationLevel.COUNTY, intervention, run_validation=not disable_validation,
         )
-        county_summaries, county_timeseries = api_pipeline.generate_api(
-            county_result, input_dir
-        )
+        county_summaries, county_timeseries = api_pipeline.generate_api(county_result, input_dir)
         api_pipeline.deploy_results([*county_summaries, *county_timeseries], output)
 
-        counties_summary = api_pipeline.build_counties_summary(
-            county_summaries, intervention
-        )
+        counties_summary = api_pipeline.build_counties_summary(county_summaries, intervention)
         counties_timeseries = api_pipeline.build_counties_timeseries(
             county_timeseries, intervention
         )
         summarized_timeseries = api_pipeline.build_prediction_header_timeseries_data(
             counties_timeseries
         )
-        api_pipeline.deploy_prediction_timeseries_csvs(
-            summarized_timeseries, summary_output
-        )
+        api_pipeline.deploy_prediction_timeseries_csvs(summarized_timeseries, summary_output)
 
         api_pipeline.deploy_results([counties_summary], summary_output, write_csv=True)
         api_pipeline.deploy_results([counties_timeseries], summary_output)
@@ -75,16 +55,10 @@ def deploy_counties_api(disable_validation, input_dir, output, summary_output):
 
 @click.command("county-fips-summaries")
 @click.option(
-    "--input-dir",
-    "-i",
-    default="results",
-    help="Input directory of county projections",
+    "--input-dir", "-i", default="results", help="Input directory of county projections",
 )
 @click.option(
-    "--output",
-    "-o",
-    default="results/county_summaries",
-    help="Output directory for artifacts",
+    "--output", "-o", default="results/county_summaries", help="Output directory for artifacts",
 )
 def county_fips_summaries(input_dir, output):
     """Generates summary files by state and globally of counties with model output data."""
