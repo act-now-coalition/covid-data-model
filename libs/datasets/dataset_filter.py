41f4cca0a48cb2cacbe6fa597cf33794151e9955
Tom Brown <tombrown@lyft.com>
2020-05-17 11:30:55 -0700

    replace DatasetFilter.__init__ with pydantic dataclass. add timeseries_test.py. rm get_subset county= param that seems unused

--- libs/datasets/dataset_filter.py
+++ libs/datasets/dataset_filter.py
@@ -1,22 +1,19 @@
-from typing import Union
+from typing import Union, List
+
+from pydantic.dataclasses import dataclass
+
 from libs.datasets.dataset_utils import AggregationLevel
 from libs.datasets.timeseries import TimeseriesDataset
 from libs.datasets.latest_values_dataset import LatestValuesDataset
 
 
+@dataclass
 class DatasetFilter(object):
     """Defines a filter to apply to a dataset."""
 
-    def __init__(self, aggregation_level: AggregationLevel = None, **filters):
-        """
-        Args:
-            aggregation_level: AggregationLevel of dataset
-            filters: Filters to apply to dataset.  Must match arguments of the respective
-                `get_subset` function.
-        """
-        self.aggregation_level = aggregation_level
-        self.filters = filters
+    country: str
+    states: List[str]
 
     def apply(self, dataset: Union[TimeseriesDataset, LatestValuesDataset]):
         """Applies filter to dataset by returning subset that matches filter arguments."""
-        return dataset.get_subset(self.aggregation_level, **self.filters)
+        return dataset.get_subset(aggregation_level=None, country=self.country, states=self.states)
