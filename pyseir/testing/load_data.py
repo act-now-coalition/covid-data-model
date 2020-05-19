0f87000231a0c27bad0cb81a84f2cdfc0d7a5fb9
zhang-xinyu <xinyu.zhang@grandrounds.com>
2020-05-19 14:59:30 -0600

    update load data function

--- pyseir/testing/load_data.py
+++ pyseir/testing/load_data.py
@@ -1,20 +1,33 @@
 import pandas as pd
 import numpy as np
 import enum as Enum
+import dill as pickle
+from pyseir.inference.fit_results import load_Rt_result
 
 
 def load_population_size(fips):
     """
     Load population size for a community.
     """
-    return 10000
+    return 1000
 
 def load_Rt(fips):
-    return
+    Rt = load_Rt_result(fips)
+    Rt = Rt['Rt_MAP_composite']
+    return Rt
 
 def load_projection(fips):
     """
 
     """
-    return
+    demo_output = pickle.load(open('demo_mapper_output.pkl', 'rb'))
+
+    projection = {}
+    projection.update(demo_output['compartments'])
+    for measure in demo_output:
+        if measure != 'compartments':
+            for unit in demo_output[measure]:
+                projection[measure + '__' + unit] = demo_output[measure][unit]
+
+    return projection
 
