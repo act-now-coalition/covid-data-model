e46381dbb27c77e9ee41cc3d747c96a1f78963b7
Tom Brown <tombrown@lyft.com>
2020-05-21 14:23:18 -0700

    Add tests, but don't have them passing yet

--- test/build_processed_data_test.py
+++ test/build_processed_data_test.py
@@ -1,6 +1,40 @@
-from libs import build_processed_dataset
+from unittest.mock import patch
+
+import pytest
+
+from libs import build_processed_dataset, validate_results
+from libs.datasets import CommonFields
+import pandas as pd
+
+from libs.enums import Intervention
 
 
-def test_get_testing_df():
-    testing_df = build_processed_dataset.get_testing_timeseries_by_state("MA")
-    assert testing_df is not None
+def test_get_usa_by_states_df():
+    empty_df_with_state = pd.DataFrame([], columns=['state',])
+    with patch('libs.build_processed_dataset.get_state_projections_df', return_value=empty_df_with_state) as mock:
+        df = build_processed_dataset.get_usa_by_states_df(input_dir="/tmp", intervention_type=Intervention.OBSERVED_INTERVENTION.value)
+    validate_results.validate_states_df("MA", df)
+    df.set_index([CommonFields.DATE, CommonFields.STATE], inplace=True)
+    print(df.info())
+    print(df.head())
+    assert df.loc[("2020-04-01", "MA"), CommonFields.POSITIVE_TESTS] > 0
+
+
+def test_get_testing_df_by_state():
+    df = build_processed_dataset.get_testing_timeseries_by_state("MA")
+    assert CommonFields.POSITIVE_TESTS in df.columns
+    assert CommonFields.NEGATIVE_TESTS in df.columns
+    df.set_index(CommonFields.DATE, inplace=True)
+    assert df.loc["2020-04-01", CommonFields.POSITIVE_TESTS] > 0
+
+
+# Check San Francisco and Houston (Harris County, TX)
+@pytest.mark.parametrize(
+    "fips", ["06075", "48201"],
+)
+def test_get_testing_by_fips(fips):
+    df = build_processed_dataset.get_testing_timeseries_by_fips(fips)
+    assert CommonFields.POSITIVE_TESTS in df.columns
+    assert CommonFields.NEGATIVE_TESTS in df.columns
+    df.set_index(CommonFields.DATE, inplace=True)
+    assert df.loc["2020-04-01", CommonFields.POSITIVE_TESTS] > 0
--- test/combined_dataset_test.py
+++ test/combined_dataset_test.py
@@ -1,3 +1,6 @@
+import logging
+import re
+
 import pytest
 
 from libs.datasets import combined_datasets
@@ -31,10 +34,36 @@ def test_unique_index_values_us_latest():
     [JHUDataset, CDSDataset, CovidTrackingDataSource, NevadaHospitalAssociationData,],
 )
 def test_unique_timeseries(data_source_cls):
-
     data_source = data_source_cls.local()
     timeseries = TimeseriesDataset.build_from_data_source(data_source)
     timeseries = combined_datasets.US_STATES_FILTER.apply(timeseries)
     timeseries_data = timeseries.data.set_index(timeseries.INDEX_FIELDS)
-    duplicates = timeseries_data.index.duplicated()
-    assert not sum(duplicates)
+    duplicates = timeseries_data.index.duplicated(keep=False)
+    assert not sum(duplicates), str(timeseries_data.loc[duplicates])
+
+
+@pytest.mark.parametrize(
+    "data_source_cls",
+    [JHUDataset, CDSDataset, CovidTrackingDataSource, NevadaHospitalAssociationData, ],
+)
+def test_expected_field_in_sources(data_source_cls):
+    data_source = data_source_cls.local()
+    # Extract the USA data from the raw DF. Replace this with cleaner access when the DataSource makes it easy.
+    rename_columns = {source: common for common, source in data_source.all_fields_map().items()}
+    renamed_data = data_source.data.rename(columns=rename_columns)
+    usa_data = renamed_data.loc[renamed_data["country"] == "USA"]
+
+    assert not usa_data.empty
+
+    states = set(usa_data["state"])
+
+    if data_source.SOURCE_NAME == "NHA":
+        assert states == {"NV"}
+    else:
+        good_state = set()
+        for state in states:
+            if re.fullmatch(r'[A-Z]{2}', state):
+                good_state.add(state)
+            else:
+                logging.info(f"Ignoring {state} in {data_source.SOURCE_NAME}")
+        assert len(good_state) >= 48
