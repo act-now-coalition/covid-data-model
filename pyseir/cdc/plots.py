1d7fdefe2caf6f06f15b6891e22d06dc95d37405
zhang-xinyu <xinyu.zhang@grandrounds.com>
2020-05-25 09:33:24 -0500

    update plots.py

--- pyseir/cdc/plots.py
+++ pyseir/cdc/plots.py
@@ -1,57 +1,97 @@
-from pyseir.fit_results import load_mle_model, load_inference_result
+from pyseir.inference.fit_results import load_mle_model, load_inference_result
 from pyseir import load_data
 import pandas as pd
 from pyseir.utils import REF_DATE
 import numpy as np
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+from matplotlib.backends import backend_pdf
 import us
 import matplotlib
 from pyseir.cdc.utils import Target
+from pyseir.cdc.output_mapper import OutputMapper, REPORT_FOLDER, TEAM, MODEL, FORECAST_DATE, DATE_FORMAT
+from collections import defaultdict
+from epiweeks import Week, Year
+
+LABELS = {Target.CUM_DEATH: 'cumulative death',
+          Target.INC_DEATH: 'incident death'}
+
+
+def aggregate_by_week(dates, data):
+    """
+
+    """
+
+    saturdays = np.array([d + timedelta((12 - d.weekday()) % 7) for d in dates])
+    df = pd.DataFrame({'data': data, 'saturdays': saturdays})
+    df = df.groupby('saturdays')['data'].sum().reset_index()
+
+    return df['saturdays'].values, df['data'].values
+
 
 def plot_results(fips, forecast_date, target, pdf):
+    """
+
+    """
+
     times, _, death = load_data.load_new_case_data_by_state(fips, t0=REF_DATE)
-    df = pd.read_csv(f'output/pyseir/cdc/{forecast_date}_CovidActNow_SEIR_CAN_{fips}.csv')
-    df = df[df.target.str.contains(f'day ahead {target.value}')]
+    df = pd.read_csv(f'{REPORT_FOLDER}/{forecast_date}_{TEAM}_{MODEL}_{fips}.csv')
 
-    results = list()
+    results = defaultdict(list)
 
-    for target_name in df.target.unique():
-        sub_df = df[df.target == target_name]
-        sub_df['quantile'] = sub_df['quantile'].apply(lambda x: float(x))
-        sub_df['pdf'] = np.append(sub_df['quantile'].values[0], np.diff(sub_df['quantile']))
-        results.append(pd.DataFrame({
-            'target_end_date': [sub_df.iloc[0]['target_end_date']],
-            'expected': [(sub_df['pdf'] * sub_df['value']).sum()],
-            'ci_025': [float(sub_df[sub_df['quantile'] == 0.025]['value'])],
-            'ci_975': [float(sub_df[sub_df['quantile'] == 0.975]['value'])]}))
+    for unit in ['day', 'wk']:
+        df_by_unit = df[df.target.str.contains(f'{unit} ahead {target.value}')].copy()
+        for target_name in df_by_unit.target.unique():
+            sub_df = df_by_unit[df_by_unit.target == target_name]
+            sub_df['quantile'] = sub_df['quantile'].apply(lambda x: float(x))
+            sub_df['pdf'] = np.append(sub_df['quantile'].values[0], np.diff(sub_df['quantile']))
+            results[unit].append(pd.DataFrame({
+                'target_end_date': [sub_df.iloc[0]['target_end_date']],
+                'expected': [(sub_df['pdf'] * sub_df['value']).sum()],
+                'ci_025': [float(sub_df[sub_df['quantile'] == 0.025]['value'])],
+                'ci_975': [float(sub_df[sub_df['quantile'] == 0.975]['value'])]}))
 
-    results = pd.concat(results)
-    results['target_end_date'] = pd.to_datetime(results['target_end_date'])
+    for unit in ['day', 'wk']:
+        results[unit] = pd.concat(results[unit])
+        results[unit]['target_end_date'] = pd.to_datetime(results[unit]['target_end_date'])
 
-    plt.figure()
-    if target is Target.CUM_DEATH:
-        plt.plot([REF_DATE + timedelta(t) for t in times], death.cumsum(), label='observed')
-    plt.plot(results['target_end_date'], results['mean'])
-    plt.fill_between(x=results['target_end_date'],
-                     y1=results['ci_025'], y2=results['ci_975'],
-                     label='CI_95', alpha=0.3)
-    plt.legend()
+    plt.figure(figsize=(10, 6))
+    for n, unit in enumerate(['day', 'wk']):
+        plt.subplot(1, 2, n+1)
+        if target is Target.CUM_DEATH:
+            plt.plot([REF_DATE + timedelta(t) for t in times], death.cumsum(), label='observed')
+        elif target is Target.INC_DEATH:
+            if unit  == 'day':
+                plt.plot([REF_DATE + timedelta(t) for t in times], death, label='observed')
+            elif unit == 'wk':
+                dates, values = aggregate_by_week([REF_DATE + timedelta(t) for t in times], death)
+                plt.plot(dates, values, label='observed')
 
-    plt.title('\n'.join([us.states.lookup(fips).name, fips, 'cumulative death']))
+        plt.plot(results[unit]['target_end_date'], results[unit]['expected'], marker='o')
+        plt.fill_between(x=results[unit]['target_end_date'],
+                         y1=results[unit]['ci_025'],
+                         y2=results[unit]['ci_975'],
+                         label=f'CI_95',
+                         alpha=0.3)
+        plt.ylabel(f'{unit} ahead forecast')
+        plt.xlabel('time')
+        plt.legend()
+        plt.xticks(rotation=45)
 
+
+    plt.suptitle('/'.join([us.states.lookup(fips).name, fips, LABELS[target]]))
     plt.tight_layout()
+    plt.subplots_adjust(top=0.88)
     pdf.savefig()
+
 
 def run_all():
     df_whitelist = load_data.load_whitelist()
     df_whitelist = df_whitelist[df_whitelist['inference_ok'] == True]
-    fips_list = list(df_whitelist['fips'].str[:2].unique())
-
-    om = OutputMapper(fips='06')
-    for target in
-    output_path = 'output/pyseir/cdc/report_cum_death.pdf'
-    pdf = matplotlib.backends.backend_pdf.PdfPages(output_path)
+    fips_list = list(df_whitelist['fips'].str[:2].unique())[:5]
+    output_path = f'{REPORT_FOLDER}/report_{datetime.strftime(FORECAST_DATE, DATE_FORMAT)}.pdf'
+    pdf = backend_pdf.PdfPages(output_path)
     for fips in fips_list:
-        plot_results(fips, forecast_date, target, pdf)
+        for target in [Target.CUM_DEATH, Target.INC_DEATH]:
+            plot_results(fips, datetime.strftime(FORECAST_DATE, DATE_FORMAT), target, pdf)
     pdf.close()
