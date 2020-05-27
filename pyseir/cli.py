4f1b0d28ffe6fa5f5be2a0e64f4b283d310d6bbb
Chris Kelly <ghop02@gmail.com>
2020-05-26 21:03:27 -0400

    Default Dataset caching to enabled and improve logging (#444)

--- pyseir/cli.py
+++ pyseir/cli.py
@@ -59,7 +59,7 @@ def _cache_global_datasets():
 @click.group()
 def entry_point():
     """Basic entrypoint for cortex subcommands"""
-    dataset_cache.set_pickle_cache_tempdir()
+    dataset_cache.set_pickle_cache_dir()
     sentry_sdk.init(os.getenv("SENTRY_DSN"))
     structlog.configure(
         processors=[
