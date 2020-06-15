2c7b8cd292190ba0f33c11c7664a8858365bac6b
Brett Boval <brettboval@gmail.com>
2020-06-15 13:39:24 -0700

    For Future Projections, Cap All Scenarios at R=2.5 (#493)
    * Cap Suppression Policy R No Intervention at 2.5
    
    This commit sets the upper bound for the hospitalized projection at 2.5. This will stabilize the outputted no intervention estimate, since almost all states have a value greater than 2.5.
    
    * Also Apply Cap to R_eff2

--- pyseir/models/suppression_policies.py
+++ pyseir/models/suppression_policies.py
@@ -22,28 +22,39 @@ distancing_measure_suppression = {
 }
 
 
-def get_future_suppression_from_r0(R0, scenario):
+def estimate_future_suppression_from_fits(inferred_params, scenario):
     """
-    Returns the future suppression level for a given R0 and a "future scenario".
+    Returns the future suppression level for a given R0, R_eff2 and a "future scenario". The
+    "no_intervention" scenario is capped at an effective R rate of 2.5 as suggested by the CDC
+    planning scenarios (https://www.cdc.gov/coronavirus/2019-ncov/hcp/planning-scenarios.html).
 
     Parameters
     ----------
-    R0:float
-        Reproduction number
+    inferred_params: dict
+        Fit results including R0 and eps2 as keys
     scenario: str
         'no_intervention', 'flatten_the_curve', 'social_distancing'.
 
     Returns
     -------
     epsilon: float
-        Suppression fraction.
+        Suppression fraction compared to R0.
     """
-    if scenario == "no_intervention":
-        return 1
+    CDC_MAX = 2.5  # https://www.cdc.gov/coronavirus/2019-ncov/hcp/planning-scenarios.html
+    FLATTEN_THE_CURVE = 0.97
+    SOCIAL_DISTANCING = 1.7
+
+    R0 = inferred_params["R0"]
+    R_eff2 = inferred_params["eps2"] * R0
+
+    if scenario == "inferred":
+        return min(R_eff2, CDC_MAX) / R0
+    elif scenario == "no_intervention":
+        return min(R0, CDC_MAX) / R0
     elif scenario == "flatten_the_curve":
-        return 0.97 / R0
+        return FLATTEN_THE_CURVE / R0
     elif scenario == "social_distancing":
-        return 1.7 / R0
+        return SOCIAL_DISTANCING / R0
     else:
         raise ValueError(f"Suppression {scenario} not valid")
 
