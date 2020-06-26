73475028f778170acc17d34e0151aa72bd78a069
Chris Kelly <ghop02@gmail.com>
2020-06-26 12:54:38 -0500

    Check for unique values

--- libs/datasets/sources/texas_hospitalizations.py
+++ libs/datasets/sources/texas_hospitalizations.py
@@ -9,10 +9,6 @@ class TexasHospitalizations(data_source.DataSource):
     DATA_PATH = "data/states/tx/tx_fips_hospitalizations.csv"
     SOURCE_NAME = "tx_hosp"
 
-    # CMDC data reports values at both the state and county level. However, state values
-    # are not reported until complete values from states are received.  The latest
-    # row may contain some county data but not state data - instead of aggregating and returning
-    # incomplete state data, we are choosing to not aggregate.
     FILL_MISSING_STATE_LEVEL_DATA = False
 
     INDEX_FIELD_MAP = {
