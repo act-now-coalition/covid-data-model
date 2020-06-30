fb6c59d645cdf681927a5cfbe224b538df27f518
Tom Brown <tomgith4@thecap.org>
2020-06-30 16:53:23 -0700

    better test name

--- test/us_state_abbrev_test.py
+++ test/us_state_abbrev_test.py
@@ -6,7 +6,7 @@ from libs.us_state_abbrev import (
 )
 
 
-def test():
+def test_state_name_fips_abbrev_maps():
     assert US_STATE_ABBREV["Wisconsin"] == "WI"
     assert abbrev_us_state["WI"] == "Wisconsin"
     assert ABBREV_US_FIPS["TX"] == "48"
