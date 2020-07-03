d3d6ff1478c69b745acc8e1d7b959fc66e41bb59
Chris Kelly <ghop02@gmail.com>
2020-07-02 19:10:39 -0500

    Working tests!

--- test/api/test_can_predictions.py
+++ test/api/test_can_predictions.py
@@ -1,11 +1,11 @@
 from datetime import date
-from api.can_api_definition import CovidActNowAreaTimeseries
+from api.can_api_definition import CovidActNowBulkTimeseries
 
 from libs import dataset_deployer
 
 
 def test_remove_root():
-    value = CovidActNowCountiesTimeseries(__root__=[]).dict()
+    value = CovidActNowBulkTimeseries(__root__=[]).dict()
     assert value == {"__root__": []}
     result = dataset_deployer.remove_root_wrapper(value)
     assert result == []
