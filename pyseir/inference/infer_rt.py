e25c5aeb09be8f8a3b9c35dae51ffc1a0622cfb3
tashwoods <tashwoods@gmail.com>
2020-07-07 14:00:27 -0500

    truly remove extra calls in infer_rt.py

--- pyseir/inference/infer_rt.py
+++ pyseir/inference/infer_rt.py
@@ -895,7 +895,7 @@ class RtInferenceEngine:
         # df_all.to_csv("df_all_" + self.display_name + ".csv")
 
         # log.info("run forecast")
-        ForecastRt.run_forecast(df_all)
+        # ForecastRt.run_forecast(df_all)
         # log.info("done running forecast")
         return df_all
 
