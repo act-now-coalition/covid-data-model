8d32aa03c7b4fa28e3c367dba439b7dab0f3b059
Chris Kelly <ghop02@gmail.com>
2020-07-11 12:46:55 -0400

    Add github action to update dataset snapshot

---
+++ tools/push-data-update.sh
@@ -0,0 +1,6 @@
+now=$(date)
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add -A dataset_pointers/
+git commit -m "Update pointers to persisted datasets at $now"
+git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
