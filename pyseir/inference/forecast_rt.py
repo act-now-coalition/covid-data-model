2be7b56d9c87563d7b394000f0df94531de410c6
tashwoods <tashwoods@gmail.com>
2020-07-13 16:42:04 -0500

    run 1000 epochs

--- pyseir/inference/forecast_rt.py
+++ pyseir/inference/forecast_rt.py
@@ -81,7 +81,7 @@ class ForecastRt:
         self.train_size = 0.8
         self.n_test_days = 10
         self.n_batch = 1
-        self.n_epochs = 1
+        self.n_epochs = 1000
         self.n_hidden_layer_dimensions = 100
         self.dropout = 0
         self.patience = 50
