3dc6538bbcd6000cc3f1ac37b111271d5d55105b
Xinyu Zhang <xinyu.zhang@grandrounds.com>
2020-07-12 21:41:40 -0500

    update targets and units

--- pyseir/cdc/parameters.py
+++ pyseir/cdc/parameters.py
@@ -7,19 +7,32 @@ TEAM = 'CovidActNow'
 MODEL = 'SEIR_CAN'
 
 # type of target measures
-TARGETS = ['cum death', 'inc death', 'inc hosp']
+TARGETS_AND_UNITS = [('cum death', 'wk'),
+                     ('inc death', 'wk'),
+                     ('cum death', 'day'),
+                     ('inc death', 'day'),
+                     ('inc case', 'wk'),
+                     ('inc case', 'day'),
+                     ('inc hosp', 'day'),
+                     ('inc hosp', 'wk')]
+
+# type of target measures
+TARGETS_AND_UNITS_REPORT = [('cum death', 'wk'),
+                            ('inc death', 'wk'),
+                            ('inc case', 'wk'),
+                            ('inc hosp', 'wk')]
 
 # names of target measures that will be used to generate metadata
 TARGETS_TO_NAMES = {'cum death': 'cumulative deaths',
                     'inc death': 'incident deaths',
+                    'inc case': 'incident confirmed cases',
                     'inc hosp': 'incident hospitalizations'}
 
-# units of forecast target.
-FORECAST_TIME_UNITS = ['day', 'wk']
 # number of weeks ahead for forecast.
-FORECAST_WEEKS_NUM = 4
+FORECAST_WEEKS_NUM = 6
 # Default quantiles required by CDC.
-QUANTILES = np.concatenate([[0.01, 0.025], np.arange(0.05, 1, 0.05), [0.975, 0.99]])
+QUANTILES_STATES = np.concatenate([[0.01, 0.025], np.arange(0.05, 1, 0.05), [0.975, 0.99]])
+QUANTILES_COUNTIES = np.concatenate([[0.01, 0.025], np.arange(0.05, 1, 0.05), [0.975, 0.99]])
 # Time of forecast, default date when this runs.
 FORECAST_DATE = datetime.today() - timedelta(days=1)
 # Next epi week. Epi weeks starts from Sunday and ends on Saturday.
@@ -37,14 +50,12 @@ DATE_FORMAT = '%Y-%m-%d'
 class Target(Enum):
     CUM_DEATH = 'cum death'
     INC_DEATH = 'inc death'
-    CUM_HOSP = 'cum hosp'
+    INC_CASE = 'inc case'
     INC_HOSP = 'inc hosp'
-
 
 class ForecastTimeUnit(Enum):
     DAY = 'day'
     WK = 'wk'
-
 
 class ForecastUncertainty(Enum):
     DEFAULT = 'default'
