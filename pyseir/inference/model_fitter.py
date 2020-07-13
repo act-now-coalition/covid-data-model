ded3f8570d6182337d794d650803e0242398a2f9
Brett <brettboval@gmail.com>
2020-07-13 15:10:29 -0700

    Fixed Test Fraction

--- pyseir/inference/model_fitter.py
+++ pyseir/inference/model_fitter.py
@@ -93,8 +93,8 @@ class ModelFitter:
         limit_t_delta_phases=[14, 100],  # good as of June 3, 2020 may need to update in the future
         error_t_delta_phases=1,
         test_fraction=0.1,
-        limit_test_fraction=[0.02, 1],
-        # fix_test_fraction=True,
+        # limit_test_fraction=[0.02, 1],
+        fix_test_fraction=True,
         error_test_fraction=0.02,
         hosp_fraction=1,
         # fix_hosp_fraction=True,
@@ -231,7 +231,7 @@ class ModelFitter:
             "eps2",
             "t_delta_phases",
             # "test_fraction",
-            # "hosp_fraction",
+            "hosp_fraction",
             "log10_I_initial",
         ]
         if self.fips in overwrite_params_df["fips"].values:
