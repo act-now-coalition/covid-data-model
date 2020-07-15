44f16c4e9c8a4114450fdc88f7009f3bb4badecf
Chris Kelly <ghop02@gmail.com>
2020-07-15 10:06:04 -0400

    Update data for 07/15 and fix script to be executable for data update

--- tools/push-data-update.sh
+++ tools/push-data-update.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 now=$(date)
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
-git add -A dataset_pointers/
+git add -A data
 git commit -m "Update pointers to persisted datasets at $now"
 git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
