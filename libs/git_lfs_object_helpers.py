d5041cf31b79969bcabdfcaeb7259ddefb01d653
Chris Kelly <ghop02@gmail.com>
2020-07-15 18:36:42 -0400

    Adding a print statement

--- libs/git_lfs_object_helpers.py
+++ libs/git_lfs_object_helpers.py
@@ -42,6 +42,7 @@ def find_commit(
 
     Returns: Commit if matching commit found.
     """
+    print(list(repo.iter_commits(paths=path)))
     if previous_commit:
         commit_iterator = repo.iter_commits(paths=path)
         _ = next(commit_iterator)
