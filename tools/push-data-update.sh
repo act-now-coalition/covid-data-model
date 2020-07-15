abbe66a79a3b0e098e0e6e454cc3d48750575ddf
Chris Kelly <ghop02@gmail.com>
2020-07-15 10:20:37 -0400

    Update git comment

--- tools/push-data-update.sh
+++ tools/push-data-update.sh
@@ -4,5 +4,5 @@ now=$(date)
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add -A data
-git commit -m "Update pointers to persisted datasets at $now"
+git commit -m "Updating saved datasets at $now"
 git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
