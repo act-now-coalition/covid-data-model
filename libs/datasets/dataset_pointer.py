6bf6b3a6c877e8aff185fb82773c8bc1789017df
Chris Kelly <ghop02@gmail.com>
2020-07-17 13:12:38 -0400

    Adding ability to load previous persisted datasets from git lfs history (#552)
    * Adding ability to load previous persisted datasets from git lfs history
    
    * Adding a print statement
    
    * remove test for previous commit
    
    * lint
    
    * adding ability to choose a specific commit
    
    * Remove pointer check
    
    * Renaming to reflect behavior; adding support for buffers or path in load_csv
    
    * Remove stale comment

--- libs/datasets/dataset_pointer.py
+++ libs/datasets/dataset_pointer.py
@@ -1,20 +1,14 @@
 from typing import Type, Tuple
 import os
-import tempfile
+import io
 import pathlib
 import datetime
 
+import git
 import structlog
-import numpy as np
-import pandas as pd
 import pydantic
-from covidactnow.datapublic import common_df
 
-from libs.datasets import dataset_base
-from libs.datasets import combined_datasets
-from libs.datasets import timeseries
-from libs.datasets import latest_values_dataset
-from libs.datasets import dataset_utils
+from libs import git_lfs_object_helpers
 from libs.datasets.dataset_base import DatasetBase
 from libs.datasets.dataset_utils import DatasetType
 from libs.github_utils import GitSummary
@@ -51,7 +45,27 @@ class DatasetPointer(pydantic.BaseModel):
         _logger.info("Successfully saved dataset", path=str(self.path))
         return self.path
 
-    def load_dataset(self) -> DatasetBase:
+    def load_dataset(
+        self, before: str = None, previous_commit: bool = False, commit: str = None
+    ) -> DatasetBase:
+        """Load dataset from file specified by pointer.
+
+        If options are specified, will load from git history of the file.
+
+        Args:
+            before: If set, returns dataset from first commit for file before date.
+            previous_commit: If true, returns the dataset from previous commit.
+            commit: SHA of specific commit.
+
+        Returns: Instantiated dataset.
+        """
+        if before or previous_commit or commit:
+            lfs_data = git_lfs_object_helpers.get_data_for_path(
+                self.path, before=before, previous_commit=previous_commit, commit=commit
+            )
+            lfs_buf = io.BytesIO(lfs_data)
+            return self.dataset_type.dataset_class.load_csv(lfs_buf)
+
         return self.dataset_type.dataset_class.load_csv(self.path)
 
     def save(self, directory: pathlib.Path) -> pathlib.Path:
