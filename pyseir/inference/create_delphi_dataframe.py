f0004905a9ba29d1445dbe44a04b5482e2399e23
tashwoods <tashwoods@gmail.com>
2020-07-17 15:55:10 -0500

    add valid aws-lake data to merged_delphi_df

--- pyseir/inference/create_delphi_dataframe.py
+++ pyseir/inference/create_delphi_dataframe.py
@@ -8,40 +8,51 @@ import os, sys, glob
 from matplotlib import pyplot as plt
 import us
 import structlog
+from functools import reduce
 
-CSV_FOLDER = "/Users/natashawoods/Desktop/later.nosync/covid_act_now.nosync/covid-data-public/data/aws-lake/old_aws_lake/"
-
-# Get list of all available CSV files
-csv_files = glob.glob(CSV_FOLDER + "*csv")
-
-
-# Load google health trends data
-ght = pd.read_csv(
-    CSV_FOLDER + "timeseries-ght.csv", converters={"fips": str}, parse_dates=True, index_col="date"
+CSV_FOLDER = (
+    "/Users/natashawoods/Desktop/later.nosync/covid_act_now.nosync/covid-data-public/data/aws-lake/"
 )
 aggregate_level_name = "aggregate_level"
 aggregate_select = "state"
-ght = ght[ght[aggregate_level_name] == aggregate_select]
-ght.to_csv("ght_test.csv")
-
 
 # load CAN input data
 can_data = "/Users/natashawoods/Desktop/later.nosync/covid_act_now.nosync/covid-data-model/pyseir_data/merged_results.csv"
 can_df = pd.read_csv(can_data, converters={"fips": str}, parse_dates=True, index_col="date")
 
-# Merge dataframes
-merged_df = pd.merge(
-    can_df,
-    ght,
-    how="left",
-    left_on=["fips", "date", "aggregate_level", "state"],
-    right_on=["fips", "date", "aggregate_level", "state"],
+# Get list of all available CSV files
+csv_files = glob.glob(CSV_FOLDER + "*csv")
+print(csv_files)
+
+delphi_dataframes = []
+for delphi_file in csv_files:
+    delphi_var_df = pd.read_csv(
+        delphi_file, converters={"fips": str}, parse_dates=True, index_col="date"
+    )
+
+    delphi_var_df = delphi_var_df[delphi_var_df[aggregate_level_name] == aggregate_select]
+
+    delphi_dataframes.append(delphi_var_df)
+
+merged_df = reduce(
+    lambda left, right: pd.merge(
+        left,
+        right,
+        left_on=["fips", "date", aggregate_level_name, aggregate_select],
+        right_on=["fips", "date", aggregate_level_name, aggregate_select],
+    ),
+    delphi_dataframes,
 )
 
-merged_df["fips_int"] = merged_df["fips"].astype(int)
-merged_df.to_csv("delphi_merged.csv")
+merged_df.to_csv("merged_delphi_df.csv")
+# Merge dataframes
+# merged_df = pd.merge(
+#    can_df,
+#    delphi_var_df,
+#    how="left",
+#    left_on=["fips", "date", aggregate_level_name, aggregate_select],
+#    right_on=["fips", "date", aggregate_level_name, aggregate_select],
+# )
 
-
-print(ght)
-
-print(csv_files)
+# merged_df["fips_int"] = merged_df["fips"].astype(int)
+# merged_df.to_csv("delphi_merged.csv")
