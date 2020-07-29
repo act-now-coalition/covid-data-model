1f1bdd466e1865550f62e0650b787d3ddc1d1f71
Brett <brettboval@gmail.com>
2020-07-28 22:28:10 -0700

    Update patches.py Based on PR Feedback

--- pyseir/rt/patches.py
+++ pyseir/rt/patches.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 
-from libs.datasets.combined_datasets import load_us_latest_dataset
+from libs.datasets import combined_datasets
+from libs.datasets.combined_datasets import CommonFields
 import pyseir.utils
-
-population_dict = load_us_latest_dataset().data.set_index("fips")["population"].to_dict()
 
 
 def patch_aggregate_rt_results(fips_superset: list) -> pd.DataFrame:
@@ -26,21 +25,22 @@ def patch_aggregate_rt_results(fips_superset: list) -> pd.DataFrame:
             fips, pyseir.utils.RunArtifact.RT_INFERENCE_RESULT
         )
         tmp = pd.read_json(path)
-        tmp["population"] = population_dict[fips]
+        tmp["population"] = combined_datasets.get_us_latest_for_fips(fips)[CommonFields.POPULATION]
         return tmp
 
     combined_df = pd.concat([load_and_append_population(x) for x in fips_superset])
 
     def f(x):
-        """Helper function to apply to weighted arithmetic means"""
-        d = dict()
-        d["Rt_MAP_composite"] = np.average(x["Rt_MAP_composite"], weights=x["population"])
+        """Helper function to apply to weighted arithmetic means to groupby objects"""
+        # TODO: Decide whether Rt_ci95_composite should be changed to being combined in quadrature
+        #  http://ipl.physics.harvard.edu/wp-uploads/2013/03/PS3_Error_Propagation_sp13.pdf instead
+        #  of the population weighted arithmetic mean
 
-        # TODO: Decide whether these errors should be changed to being combined in quadrature
-        # http://ipl.physics.harvard.edu/wp-uploads/2013/03/PS3_Error_Propagation_sp13.pdf instead
-        # of the population weighted arithmetic mean
-        d["Rt_ci95_composite"] = np.average(x["Rt_ci95_composite"], weights=x["population"])
+        weighted_data = dict(
+            Rt_MAP_composite=np.average(x["Rt_MAP_composite"], weights=x["population"]),
+            Rt_ci95_composite=np.average(x["Rt_ci95_composite"], weights=x["population"]),
+        )
 
-        return pd.Series(d, index=["Rt_MAP_composite", "Rt_ci95_composite"])
+        return pd.Series(weighted_data, index=["Rt_MAP_composite", "Rt_ci95_composite"])
 
     return combined_df.groupby(combined_df.index).apply(f)
