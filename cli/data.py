8341372482b3f36759002b2ebedc0c8f31f1d12e
Chris Kelly <ghop02@gmail.com>
2020-08-06 12:54:04 -0400

    Adding ability to update google sheet with data availability report

--- cli/data.py
+++ cli/data.py
@@ -1,15 +1,19 @@
 from typing import Optional
 import logging
 import pathlib
+
 import click
+import gspread
+
+from libs import google_sheet_helpers
 from libs.qa import dataset_summary
+from libs.qa import data_availability
 from libs.datasets.timeseries import TimeseriesDataset
 from libs.datasets import dataset_utils
 from libs.datasets import combined_dataset_utils
 from libs.datasets import combined_datasets
 from libs.datasets.combined_dataset_utils import DatasetType
 from libs.datasets.dataset_utils import AggregationLevel
-
 
 PROD_BUCKET = "data.covidactnow.org"
 
@@ -52,3 +56,21 @@ def save_summary(output_dir: pathlib.Path, filename: str, level: Optional[Aggreg
         us_timeseries = us_timeseries.get_subset(aggregation_level=level)
 
     _save_field_summary(us_timeseries, output_dir / filename)
+
+
+@main.command()
+@click.option("--name", envvar="DATA_AVAILIBILITY_SHEET_NAME", default="Data Availability - Dev")
+@click.option("--share-email")
+def update_availability_report(name: str, share_email: Optional[str]):
+    sheet = google_sheet_helpers.open_or_create_spreadsheet(name, share_email=share_email)
+    google_sheet_helpers.update_info_sheet(sheet)
+    data_sources_by_source_name = data_availability.load_all_latest_sources()
+
+    for name, dataset in data_sources_by_source_name.items():
+        _logger.info(f"Updating {name}")
+        report = data_availability.build_data_availability_report(dataset)
+        data_availability.update_multi_field_availability_report(
+            sheet, report, name, columns_to_drop=["source", "fips", "generated"]
+        )
+
+    _logger.info("Finished updating data availability report")
---
+++ libs/google_sheet_helpers.py
@@ -0,0 +1,113 @@
+from typing import Optional
+import datetime
+import base64
+import os
+import tempfile
+import structlog
+import gspread
+import pathlib
+from libs.datasets import dataset_utils
+from libs.datasets import dataset_pointer
+from libs.datasets.dataset_utils import DatasetType
+from libs.datasets.dataset_pointer import DatasetPointer
+
+
+_logger = structlog.getLogger(__name__)
+
+# base 64 encoded service account json file.
+SERVICE_ACCOUNT_DATA_ENV_NAME = "GOOGLE_SHEETS_SERVICE_ACCOUNT_DATA"
+
+
+def init_client() -> gspread.Client:
+    service_account_data = os.environ[SERVICE_ACCOUNT_DATA_ENV_NAME]
+    if service_account_data:
+        _logger.info("Loading service account from env variable data.")
+        service_account_data = base64.b64decode(service_account_data)
+
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            tmp_file.write(service_account_data)
+            tmp_file.flush()
+            return gspread.service_account(filename=tmp_file.name)
+
+    return gspread.service_account()
+
+
+def create_or_replace_worksheet(
+    sheet: gspread.Spreadsheet, worksheet_name: str
+) -> gspread.Worksheet:
+    """Creates or replaces a worksheet with name `worksheet_name`.
+
+    Note(chris): Taking the approach of deleting worksheet to make sure that
+    state of worksheet is totally clean.  Other methods of clearing worksheet using gspread
+    did not clear conditional formatting rules.
+
+    Args:
+        sheet: Spreadsheet
+        worksheet_name: Name of worksheet.
+
+    Returns: Newly created Worksheet.
+    """
+    try:
+        worksheet = sheet.worksheet(worksheet_name)
+        try:
+            sheet.del_worksheet(worksheet)
+        except Exception:
+            # If worksheet name exists but is the only worksheet, need to add a new tmp sheet
+            # first then delete the old one
+            new_worksheet = sheet.add_worksheet("tmp", 100, 100)
+            sheet.del_worksheet(worksheet)
+            new_worksheet.update_title(worksheet_name)
+            return new_worksheet
+    except gspread.WorksheetNotFound:
+        pass
+
+    return sheet.add_worksheet(worksheet_name, 100, 100)
+
+
+def open_or_create_spreadsheet(
+    sheet_name: str,
+    share_email: Optional[str] = None,
+    gspread_client: Optional[gspread.Client] = None,
+) -> gspread.Spreadsheet:
+    """Opens or creates a spreadsheet, optionally sharing with `share_email`.
+
+    Args:
+        sheet_name: Name of sheet to open or create.
+        share_email: Email to share sheet with.
+
+    Returns: Spreadsheet.
+    """
+
+    gspread_client = gspread_client or init_client()
+
+    try:
+        sheet = gspread_client.open(sheet_name)
+    except gspread.SpreadsheetNotFound:
+        _logger.info("Sheet not found, creating.", sheet_name=sheet_name)
+        sheet = gspread_client.create(sheet_name)
+
+    if share_email:
+        _logger.info("Sharing sheet", email=share_email)
+        sheet.share(share_email, perm_type="user", role="writer")
+
+    return sheet
+
+
+def update_info_sheet(
+    sheet: gspread.Spreadsheet,
+    sheet_name: str = "Update Info",
+    pointer_directory: pathlib.Path = dataset_utils.DATA_DIRECTORY,
+):
+    filename = dataset_pointer.form_filename(DatasetType.TIMESERIES)
+    pointer_path = pointer_directory / filename
+    pointer = DatasetPointer.parse_raw(pointer_path.read_text())
+    data = [
+        ("Field", "Value"),
+        ("Updated at", datetime.datetime.utcnow().isoformat()),
+        ("Covid Data Model SHA", pointer.model_git_info.sha),
+        ("Covid Data Public SHA", pointer.data_git_info.sha),
+    ]
+    worksheet = create_or_replace_worksheet(sheet, sheet_name)
+    worksheet.update(data)
+
+    _logger.info("Successfully updated Info worksheet")
