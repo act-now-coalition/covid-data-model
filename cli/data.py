53840d6ed16f5eb3a8834c93f843b96b1ea2fe0a
Chris Kelly <ghop02@gmail.com>
2020-08-10 10:55:19 -0400

    Add data availability sheet (#602)
    * Adding ability to update google sheet with data availability report
    
    * Add test for data availibility report
    
    * Add gspread to requirements
    
    * update data availability notebook
    
    * A bit of small cleanup, add to update datasets github action
    
    * fix test

--- cli/data.py
+++ cli/data.py
@@ -1,15 +1,19 @@
 from typing import Optional
 import logging
 import pathlib
+
 import click
+import gspread
+
+from libs import google_sheet_helpers
 from libs.qa import dataset_summary
+from libs.qa import data_availability
 from libs.datasets.timeseries import TimeseriesDataset
 from libs.datasets import dataset_utils
 from libs.datasets import combined_dataset_utils
 from libs.datasets import combined_datasets
 from libs.datasets.combined_dataset_utils import DatasetType
 from libs.datasets.dataset_utils import AggregationLevel
-
 
 PROD_BUCKET = "data.covidactnow.org"
 
@@ -58,3 +62,27 @@ def save_summary(output_dir: pathlib.Path, filename: str, level: Optional[Aggreg
         us_timeseries = us_timeseries.get_subset(aggregation_level=level)
 
     _save_field_summary(us_timeseries, output_dir / filename)
+
+
+@main.command()
+@click.option("--name", envvar="DATA_AVAILABILITY_SHEET_NAME", default="Data Availability - Dev")
+@click.option("--share-email")
+def update_availability_report(name: str, share_email: Optional[str]):
+    sheet = google_sheet_helpers.open_or_create_spreadsheet(name, share_email=share_email)
+    info_worksheet = google_sheet_helpers.update_info_sheet(sheet)
+    data_sources_by_source_name = data_availability.load_all_latest_sources()
+
+    for name, dataset in data_sources_by_source_name.items():
+        _logger.info(f"Updating {name}")
+        report = data_availability.build_data_availability_report(dataset)
+        data_availability.update_multi_field_availability_report(
+            sheet, report, name, columns_to_drop=["source", "fips", "generated"]
+        )
+
+    # Reorder sheets with combined data first and metadata last
+    COLUMN_ORDER_OVERRIDE = {data_availability.COMBINED_DATA_KEY: -5, info_worksheet.title: 5}
+    worksheets = sheet.worksheets()
+    worksheets = sorted(worksheets, key=lambda x: (COLUMN_ORDER_OVERRIDE.get(x.title, 0), x.title))
+    sheet.reorder_worksheets(worksheets)
+
+    _logger.info("Finished updating data availability report")
