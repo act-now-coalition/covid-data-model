59f2d579f4f5a1408f5733cd138742633868b933
Benjamin Smith <benjamin.holtzman.smith@gmail.com>
2020-08-16 17:48:40 -0400

    fix tests

--- test/libs/top_level_metrics_test.py
+++ test/libs/top_level_metrics_test.py
@@ -10,7 +10,7 @@ from libs.top_level_metrics import (
 )
 
 
-def test_calculate_case_density(self):
+def test_calculate_case_density():
     """
     It should use population, smoothing and a normalizing factor to calculate case density.
     """
@@ -22,10 +22,10 @@ def test_calculate_case_density(self):
     density = calculate_case_density(
         cases=cases, population=pop, smooth=smooth, normalize_by=every_ten
     )
-    self.assertTrue(density.equals(pd.Series([0, 1, 2, 3], dtype="float")))
+    assert density.equals(pd.Series([0, 1, 2, 3], dtype="float"))
 
 
-def test_calculate_test_positivity(self):
+def test_calculate_test_positivity():
     """
     It should use smoothed case data to calculate positive test rate.
     """
@@ -35,10 +35,10 @@ def test_calculate_test_positivity(self):
     pos_rate = calculate_test_positivity(
         pos_cases=pos_cases, neg_tests=neg_cases, smooth=2, lag_lookback=1
     )
-    self.assertTrue(pos_rate.equals(pd.Series([1, 0.75, 2 / 3], dtype="float64")))
+    assert pos_rate.equals(pd.Series([1, 0.75, 2 / 3], dtype="float64"))
 
 
-def test_calculate_test_positivity_lagging(self):
+def test_calculate_test_positivity_lagging():
     """
     It should return an empty series if there is missing negative case data.
     """
@@ -47,31 +47,31 @@ def test_calculate_test_positivity_lagging(self):
     pos_rate = calculate_test_positivity(
         pos_cases=pos_cases, neg_tests=neg_cases, smooth=2, lag_lookback=1
     )
-    self.assertTrue(pos_rate.equals(pd.Series([], dtype="float64")))
+    assert pos_rate.equals(pd.Series([], dtype="float64"))
 
 
-def test_sliding_window(self):
+def test_sliding_window():
     """
     It should average within sliding window.
     """
     series = pd.Series([1, 2, 4, 5, 0])
     smoothed = smooth_with_rolling_average(series=series, window=2)
-    self.assertTrue(smoothed.equals, pd.Series([1, 1.5, 3, 4.5, 2.5]))
+    assert smoothed.equals, pd.Series([1, 1.5, 3, 4.5, 2.5])
 
 
-def test_window_exceeds_series(self):
+def test_window_exceeds_series():
     """
     It should not average the first value.
     """
     series = pd.Series([1, 2])
     smoothed = smooth_with_rolling_average(series=series, window=5)
-    self.assertTrue(smoothed.equals, pd.Series([1, 1.5]))
+    assert smoothed.equals, pd.Series([1, 1.5])
 
 
-def test_exclude_trailing_zeroes(self):
+def test_exclude_trailing_zeroes():
     """
     It should not average the first value.
     """
     series = pd.Series([1, 2, 4, 5, 0])
     smoothed = smooth_with_rolling_average(series=series, window=5, includeTrailingZeros=False)
-    self.assertTrue(smoothed.equals, pd.Series([1, 1.5, 3, 4.5, np.nan]))
+    assert smoothed.equals, pd.Series([1, 1.5, 3, 4.5, np.nan])
