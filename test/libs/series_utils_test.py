28046ecd116c3738a90d16bf0d2e02469d6d76fc
Chris Kelly <ghop02@gmail.com>
2020-08-19 17:57:59 -0400

    Add interpolation tests

--- test/libs/series_utils_test.py
+++ test/libs/series_utils_test.py
@@ -30,3 +30,30 @@ def test_exclude_trailing_zeroes():
         series=series, window=2, include_trailing_zeros=False
     )
     pd.testing.assert_series_equal(smoothed, pd.Series([1, 1.5, 3, 4.5, np.nan]))
+
+
+def test_interpolation():
+
+    series = pd.Series([np.nan, 1, 2, 2, 4])
+
+    results = series_utils.interpolate_stalled_values(series)
+    expected = pd.Series([np.nan, 1, 2, 3, 4])
+    pd.testing.assert_series_equal(results, expected)
+
+
+def test_interpolation_bounds():
+
+    series = pd.Series([np.nan, 1, 2, 2, 4, np.nan])
+
+    results = series_utils.interpolate_stalled_values(series)
+    expected = pd.Series([np.nan, 1, 2, 3, 4, np.nan])
+    pd.testing.assert_series_equal(results, expected)
+
+
+def test_interpolation_missing_index():
+
+    series = pd.Series([np.nan, 1, 2, 2, 5], index=[0, 1, 2, 3, 5])
+
+    results = series_utils.interpolate_stalled_values(series)
+    expected = pd.Series([np.nan, 1, 2, 3, 5], index=[0, 1, 2, 3, 5])
+    pd.testing.assert_series_equal(results, expected)
