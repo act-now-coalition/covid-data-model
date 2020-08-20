95d8f1be3aa5f33fcbcb6e513ab0d288d508a5dc
Brett <brettboval@gmail.com>
2020-08-20 14:32:41 -0400

    Update utils.py

--- pyseir/icu/utils.py
+++ pyseir/icu/utils.py
@@ -1,5 +1,6 @@
 from libs.datasets import combined_datasets
 from covidactnow.datapublic.common_fields import CommonFields
+from libs.datasets.dataset_utils import AggregationLevel
 import pandas as pd
 import us
 
@@ -18,16 +19,18 @@ def _cases_quantile_range(x):
 def calculate_case_based_weights():
     LOOKBACK_DAYS = 31
     cutoff_date = pd.Timestamp.now() - pd.Timedelta(days=LOOKBACK_DAYS)
-    ts = combined_datasets.load_us_timeseries_dataset().get_subset(after=cutoff_date)
+    ts = combined_datasets.load_us_timeseries_dataset().get_subset(
+        after=cutoff_date, aggregation_level=AggregationLevel.COUNTY
+    )
 
     last_month_cum_cases = ts.data.groupby("fips").apply(_cases_quantile_range)
     last_month_cum_cases.name = "summed_cases"
 
     df = last_month_cum_cases.reset_index().dropna()
-    df = df.drop(df[df["fips"].str.len() == 2].index)  # Drop Rows with State Data
-    df["state"] = [us.states.lookup(x[:2]).abbr for x in df.fips]  # Apply State Labels to Groupby
+    df["state_fips"] = df["fips"].str.slice(0, 2)  # Apply State Labels to Groupby
     # Normalize the cases based on the groupby total
-    df["weight"] = df.groupby("state")["summed_cases"].transform(lambda x: x / x.sum()).round(5)
+    df["weight"] = df.groupby("state_fips")["summed_cases"].transform(lambda x: x / x.sum())
+    df["weight"] = df["weight"].round(5)
     # Convert to dict mapping
     output = df.set_index("fips")["weight"].to_dict()
     return output
