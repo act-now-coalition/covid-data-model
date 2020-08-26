b2a9c16d41b6c88291885a249d19441e293af636
Tom Brown <tomgith4@thecap.org>
2020-08-26 12:47:27 -0700

    use tmp_path in test_pyseir_end_to_end_idaho

--- test/pyseir_end_to_end_test.py
+++ test/pyseir_end_to_end_test.py
@@ -13,24 +13,22 @@ pytestmark = pytest.mark.filterwarnings("error")
 
 
 @pytest.mark.slow
-def test_pyseir_end_to_end_idaho():
+def test_pyseir_end_to_end_idaho(tmp_path):
     # This covers a lot of edge cases.
+    with unittest.mock.patch("pyseir.utils.OUTPUT_DIR", str(tmp_path)):
+        cli._build_all_for_states(states=["ID"], fips="16001")
 
-    with temppathlib.TemporaryDirectory() as tmp_dir:
-        with unittest.mock.patch("pyseir.utils.OUTPUT_DIR", tmp_dir.path):
-            cli._build_all_for_states(states=["ID"], fips="16001")
-
-            path = get_run_artifact_path("16001", RunArtifact.WEB_UI_RESULT).replace(
-                "__INTERVENTION_IDX__", "2"
-            )
-            path = pathlib.Path(path)
-            assert path.exists()
-            output = CANPyseirLocationOutput.load_from_path(path)
-            data = output.data
-            with_values = data[schema.RT_INDICATOR].dropna()
-            assert len(with_values) > 10
-            assert (with_values > 0).all()
-            assert (with_values < 6).all()
+        path = get_run_artifact_path("16001", RunArtifact.WEB_UI_RESULT).replace(
+            "__INTERVENTION_IDX__", "2"
+        )
+        path = pathlib.Path(path)
+        assert path.exists()
+        output = CANPyseirLocationOutput.load_from_path(path)
+        data = output.data
+        with_values = data[schema.RT_INDICATOR].dropna()
+        assert len(with_values) > 10
+        assert (with_values > 0).all()
+        assert (with_values < 6).all()
 
 
 @pytest.mark.slow
