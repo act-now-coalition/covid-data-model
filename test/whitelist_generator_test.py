cc6a1479e85c773569fb095ad6426f1c8bbe8028
Tom Brown <tomgith4@thecap.org>
2020-08-26 15:23:20 -0700

    turn all warnings in whitelist_generator_test to errors

--- test/whitelist_generator_test.py
+++ test/whitelist_generator_test.py
@@ -1,4 +1,10 @@
+import pytest
+
 from pyseir.inference.whitelist_generator import WhitelistGenerator
+
+
+# turns all warnings into errors for this module
+pytestmark = pytest.mark.filterwarnings("error")
 
 
 def test_all_data_smoke_test():
