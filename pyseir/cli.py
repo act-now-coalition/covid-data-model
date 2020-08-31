a90c4498df4664906a7d96afcc40631aae200ad1
Tom Brown <tomgith4@thecap.org>
2020-08-31 13:10:09 -0700

    rm RunMode

--- pyseir/cli.py
+++ pyseir/cli.py
@@ -28,7 +28,6 @@ sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."
 
 root = logging.getLogger()
 
-DEFAULT_RUN_MODE = "can-inference-derived"
 ALL_STATES: List[str] = [state_obj.abbr for state_obj in us.STATES] + ["PR"]
 
 
@@ -75,20 +74,19 @@ def _states_region_list(state: Optional[str], default: List[str]) -> List[pipeli
 
 
 def _state_only_pipeline(
-    region: pipeline.Region, run_mode=DEFAULT_RUN_MODE, output_interval_days=1, output_dir=None,
+    region: pipeline.Region, output_interval_days=1, output_dir=None,
 ) -> model_fitter.ModelFitter:
     assert region.is_state()
 
     infer_rt.run_rt(infer_rt.RegionalInput.from_region(region))
     fitter = model_fitter.run_state(region)
     ensembles_input = ensemble_runner.RegionalInput.from_model_fitter(fitter)
-    ensemble_runner.run_region(ensembles_input, ensemble_kwargs={"run_mode": run_mode})
+    ensemble_runner.run_region(ensembles_input)
     return fitter
 
 
 def _build_all_for_states(
     states: List[str],
-    run_mode=DEFAULT_RUN_MODE,
     output_interval_days=4,
     output_dir=None,
     skip_whitelist=False,
@@ -101,10 +99,7 @@ def _build_all_for_states(
     # do everything for just states in parallel
     with Pool(maxtasksperchild=1) as p:
         states_only_func = partial(
-            _state_only_pipeline,
-            run_mode=run_mode,
-            output_interval_days=output_interval_days,
-            output_dir=output_dir,
+            _state_only_pipeline, output_interval_days=output_interval_days, output_dir=output_dir,
         )
         states_regions = [pipeline.Region.from_state(s) for s in states]
         state_fitters: List[model_fitter.ModelFitter] = p.map(states_only_func, states_regions)
@@ -167,9 +162,7 @@ def _build_all_for_states(
         ensemble_inputs = [
             ensemble_runner.RegionalInput.from_model_fitter(f) for f in county_fitters
         ]
-        ensemble_func = partial(
-            ensemble_runner.run_region, ensemble_kwargs=dict(run_mode=run_mode),
-        )
+        ensemble_func = ensemble_runner
         p.map(ensemble_func, ensemble_inputs)
 
     # output it all
@@ -182,7 +175,7 @@ def _build_all_for_states(
     # TODO: Remove intermediate artifacts and paralellize artifacts creation better
     # Approximately 40% of the processing time is taken on this step
     web_ui_mapper = WebUIDataAdaptorV1(
-        output_interval_days=output_interval_days, run_mode=run_mode, output_dir=output_dir,
+        output_interval_days=output_interval_days, output_dir=output_dir,
     )
 
     webui_inputs = [
@@ -235,64 +228,10 @@ def run_mle_fits(state, states_only):
 
 @entry_point.command()
 @click.option(
-    "--state", help="State to generate files for. If no state is given, all states are computed."
-)
-@click.option(
-    "--run-mode",
-    default=DEFAULT_RUN_MODE,
-    type=click.Choice([run_mode.value for run_mode in ensemble_runner.RunMode]),
-    help="State to generate files for. If no state is given, all states are computed.",
-)
-@click.option("--states-only", default=False, is_flag=True, type=bool, help="Only model states")
-def run_ensembles(state, run_mode, states_only):
-    run_region = partial(ensemble_runner.run_region, ensemble_kwargs={"run_mode": run_mode})
-    for region in _states_region_list(state=state, default=ALL_STATES):
-        state_regional_input = ensemble_runner.RegionalInput.from_region(region)
-        run_region(state_regional_input)
-        if not states_only:
-            # Run county level
-            with Pool(maxtasksperchild=1) as p:
-                p.map(run_region, state_regional_input.get_counties_regional_input())
-
-
-@entry_point.command()
-@click.option(
-    "--state", help="State to generate files for. If no state is given, all states are computed."
-)
-@click.option(
-    "--output-interval-days",
-    default=1,
-    type=int,
-    help="Number of days between outputs for the WebUI payload.",
-)
-@click.option(
-    "--run-mode",
-    default=DEFAULT_RUN_MODE,
-    type=click.Choice([run_mode.value for run_mode in ensemble_runner.RunMode]),
-    help="State to generate files for. If no state is given, all states are computed.",
-)
-@click.option("--states-only", default=False, is_flag=True, type=bool, help="Only model states")
-def map_outputs(state, output_interval_days, run_mode, states_only):
-    web_ui_mapper = WebUIDataAdaptorV1(
-        output_interval_days=int(output_interval_days), run_mode=run_mode,
-    )
-    for region in _states_region_list(state=state, default=ALL_STATES):
-        state_input = webui_data_adaptor_v1.RegionalInput.from_region(region)
-        web_ui_mapper.write_region(state_input)
-
-
-@entry_point.command()
-@click.option(
     "--states",
     "-s",
     multiple=True,
     help="a list of states to generate files for. If no state is given, all states are computed.",
-)
-@click.option(
-    "--run-mode",
-    default=DEFAULT_RUN_MODE,
-    type=click.Choice([run_mode.value for run_mode in ensemble_runner.RunMode]),
-    help="State to generate files for. If no state is given, all states are computed.",
 )
 @click.option(
     "--output-interval-days",
@@ -314,7 +253,7 @@ def map_outputs(state, output_interval_days, run_mode, states_only):
 @click.option("--states-only", is_flag=True, help="If set, only runs on states.")
 @click.option("--output-dir", default=None, type=str, help="Directory to deploy webui output.")
 def build_all(
-    states, run_mode, output_interval_days, output_dir, skip_whitelist, states_only, fips,
+    states, output_interval_days, output_dir, skip_whitelist, states_only, fips,
 ):
     # split columns by ',' and remove whitespace
     states = [c.strip() for c in states]
@@ -325,7 +264,6 @@ def build_all(
 
     _build_all_for_states(
         states,
-        run_mode=DEFAULT_RUN_MODE,
         output_interval_days=output_interval_days,
         output_dir=output_dir,
         skip_whitelist=skip_whitelist,
