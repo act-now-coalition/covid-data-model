3ad1b6d2139a84735dc181c3a65cc3f47be3e83c
Tom Brown <tomgith4@thecap.org>
2020-09-08 11:22:31 -0700

    add simple asserts that test_get_icu returned /something/

--- test/pyseir/icu/infer_icu_test.py
+++ test/pyseir/icu/infer_icu_test.py
@@ -1,17 +1,26 @@
 import pytest
+from covidactnow.datapublic.common_fields import CommonFields
 
 from libs import pipeline
 from pyseir.icu import infer_icu
 
 
-# Check some counties picked arbitrarily: San Francisco/06075, Houston (Harris County, TX)/48201, Riley KS/20161
-@pytest.mark.parametrize("fips", ["06075", "48201", "20161"])
+@pytest.mark.parametrize(
+    "fips",
+    [
+        "06075",  # San Francisco
+        "48201",  # Houston (Harris County, TX)
+        "20161",  # Riley KS (small, takes a different code path).
+    ],
+)
 def test_get_icu(fips):
     region = pipeline.Region.from_fips(fips)
     regional_combined_data = pipeline.RegionalCombinedData.from_region(region)
     state_combined_data = pipeline.RegionalCombinedData.from_region(region.get_state_region())
-    infer_icu.get_icu_timeseries(
+    result_series = infer_icu.get_icu_timeseries(
         region,
         regional_combined_data=regional_combined_data.get_timeseries(),
         state_combined_data=state_combined_data.get_timeseries(),
     )
+    assert not result_series.dropna().empty
+    assert result_series.index.names == [CommonFields.DATE]
