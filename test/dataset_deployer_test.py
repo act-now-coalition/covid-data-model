29f2a92f62fc684db1df1e927bf35b1dd611e38d
Chris Kelly <ghop02@gmail.com>
2020-10-05 13:31:04 -0400

    Adding test for skipping keys

--- test/dataset_deployer_test.py
+++ test/dataset_deployer_test.py
@@ -9,3 +9,12 @@ def test_flatten_dict():
 
     expected = {"key1": 1, "key2.nested1": "a", "key2.nested2": "b"}
     assert results == expected
+
+
+def test_write_nested_csv_with_skipped_keys(tmp_path):
+    output_path = tmp_path / "output.csv"
+
+    data = [{"foo": {"bar": 1, "baz": 2}}]
+    dataset_deployer.write_nested_csv(data, output_path, keys_to_skip=["foo.bar"])
+    header = output_path.read_text().split("\n")[0]
+    assert header == "foo.baz"
