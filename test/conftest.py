8643d27f836f2df92b02b9687deef54ffaf56456
Chris Kelly <ghop02@gmail.com>
2020-10-06 14:16:51 -0400

    Adding nyc test icu/rt dataset + fix generate_api_tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import pathlib
 import pytest
 from libs import pipeline
+from libs.datasets import timeseries
 
 
 @pytest.fixture
@@ -18,3 +19,21 @@ def nyc_model_output_path() -> pathlib.Path:
     # generated from running pyseir model output.  To update, run
     test_root = pathlib.Path(__file__).parent
     return test_root / "data" / "pyseir" / "36061.1.json"
+
+
+@pytest.fixture
+def nyc_rt_dataset(nyc_region) -> timeseries.OneRegionTimeseriesDataset:
+    # generated from running pyseir model output.
+    test_root = pathlib.Path(__file__).parent
+    path = test_root / "data" / "pyseir" / "rt_combined_metric.csv"
+    dataset = timeseries.MultiRegionTimeseriesDataset.from_csv(path)
+    return dataset.get_one_region(nyc_region)
+
+
+@pytest.fixture
+def nyc_icu_dataset(nyc_region) -> timeseries.OneRegionTimeseriesDataset:
+    # generated from running pyseir model output.
+    test_root = pathlib.Path(__file__).parent
+    path = test_root / "data" / "pyseir" / "icu_combined_metric.csv"
+    dataset = timeseries.MultiRegionTimeseriesDataset.from_csv(path)
+    return dataset.get_one_region(nyc_region)
