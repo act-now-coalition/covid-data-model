248175569291d56685b119a4d7f9d7fabea63324
GitHub Action <action@github.com>
2020-10-15 19:01:58 -0400

    Update test

--- test/libs/api_usage_updater_test.py
+++ test/libs/api_usage_updater_test.py
@@ -5,15 +5,10 @@ from libs import update_api_user_metrics
 def test_activity_summary_query(mocker):
     query_mock = mocker.patch.object(update_api_user_metrics, "_run_query")
     now = datetime.utcnow()
-    example_boto_response = {
-        "results": [
-            [
-                {"field": "signupDate", "value": now.isoformat()},
-                {"field": "daysActive", "value": "10"},
-            ]
-        ]
-    }
-    query_mock.return_value = example_boto_response
+    example_rows = [
+        {"signupDate": now.isoformat(), "daysActive": "10"},
+    ]
+    query_mock.return_value = example_rows
 
-    results = update_api_user_metrics.run_user_activity_summary_query("test_log_group")
+    results = update_api_user_metrics.run_user_activity_summary_query("table", "database")
     assert results == [{"signupDate": now.date().isoformat(), "daysActive": 10}]
