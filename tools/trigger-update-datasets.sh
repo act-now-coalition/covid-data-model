f4f39a5c169b44989a696758d81d751a745ef12d
Chris Kelly <ghop02@gmail.com>
2020-10-21 16:09:40 -0400

    Add dataset update trigger (#748)

---
+++ tools/trigger-update-datasets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# trigger-update-datasets.sh - Updates datasets
+
+set -o nounset
+set -o errexit
+
+CMD=$0
+
+# Checks command-line arguments, sets variables, etc.
+prepare () {
+
+  if [[ -z ${GITHUB_TOKEN:-} ]]; then
+    echo "Error: GITHUB_TOKEN must be set to a personal access token. See:"
+    echo "https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line"
+    exit 1
+  fi
+}
+
+execute () {
+  curl -H "Authorization: token $GITHUB_TOKEN" \
+      --request POST \
+      --data "{\"event_type\": \"update-dataset-snapshot\" }" \
+      https://api.github.com/repos/covid-projections/covid-data-model/dispatches
+
+  echo "Updating combined datasets. Go to https://github.com/covid-projections/covid-data-model/actions to monitor progress."
+}
+
+prepare "$@"
+execute
