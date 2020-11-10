658291bfea2bab7a72d61c47591a5eae6cff49f3
GitHub Action <action@github.com>
2020-11-10 17:40:12 -0500

    adding csbas to docs

--- api/update_open_api_spec.py
+++ api/update_open_api_spec.py
@@ -11,6 +11,7 @@ from openapi_schema_pydantic.util import construct_open_api_with_schema_class
 
 COUNTY_TAG = "County Data"
 STATE_TAG = "State Data"
+CBSA_TAG = "CBSA (metro) Data"
 
 
 fips_parameter = {
@@ -18,6 +19,13 @@ fips_parameter = {
     "in": "path",
     "required": True,
     "description": "5 Letter County FIPS code",
+    "schema": {"type": "string"},
+}
+cbsa_parameter = {
+    "name": "cbsa_id",
+    "in": "path",
+    "required": True,
+    "description": "5 Letter CBSA ID",
     "schema": {"type": "string"},
 }
 state_parameter = {
@@ -92,57 +100,6 @@ COUNTY_TIMESERIES = APIEndpoint(
     summary="Single County Timeseries",
     schema_cls=can_api_v2_definition.RegionSummaryWithTimeseries,
 )
-STATE_SUMMARY = APIEndpoint(
-    endpoint="/state/{state}.json",
-    parameters=[state_parameter],
-    tags=[STATE_TAG],
-    description="Region Summary object for a single state.",
-    summary="Single State Summary",
-    schema_cls=can_api_v2_definition.RegionSummary,
-)
-STATE_TIMESERIES = APIEndpoint(
-    endpoint="/state/{state}.timeseries.json",
-    parameters=[state_parameter],
-    tags=[STATE_TAG],
-    description="Region Summary with Timeseries for a single state.",
-    summary="Single State Timeseries",
-    schema_cls=can_api_v2_definition.RegionSummaryWithTimeseries,
-)
-
-
-ALL_STATE_SUMMARY = APIEndpoint(
-    endpoint="/states.json",
-    parameters=[],
-    tags=[STATE_TAG],
-    description="Region Summaries for all states",
-    summary="All states summary (json)",
-    schema_cls=can_api_v2_definition.AggregateRegionSummary,
-)
-ALL_STATE_SUMMARY_CSV = APIEndpoint(
-    endpoint="/states.csv",
-    parameters=[],
-    tags=[STATE_TAG],
-    description="Region Summaries for all states",
-    summary="All states summary (csv)",
-    schema_cls=can_api_v2_definition.AggregateRegionSummary,
-)
-ALL_STATE_TIMESERIES = APIEndpoint(
-    endpoint="/states.timeseries.json",
-    parameters=[],
-    tags=[STATE_TAG],
-    description="Region summaries with timeseries for all states",
-    summary="All states timeseries",
-    schema_cls=can_api_v2_definition.AggregateRegionSummaryWithTimeseries,
-)
-ALL_STATE_TIMESERIES_CSV = APIEndpoint(
-    endpoint="/states.timeseries.csv",
-    parameters=[],
-    tags=[STATE_TAG],
-    description="Region summaries with timeseries for all states",
-    summary="All states timeseries (csv)",
-    schema_cls=can_api_v2_definition.AggregateFlattenedTimeseries,
-)
-
 ALL_COUNTY_SUMMARY = APIEndpoint(
     endpoint="/counties.json",
     parameters=[],
@@ -177,11 +134,112 @@ ALL_COUNTY_TIMESERIES_CSV = APIEndpoint(
 )
 
 
+STATE_SUMMARY = APIEndpoint(
+    endpoint="/state/{state}.json",
+    parameters=[state_parameter],
+    tags=[STATE_TAG],
+    description="Region Summary object for a single state.",
+    summary="Single State Summary",
+    schema_cls=can_api_v2_definition.RegionSummary,
+)
+STATE_TIMESERIES = APIEndpoint(
+    endpoint="/state/{state}.timeseries.json",
+    parameters=[state_parameter],
+    tags=[STATE_TAG],
+    description="Region Summary with Timeseries for a single state.",
+    summary="Single State Timeseries",
+    schema_cls=can_api_v2_definition.RegionSummaryWithTimeseries,
+)
+ALL_STATE_SUMMARY = APIEndpoint(
+    endpoint="/states.json",
+    parameters=[],
+    tags=[STATE_TAG],
+    description="Region Summaries for all states",
+    summary="All states summary (json)",
+    schema_cls=can_api_v2_definition.AggregateRegionSummary,
+)
+ALL_STATE_SUMMARY_CSV = APIEndpoint(
+    endpoint="/states.csv",
+    parameters=[],
+    tags=[STATE_TAG],
+    description="Region Summaries for all states",
+    summary="All states summary (csv)",
+    schema_cls=can_api_v2_definition.AggregateRegionSummary,
+)
+ALL_STATE_TIMESERIES = APIEndpoint(
+    endpoint="/states.timeseries.json",
+    parameters=[],
+    tags=[STATE_TAG],
+    description="Region summaries with timeseries for all states",
+    summary="All states timeseries",
+    schema_cls=can_api_v2_definition.AggregateRegionSummaryWithTimeseries,
+)
+ALL_STATE_TIMESERIES_CSV = APIEndpoint(
+    endpoint="/states.timeseries.csv",
+    parameters=[],
+    tags=[STATE_TAG],
+    description="Region summaries with timeseries for all states",
+    summary="All states timeseries (csv)",
+    schema_cls=can_api_v2_definition.AggregateFlattenedTimeseries,
+)
+
+CBSA_SUMMARY = APIEndpoint(
+    endpoint="/cbsa/{cbsa_id}.json",
+    parameters=[cbsa_parameter],
+    tags=[CBSA_TAG],
+    description="Region Summary object for a single CBSA.",
+    summary="Single CBSA Summary",
+    schema_cls=can_api_v2_definition.RegionSummary,
+)
+CBSA_TIMESERIES = APIEndpoint(
+    endpoint="/cbsa/{cbsa_id}.timeseries.json",
+    parameters=[cbsa_parameter],
+    tags=[CBSA_TAG],
+    description="Region Summary with Timeseries for a single CBSA.",
+    summary="Single CBSA Timeseries",
+    schema_cls=can_api_v2_definition.RegionSummaryWithTimeseries,
+)
+ALL_CBSA_SUMMARY = APIEndpoint(
+    endpoint="/cbsas.json",
+    parameters=[],
+    tags=[CBSA_TAG],
+    description="Region Summaries for all CBSAs",
+    summary="All CBSAs summary (json)",
+    schema_cls=can_api_v2_definition.AggregateRegionSummary,
+)
+ALL_CBSA_SUMMARY_CSV = APIEndpoint(
+    endpoint="/cbsas.csv",
+    parameters=[],
+    tags=[CBSA_TAG],
+    description="Region Summaries for all CBSAs",
+    summary="All CBSAs summary (csv)",
+    schema_cls=can_api_v2_definition.AggregateRegionSummary,
+)
+ALL_CBSA_TIMESERIES = APIEndpoint(
+    endpoint="/cbsas.timeseries.json",
+    parameters=[],
+    tags=[CBSA_TAG],
+    description="Region summaries with timeseries for all CBSAs",
+    summary="All CBSAs timeseries",
+    schema_cls=can_api_v2_definition.AggregateRegionSummaryWithTimeseries,
+)
+ALL_CBSA_TIMESERIES_CSV = APIEndpoint(
+    endpoint="/cbsas.timeseries.csv",
+    parameters=[],
+    tags=[CBSA_TAG],
+    description="Region summaries with timeseries for all CBSAs",
+    summary="All CBSAs timeseries (csv)",
+    schema_cls=can_api_v2_definition.AggregateFlattenedTimeseries,
+)
+
+
 ALL_ENDPOINTS = [
     COUNTY_SUMMARY,
     COUNTY_TIMESERIES,
     STATE_SUMMARY,
     STATE_TIMESERIES,
+    CBSA_SUMMARY,
+    CBSA_TIMESERIES,
     ALL_STATE_SUMMARY,
     ALL_STATE_SUMMARY_CSV,
     ALL_STATE_TIMESERIES,
@@ -190,6 +248,10 @@ ALL_ENDPOINTS = [
     ALL_COUNTY_SUMMARY_CSV,
     ALL_COUNTY_TIMESERIES,
     ALL_COUNTY_TIMESERIES_CSV,
+    ALL_CBSA_SUMMARY,
+    ALL_CBSA_SUMMARY_CSV,
+    ALL_CBSA_TIMESERIES,
+    ALL_CBSA_TIMESERIES_CSV,
 ]
 
 
