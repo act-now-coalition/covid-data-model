03bc15925a6c083bc368576eb2ba2a4df99590ab
Tom Brown <tomgith4@thecap.org>
2020-11-17 14:07:50 -0800

    better comment and assert

--- test/libs/datasets/timeseries_test.py
+++ test/libs/datasets/timeseries_test.py
@@ -784,9 +784,8 @@ def test_timeseries_empty():
 
 
 def test_timeseries_empty_static_not_empty():
-    # Check that empty geodata_timeseries_df creates a MultiRegionDataset
-    # and that get_one_region raises expected exception.
+    # Check that empty timeseries does not prevent static data working as expected.
     dataset = timeseries.MultiRegionDataset.from_geodata_timeseries_df(
         pd.DataFrame([], columns=[CommonFields.LOCATION_ID, CommonFields.DATE])
     ).add_static_values(pd.DataFrame([{"location_id": "iso1:us#fips:97111", "m1": 1234}]))
-    dataset.get_one_region(Region.from_fips("97111"))
+    assert dataset.get_one_region(Region.from_fips("97111")).latest["m1"] == 1234
