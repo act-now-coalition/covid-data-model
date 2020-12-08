act-now-coalition/covid-data-model

commit ae7dfcb5b36eea93771b8d92e453766cc5903465

Combined DataSources using MultiRegionDataset, remove latest values (#829)
## This PR

* Deletes most of `LatestValuesDataset` and `TimeseriesDataset`
* Deletes `combined_datasets.build_from_sources`, the old dataset merger.
* Moves aggregations that were in `LatestValuesDataset` to aggregate_puerto_rico_from_counties and aggregate_to_new_york_city.
* Fixes NYC test positivity aggregation by using timeseries.aggregate_regions
* Updates `libs/qa/common_df_diff.py` to use location_id instead of fips
* Makes crude attempt at fixing `libs/qa/data_availability.py` to use static_and_timeseries_latest_with_fips() without LatestValuesDataset. Tests pass but otherwise not tested.

## Tested

Ran `./run.py data update` (which finishes in about 1-2min on my laptop, vs 5-6 mins before this change).
The only change in `git difftool -t vimdiff master -- data/multiregion-wide-dates.csv` is NYC test_positivity looks much better and NYC lost provenance information because it isn't passed on in aggregation. `data/multiregion-static.csv` is unchanged from master.

libs/notebook_helpers.py
@@ -4,12 +4,10 @@ import os
 import pandas as pd
 
 from libs.datasets import dataset_utils
-from libs.datasets.timeseries import TimeseriesDataset
 from libs.datasets.sources import covid_county_data
 from libs.datasets.sources import covid_tracking_source
 from libs.datasets.sources import nytimes_dataset
 from libs.datasets.sources import texas_hospitalizations
-from libs.datasets.combined_datasets import US_STATES_FILTER
 
 _logger = logging.getLogger(__name__)
 
@@ -31,7 +29,7 @@ def set_covid_data_public():
     raise Exception("Could not find covid-data-public")
 
 
-def load_data_sources_by_name() -> Dict[str, TimeseriesDataset]:
+def load_data_sources_by_name() -> Dict[str, pd.DataFrame]:
 
     sources = [
         covid_county_data.CovidCountyDataDataSource,
@@ -41,21 +39,9 @@ def load_data_sources_by_name() -> Dict[str, TimeseriesDataset]:
     ]
     source_map = {}
     for source_cls in sources:
-        dataset = source_cls.local().timeseries()
-        dataset = US_STATES_FILTER.apply(dataset)
+        dataset = source_cls.local().multi_region_dataset().static_and_timeseries_latest_with_fips()
+
         dataset.data["source"] = source_cls.__name__
         source_map[source_cls.__name__] = dataset
 
     return source_map
-
-
-def load_combined_timeseries(
-    sources: Dict[str, TimeseriesDataset], timeseries: TimeseriesDataset
-) -> TimeseriesDataset:
-    timeseries_data = timeseries.data.copy()
-    timeseries_data["source"] = "Combined Data"
-
-    combined_timeseries = TimeseriesDataset(
-        pd.concat([timeseries_data] + [source.data for source in sources.values()])
-    )
-    return combined_timeseries

cli/data.py
@@ -14,19 +14,16 @@ from covidactnow.datapublic.common_fields import CommonFields
 from libs import google_sheet_helpers, wide_dates_df
 from libs import pipeline
 from libs.datasets import AggregationLevel
+from libs.datasets import combined_dataset_utils
+from libs.datasets import custom_aggregations
 from libs.datasets import statistical_areas
 from libs.datasets.combined_datasets import (
     ALL_TIMESERIES_FEATURE_DEFINITION,
-    US_STATES_FILTER,
     ALL_FIELDS_FEATURE_DEFINITION,
 )
-from libs.datasets.latest_values_dataset import LatestValuesDataset
-from libs.datasets.timeseries import MultiRegionDataset
+from libs.datasets.timeseries import DatasetName
 from libs.datasets import timeseries
-from libs.qa import data_availability
-from libs.datasets.timeseries import TimeseriesDataset
 from libs.datasets import dataset_utils
-from libs.datasets import combined_dataset_utils
 from libs.datasets import combined_datasets
 from libs.datasets.sources import forecast_hub
 from libs.us_state_abbrev import ABBREV_US_UNKNOWN_COUNTY_FIPS
@@ -64,7 +61,11 @@ def update_forecasts(filename):
     help="Aggregate states to one USA country region",
     default=False,
 )
-def update(wide_dates_filename, aggregate_to_country: bool):
+@click.option("--state", type=str, help="For testing, a two letter state abbr")
+@click.option("--fips", type=str, help="For testing, a 5 digit county fips")
+def update(
+    wide_dates_filename, aggregate_to_country: bool, state: Optional[str], fips: Optional[str]
+):
     """Updates latest and timeseries datasets to the current checked out covid data public commit"""
     path_prefix = dataset_utils.DATA_DIRECTORY.relative_to(dataset_utils.REPO_ROOT)
 
@@ -75,23 +76,27 @@ def update(wide_dates_filename, aggregate_to_country: bool):
         )
     )
     data_sources = {
-        data_source_cls.SOURCE_NAME: data_source_cls.local()
+        data_source_cls.SOURCE_NAME: data_source_cls.local().multi_region_dataset()
         for data_source_cls in data_source_classes
     }
-    timeseries_dataset: TimeseriesDataset = combined_datasets.build_from_sources(
-        TimeseriesDataset, data_sources, ALL_TIMESERIES_FEATURE_DEFINITION, filter=US_STATES_FILTER
-    )
-    latest_dataset: LatestValuesDataset = combined_datasets.build_from_sources(
-        LatestValuesDataset, data_sources, ALL_FIELDS_FEATURE_DEFINITION, filter=US_STATES_FILTER,
-    )
-    multiregion_dataset = MultiRegionDataset.from_timeseries_and_latest(
-        timeseries_dataset, latest_dataset
+    if state or fips:
+        data_sources = {
+            name: dataset.get_subset(state=state, fips=fips)
+            # aggregation_level=AggregationLevel.STATE)
+            for name, dataset in data_sources.items()
+        }
+    multiregion_dataset = timeseries.combined_datasets(
+        data_sources,
+        build_field_dataset_source(ALL_TIMESERIES_FEATURE_DEFINITION),
+        build_field_dataset_source(ALL_FIELDS_FEATURE_DEFINITION),
     )
     multiregion_dataset = timeseries.add_new_cases(multiregion_dataset)
     multiregion_dataset = timeseries.drop_new_case_outliers(multiregion_dataset)
     multiregion_dataset = timeseries.drop_regions_without_population(
         multiregion_dataset, KNOWN_LOCATION_ID_WITHOUT_POPULATION, structlog.get_logger()
     )
+    multiregion_dataset = timeseries.aggregate_puerto_rico_from_counties(multiregion_dataset)
+    multiregion_dataset = custom_aggregations.aggregate_to_new_york_city(multiregion_dataset)
 
     aggregator = statistical_areas.CountyToCBSAAggregator.from_local_public_data()
     cbsa_dataset = aggregator.aggregate(multiregion_dataset)
@@ -103,18 +108,15 @@ def update(wide_dates_filename, aggregate_to_country: bool):
         )
         multiregion_dataset = multiregion_dataset.append_regions(country_dataset)
 
-    _, multiregion_pointer = combined_dataset_utils.update_data_public_head(
-        path_prefix, latest_dataset, multiregion_dataset,
-    )
+    combined_dataset_utils.persist_dataset(multiregion_dataset, path_prefix)
 
     # Write DataSource objects that have provenance information, which is only set when significant
     # processing of the source data is done in this repo before it is combined. The output is not
     # used downstream, it is for debugging only.
-    for data_source in data_sources.values():
-        if data_source.provenance is not None:
+    for name, source_dataset in data_sources.items():
+        if not source_dataset.provenance.empty:
             wide_dates_df.write_csv(
-                data_source.timeseries().get_date_columns(),
-                path_prefix / f"{data_source.SOURCE_NAME}-wide-dates.csv",
+                source_dataset.timeseries_rows(), path_prefix / f"{name}-wide-dates.csv",
             )
 
     if wide_dates_filename:
@@ -181,6 +183,8 @@ def run_population_filter(output_path: pathlib.Path):
 @click.option("--name", envvar="DATA_AVAILABILITY_SHEET_NAME", default="Data Availability - Dev")
 @click.option("--share-email")
 def update_availability_report(name: str, share_email: Optional[str]):
+    from libs.qa import data_availability
+
     sheet = google_sheet_helpers.open_or_create_spreadsheet(name, share_email=share_email)
     info_worksheet = google_sheet_helpers.update_info_sheet(sheet)
     data_sources_by_source_name = data_availability.load_all_latest_sources()
@@ -216,3 +220,14 @@ def update_case_based_icu_utilization_weights():
     _logger.info(f"Saved case-based ICU Utilization weights to {output_path}")
     with open(output_path, "w") as f:
         json.dump(output, f, indent=2, sort_keys=True)
+
+
+def build_field_dataset_source(feature_definition_config):
+    feature_definition = {
+        # timeseries.combined_datasets has the highest priority first.
+        # TODO(tom): reverse the hard-coded FeatureDataSourceMap and remove the reversed call.
+        field_name: list(reversed(list(DatasetName(cls.SOURCE_NAME) for cls in classes)))
+        for field_name, classes in feature_definition_config.items()
+        if classes
+    }
+    return feature_definition

libs/datasets/beds.py
@@ -1,217 +1,0 @@
-from typing import Optional
-import pandas as pd
-from libs.datasets import dataset_utils
-from libs.datasets.dataset_utils import AggregationLevel
-from libs.datasets import custom_aggregations
-
-
-class BedsDataset(object):
-    class Fields(object):
-        COUNTRY = "country"
-        STATE = "state"
-        FIPS = "fips"
-        STAFFED_BEDS = "staffed_beds"
-        LICENSED_BEDS = "licensed_beds"
-        ICU_BEDS = "icu_beds"
-        ALL_BED_TYPICAL_OCCUPANCY_RATE = "all_beds_occupancy_rate"
-        ICU_TYPICAL_OCCUPANCY_RATE = "icu_occupancy_rate"
-
-        AGGREGATE_LEVEL = "aggregate_level"
-
-        COUNTY = "county"
-        SOURCE = "source"
-        # Combined beds are the max of staffed and licensed beds.
-        # The need for this may be dataset specific, but there are cases in the
-        # DH Beds dataset where a source only has either staffed or licensed.
-        # To provide a better best guess, we take the max of the two in `from_source`.
-        MAX_BED_COUNT = "max_bed_count"
-
-    # Key for grouping data at the state level.
-    # This includes other keys that should be preserved - for example, this will preserve
-    # the source field during a groupby.
-    STATE_GROUP_KEY = [
-        Fields.SOURCE,
-        Fields.AGGREGATE_LEVEL,
-        Fields.COUNTRY,
-        Fields.STATE,
-    ]
-
-    COUNTY_GROUP_KEY = [
-        Fields.SOURCE,
-        Fields.AGGREGATE_LEVEL,
-        Fields.COUNTRY,
-        Fields.STATE,
-        Fields.FIPS,
-    ]
-
-    def __init__(self, data):
-        self.data = data
-        self.validate()
-
-    @property
-    def state_data(self) -> pd.DataFrame:
-        """Returns a new BedsDataset containing only state data."""
-        is_state = self.data[self.Fields.AGGREGATE_LEVEL] == AggregationLevel.STATE.value
-        return self.data[is_state]
-
-    @property
-    def county_data(self) -> pd.DataFrame:
-        """Returns a new BedsDataset containing only county data."""
-        is_county = self.data[self.Fields.AGGREGATE_LEVEL] == AggregationLevel.COUNTY.value
-        return self.data[is_county]
-
-    @classmethod
-    def from_source(cls, source: "DataSource", fill_missing_state=True):
-        """Loads data from a specific datasource.
-
-        Remaps columns from source dataset, fills in missing data
-        by computing aggregates, and adds standardized county names from fips.
-
-        Args:
-            source: Data source.
-            fill_missing_state: If True, fills in missing state level data by
-                aggregating county level for a given state.
-        """
-        if not source.BEDS_FIELD_MAP:
-            raise ValueError("Source must have beds field map.")
-
-        data = source.data
-
-        to_common_fields = {value: key for key, value in source.BEDS_FIELD_MAP.items()}
-        final_columns = to_common_fields.values()
-        data = data.rename(columns=to_common_fields)[final_columns]
-        data[cls.Fields.SOURCE] = source.SOURCE_NAME
-
-        # Generating max bed count.
-        columns_to_consider = [cls.Fields.STAFFED_BEDS, cls.Fields.LICENSED_BEDS]
-        data[cls.Fields.MAX_BED_COUNT] = data[columns_to_consider].max(axis=1)
-
-        data = cls._aggregate_new_york_data(data)
-        if fill_missing_state:
-            non_matching = dataset_utils.aggregate_and_get_nonmatching(
-                data, cls.STATE_GROUP_KEY, AggregationLevel.COUNTY, AggregationLevel.STATE,
-            ).reset_index()
-
-            data = pd.concat([data, non_matching])
-
-        fips_data = dataset_utils.build_fips_data_frame()
-        data = dataset_utils.add_county_using_fips(data, fips_data)
-
-        return cls(data)
-
-    @classmethod
-    def _aggregate_new_york_data(cls, data):
-        # When grouping nyc data, we don't want to count the generated field
-        # as a value to sum.
-        nyc_data = data[data[cls.Fields.FIPS].isin(custom_aggregations.ALL_NYC_FIPS)]
-        if not len(nyc_data):
-            return data
-        group = cls.STATE_GROUP_KEY
-        weighted_all_bed_occupancy = None
-
-        if cls.Fields.ALL_BED_TYPICAL_OCCUPANCY_RATE in data.columns:
-            licensed_beds = nyc_data[cls.Fields.LICENSED_BEDS]
-            occupancy_rates = nyc_data[cls.Fields.ALL_BED_TYPICAL_OCCUPANCY_RATE]
-            weighted_all_bed_occupancy = (
-                licensed_beds * occupancy_rates
-            ).sum() / licensed_beds.sum()
-        weighted_icu_occupancy = None
-        if cls.Fields.ICU_TYPICAL_OCCUPANCY_RATE in data.columns:
-            icu_beds = nyc_data[cls.Fields.ICU_BEDS]
-            occupancy_rates = nyc_data[cls.Fields.ICU_TYPICAL_OCCUPANCY_RATE]
-            weighted_icu_occupancy = (icu_beds * occupancy_rates).sum() / icu_beds.sum()
-
-        data = custom_aggregations.update_with_combined_new_york_counties(
-            data, group, are_boroughs_zero=False
-        )
-
-        nyc_fips = custom_aggregations.NEW_YORK_COUNTY_FIPS
-        if weighted_all_bed_occupancy:
-            data.loc[
-                data[cls.Fields.FIPS] == nyc_fips, cls.Fields.ALL_BED_TYPICAL_OCCUPANCY_RATE,
-            ] = weighted_all_bed_occupancy
-
-        if weighted_icu_occupancy:
-            data.loc[
-                data[cls.Fields.FIPS] == nyc_fips, cls.Fields.ICU_TYPICAL_OCCUPANCY_RATE
-            ] = weighted_icu_occupancy
-
-        return data
-
-    def validate(self):
-        dataset_utils.check_index_values_are_unique(self.state_data, index=self.STATE_GROUP_KEY)
-        dataset_utils.check_index_values_are_unique(self.county_data, index=self.COUNTY_GROUP_KEY)
-
-    def get_data_for_state(self, state) -> dict:
-        """Gets all data for a given state.
-
-        Args:
-            state: State abbreviation.
-
-        Returns: Dictionary with all data for a given state.
-        """
-        data = self.state_data
-        row = data[data[self.Fields.STATE] == state]
-        if not len(row):
-            return {}
-
-        return row.iloc[0].to_dict()
-
-    def get_data_for_fips(self, fips) -> dict:
-        """Gets all data for a given fips code.
-
-        Args:
-            fips: fips code.
-
-        Returns: Dictionary with all data for a given fips code.
-        """
-        row = self.data[self.data[self.Fields.FIPS] == fips]
-        if not len(row):
-            return {}
-
-        return row.iloc[0].to_dict()
-
-    def get_state_level(self, state, column=Fields.MAX_BED_COUNT) -> Optional[int]:
-        """Get beds for a specific state.
-
-        Args:
-            state: State to query.
-            column: Beds count column to return.
-
-        Returns: Beds for a state.
-        """
-        state_filter = self.state_data.state == state
-        beds = self.state_data[state_filter]
-
-        if len(beds):
-            return beds.iloc[0][column]
-        return None
-
-    def get_county_level(
-        self, state, county=None, fips=None, column=Fields.MAX_BED_COUNT
-    ) -> Optional[int]:
-        """Get beds for a specific county (from fips code or county).
-
-        Args:
-            state: State to query
-            county: Optional name of county.
-            fips: Optional fips code.
-            column: Beds count column to return.
-
-        Returns: Beds for a county.
-        """
-        if not (county or fips) or (county and fips):
-            raise ValueError("Must only pass fips or county")
-
-        data = self.county_data
-        state_filter = data.state == state
-        if fips:
-            location_filter = data.fips == fips
-        elif county:
-            location_filter = data.county == county
-
-        beds = data[state_filter & location_filter]
-        if len(beds):
-            return beds.iloc[0][column]
-
-        return None

libs/datasets/combined_dataset_utils.py
@@ -1,12 +1,8 @@
-from typing import Tuple
 import pathlib
 import datetime
-
 import structlog
 
 from libs.datasets import dataset_base
-from libs.datasets import timeseries
-from libs.datasets import latest_values_dataset
 from libs.datasets import dataset_utils
 from libs.datasets.dataset_pointer import DatasetPointer
 from libs.github_utils import GitSummary
@@ -44,23 +40,3 @@ def persist_dataset(
     dataset_pointer.save_dataset(dataset)
     dataset_pointer.save(data_directory)
     return dataset_pointer
-
-
-def update_data_public_head(
-    data_directory: pathlib.Path,
-    latest_dataset: latest_values_dataset.LatestValuesDataset,
-    timeseries_dataset: timeseries.MultiRegionDataset,
-) -> Tuple[DatasetPointer, DatasetPointer]:
-    """Persists US latest and timeseries dataset and saves dataset pointers for Latest tag.
-
-    Args:
-        data_directory: Directory to save dataset and pointer.
-        latest_dataset: The LatestValuesDataset to persist for debugging. It is not read downstream.
-        timeseries_dataset: The dataset to persist.
-
-    Returns: Tuple of DatasetPointers to latest and timeseries datasets.
-    """
-    latest_pointer = persist_dataset(latest_dataset, data_directory)
-
-    timeseries_pointer = persist_dataset(timeseries_dataset, data_directory)
-    return latest_pointer, timeseries_pointer

commit e661cbe1b9384a425e8583cd06ec7ca4035aa7ea

attempt at fixing load_data_sources_by_name (#837)
Attempt at fix for error seen at https://covidactnow.slack.com/archives/C011Z21ST8V/p1607427998029100
Breakage was introduced by https://github.com/covid-projections/covid-data-model/pull/829
Stack trace from https://github.com/covid-projections/covid-data-model/runs/1517069782?check_suite_focus=true
```
  File "/home/runner/work/covid-data-model/covid-data-model/covid-data-model/cli/data.py", line 190, in update_availability_report
    data_sources_by_source_name = data_availability.load_all_latest_sources()
  File "/home/runner/work/covid-data-model/covid-data-model/covid-data-model/libs/qa/data_availability.py", line 22, in load_all_latest_sources
    sources = notebook_helpers.load_data_sources_by_name()
  File "/home/runner/work/covid-data-model/covid-data-model/covid-data-model/libs/notebook_helpers.py", line 44, in load_data_sources_by_name
    dataset.data["source"] = source_cls.__name__
  File "/opt/hostedtoolcache/Python/3.7.9/x64/lib/python3.7/site-packages/pandas/core/generic.py", line 5274, in __getattr__
    return object.__getattribute__(self, name)
AttributeError: 'DataFrame' object has no attribute 'data'
```

My first push to this branch had lots of commits. I then force pushed a branch with 1 commit.

libs/notebook_helpers.py
@@ -41,7 +41,7 @@ def load_data_sources_by_name() -> Dict[str, pd.DataFrame]:
     for source_cls in sources:
         dataset = source_cls.local().multi_region_dataset().static_and_timeseries_latest_with_fips()
 
-        dataset.data["source"] = source_cls.__name__
+        dataset["source"] = source_cls.__name__
         source_map[source_cls.__name__] = dataset
 
     return source_map