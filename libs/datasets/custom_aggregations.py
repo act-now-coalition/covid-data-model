c5b55caae2e79c9d0b8019ebe1d5c9cc58d38e7a
Chris Kelly <ghop02@gmail.com>
2021-01-12 09:44:05 -0600

    Fix ignore na reference

--- libs/datasets/custom_aggregations.py
+++ libs/datasets/custom_aggregations.py
@@ -57,9 +57,9 @@ def replace_dc_county_with_state_data(
     static_excluding_numbers = dataset_in.get_regions_subset(
         [dc_county_region]
     ).static.select_dtypes(exclude="number")
-    dc_county_dataset = timeseries.aggregate_regions(
-        dataset_in, dc_map, ignore_na=True
-    ).add_static_values(static_excluding_numbers.reset_index())
+    dc_county_dataset = timeseries.aggregate_regions(dataset_in, dc_map).add_static_values(
+        static_excluding_numbers.reset_index()
+    )
     dataset_without_dc_county = dataset_in.remove_regions([dc_county_region])
 
     return dataset_without_dc_county.append_regions(dc_county_dataset)
