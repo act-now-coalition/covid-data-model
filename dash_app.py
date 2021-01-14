ef9901be682d155188da90ea3e83c1bd7dc43c24
Tom Brown <tomgith4@thecap.org>
2021-01-14 08:38:24 -0800

    use DataTable to browse locations. add annotation_count in table.

--- dash_app.py
+++ dash_app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import more_itertools
 
 import plotly.express as px
 import pandas as pd
@@ -12,6 +13,7 @@ from libs import pipeline
 from libs.datasets import AggregationLevel
 from libs.datasets import combined_datasets
 from libs.datasets import timeseries
+from libs.datasets.tail_filter import TagType
 from libs.datasets.timeseries import TagField
 import pandas as pd
 
@@ -25,24 +27,14 @@ app.scripts.config.serve_locally = True
 
 ds = combined_datasets.load_us_timeseries_dataset().get_subset(exclude_county_999=True)
 
-ds_static = ds.static.reset_index()
-dropdown_df = pd.DataFrame.from_dict(
-    {
-        "value": ds_static[CommonFields.LOCATION_ID],
-        "label": (
-            ds_static[CommonFields.AGGREGATE_LEVEL].astype(str)
-            + " "
-            + ds_static[CommonFields.COUNTY].astype(str)
-            + " "
-            + ds_static[CommonFields.STATE].astype(str)
-            + "("
-            + ds_static[CommonFields.LOCATION_ID].astype(str)
-            + ")"
-        ),
-    }
+df_regions = ds.static
+df_regions["annotation_count"] = (
+    ds.tag.loc[:, :, [TagType.CUMULATIVE_LONG_TAIL_TRUNCATED, TagType.CUMULATIVE_TAIL_TRUNCATED]]
+    .index.get_level_values(CommonFields.LOCATION_ID)
+    .value_counts()
 )
-dropdown_options = dropdown_df.to_dict("records")
-
+df_regions = df_regions.reset_index()  # Move location_id to from index to regular column
+df_regions["id"] = df_regions[CommonFields.LOCATION_ID]
 
 # These columns are from OneRegion... missing LOCATION_ID of timeseries.TAG_INDEX_FIELDS
 TAG_TABLE_COLUMNS = [TagField.VARIABLE, TagField.TYPE, TagField.DATE, TagField.CONTENT]
@@ -50,16 +42,33 @@ TAG_TABLE_COLUMNS = [TagField.VARIABLE, TagField.TYPE, TagField.DATE, TagField.C
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Hello Dash"),
-        html.Div(
-            children="""
-        Dash: A web application framework for Python.
-    """
+        html.H1(children="CAN Data"),
+        dash_table.DataTable(
+            id="datatable-regions",
+            columns=[
+                {"name": i, "id": i}
+                for i in [
+                    CommonFields.LOCATION_ID,
+                    CommonFields.COUNTY,
+                    CommonFields.STATE,
+                    CommonFields.POPULATION,
+                    "annotation_count",
+                ]
+            ],
+            cell_selectable=False,
+            page_size=10,
+            row_selectable="single",
+            data=df_regions.to_dict("records"),
+            editable=False,
+            filter_action="native",
+            sort_action="native",
+            sort_mode="multi",
+            page_action="native",
+            style_table={"height": "300px", "overflowY": "auto"},
+            selected_rows=[0],
+            sort_by=[{"column_id": CommonFields.POPULATION, "direction": "desc"}],
         ),
-        html.Hr(),
-        dcc.Dropdown(
-            id="location-dropdown", options=dropdown_options, value=dropdown_options[0]["value"]
-        ),
+        html.Hr(),  # Stop graph drawing over table pageination control.
         dcc.Graph(id="example-graph",),
         dash_table.DataTable(id="table", columns=[{"name": i, "id": i} for i in TAG_TABLE_COLUMNS]),
     ]
@@ -72,10 +81,13 @@ app.layout = html.Div(
 # but doesn't in the docs at https://dash.plotly.com/basic-callbacks. Odd.
 @app.callback(
     [Output("example-graph", "figure"), Output("table", "data")],
-    [Input("location-dropdown", "value")],
+    [Input("datatable-regions", "selected_row_ids")],
+    prevent_initial_call=True,
 )
-def update_figure(selected_location):
-    one_region = ds.get_one_region(pipeline.Region.from_location_id(selected_location))
+def update_figure(selected_rows):
+    one_region = ds.get_one_region(
+        pipeline.Region.from_location_id(more_itertools.one(selected_rows))
+    )
     interesting_ts = one_region.data.set_index(CommonFields.DATE)[
         [c for c in one_region.data.columns if ("test" in c or c in ("cases", "deaths"))]
     ]
