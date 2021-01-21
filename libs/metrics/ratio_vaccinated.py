3ec7fe7cedec2d6b478c6c70d72b7c0325bab0fb
Chris Kelly <ghop02@gmail.com>
2021-01-21 16:12:50 -0600

    Update docstrings

--- libs/metrics/ratio_vaccinated.py
+++ libs/metrics/ratio_vaccinated.py
@@ -1,21 +1,27 @@
-import numpy as np
 import pandas as pd
 from libs.datasets import timeseries
 from covidactnow.datapublic.common_fields import CommonFields
 
 
-def calculate_ratio_initiated(timeseries_df: timeseries.OneRegionTimeseriesDataset):
-    """Calculate  ratio.
+def calculate_ratio_initiated(timeseries_df: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
+    """Calculate ratio of population initiating vaccination.
 
     Args:
-        region_df: Data for a specific region.
+        timeseries_df: Data for a single region.
 
-    Returns: np.nan if data missing or pd.Series of ICU capacity.
+    Returns: Series
     """
     population = timeseries_df.latest[CommonFields.POPULATION]
     return timeseries_df.date_indexed[CommonFields.VACCINATIONS_COMPLETED] / population
 
 
-def calculate_ratio_completed(timeseries_df: timeseries.OneRegionTimeseriesDataset):
+def calculate_ratio_completed(timeseries_df: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
+    """Calculate ratio of population completing vaccination.
+
+    Args:
+        timeseries_df: Data for a single region.
+
+    Returns: Series
+    """
     population = timeseries_df.latest[CommonFields.POPULATION]
     return timeseries_df.date_indexed[CommonFields.VACCINATIONS_COMPLETED] / population
