c6a34db9ccfe2779c7fa7653e969e1bc0ac85d11
Chris Kelly <ghop02@gmail.com>
2021-01-21 18:47:32 -0600

    Update variable name

--- libs/metrics/ratio_vaccinated.py
+++ libs/metrics/ratio_vaccinated.py
@@ -3,25 +3,25 @@ from libs.datasets import timeseries
 from covidactnow.datapublic.common_fields import CommonFields
 
 
-def calculate_ratio_initiated(timeseries_df: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
+def calculate_ratio_initiated(dataset: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
     """Calculate ratio of population initiating vaccination.
 
     Args:
-        timeseries_df: Data for a single region.
+        dataset: Data for a single region.
 
     Returns: Series
     """
-    population = timeseries_df.latest[CommonFields.POPULATION]
-    return timeseries_df.date_indexed[CommonFields.VACCINATIONS_INITIATED] / population
+    population = dataset.latest[CommonFields.POPULATION]
+    return dataset.date_indexed[CommonFields.VACCINATIONS_INITIATED] / population
 
 
-def calculate_ratio_completed(timeseries_df: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
+def calculate_ratio_completed(dataset: timeseries.OneRegionTimeseriesDataset) -> pd.Series:
     """Calculate ratio of population completing vaccination.
 
     Args:
-        timeseries_df: Data for a single region.
+        dataset: Data for a single region.
 
     Returns: Series
     """
-    population = timeseries_df.latest[CommonFields.POPULATION]
-    return timeseries_df.date_indexed[CommonFields.VACCINATIONS_COMPLETED] / population
+    population = dataset.latest[CommonFields.POPULATION]
+    return dataset.date_indexed[CommonFields.VACCINATIONS_COMPLETED] / population
