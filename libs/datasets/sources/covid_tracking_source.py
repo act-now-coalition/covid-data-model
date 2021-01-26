518c5b3b078fe6c2720e2402bf7e689617317977
Tom Brown <tomgith4@thecap.org>
2021-01-26 15:16:40 -0800

    add missing fields in covid_tracking_source.py. data update has no diffs.

--- libs/datasets/sources/covid_tracking_source.py
+++ libs/datasets/sources/covid_tracking_source.py
@@ -1,22 +1,12 @@
-from functools import lru_cache
-
-from covidactnow.datapublic import common_df
-
 from covidactnow.datapublic.common_fields import CommonFields
 from libs.datasets import data_source
-from libs.datasets import dataset_utils
-from libs.datasets import timeseries
-from libs.datasets.dataset_utils import TIMESERIES_INDEX_FIELDS
 
 
 class CovidTrackingDataSource(data_source.DataSource):
-    COMMON_DF_CSV_PATH = "data/covid-tracking/timeseries.csv"
     SOURCE_NAME = "covid_tracking"
 
-    # Keep in sync with update_covid_tracking_data in the covid-data-public repo.
-    # DataSource objects must have a map from CommonFields to fields in the source file.
-    # For this source the conversion is done in the covid-data-public repo so the map
-    # here doesn't represent any field renaming.
+    COMMON_DF_CSV_PATH = "data/covid-tracking/timeseries.csv"
+
     EXPECTED_FIELDS = [
         CommonFields.DEATHS,
         CommonFields.CURRENT_HOSPITALIZED,
@@ -26,4 +16,10 @@ class CovidTrackingDataSource(data_source.DataSource):
         CommonFields.CUMULATIVE_ICU,
         CommonFields.POSITIVE_TESTS,
         CommonFields.NEGATIVE_TESTS,
+        CommonFields.POSITIVE_TESTS_VIRAL,
+        CommonFields.POSITIVE_CASES_VIRAL,
+        CommonFields.TOTAL_TESTS,
+        CommonFields.TOTAL_TESTS_VIRAL,
+        CommonFields.TOTAL_TESTS_PEOPLE_VIRAL,
+        CommonFields.TOTAL_TEST_ENCOUNTERS_VIRAL,
     ]
--- libs/datasets/sources/test_and_trace.py
+++ libs/datasets/sources/test_and_trace.py
@@ -7,38 +7,24 @@ from libs.datasets import data_source
 from libs.datasets import dataset_utils
 from libs.datasets import timeseries
 from libs.datasets.dataset_utils import AggregationLevel
-from libs.datasets.dataset_utils import TIMESERIES_INDEX_FIELDS
 
 
 class TestAndTraceData(data_source.DataSource):
     DATA_PATH = "data/test-and-trace/state_data.csv"
     SOURCE_NAME = "TestAndTrace"
 
-    class Fields(object):
-        FIPS = CommonFields.FIPS
-        STATE = CommonFields.STATE
-        DATE = CommonFields.DATE
-        CONTACT_TRACERS = CommonFields.CONTACT_TRACERS_COUNT
-
-        # Not in the source file, added to conform to the expectations of this repo
-        AGGREGATE_LEVEL = "aggregate_level"
-        COUNTRY = "country"
-
     EXPECTED_FIELDS = [
         CommonFields.CONTACT_TRACERS_COUNT,
     ]
-
-    @classmethod
-    def standardize_data(cls, data):
-        data[cls.Fields.COUNTRY] = "USA"
-        data[cls.Fields.AGGREGATE_LEVEL] = AggregationLevel.STATE.value
-        return data
 
     @classmethod
     @lru_cache(None)
     def make_dataset(cls) -> timeseries.MultiRegionDataset:
         data_root = dataset_utils.LOCAL_PUBLIC_DATA_PATH
         input_path = data_root / cls.DATA_PATH
-        data = pd.read_csv(input_path, parse_dates=[cls.Fields.DATE], dtype={cls.Fields.FIPS: str})
-        data = cls.standardize_data(data)
+        data = pd.read_csv(
+            input_path, parse_dates=[CommonFields.DATE], dtype={CommonFields.FIPS: str}
+        )
+        data[CommonFields.COUNTRY] = "USA"
+        data[CommonFields.AGGREGATE_LEVEL] = AggregationLevel.STATE.value
         return cls.make_timeseries_dataset(data)
