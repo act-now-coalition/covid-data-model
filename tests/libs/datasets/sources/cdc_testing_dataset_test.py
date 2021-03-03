e8bcab3640526aee9183ed599db2bc10ec64e92d
Tom Brown <tomgith4@thecap.org>
2021-03-02 23:54:04 -0800

    changed cdc_testing_dataset to work with MultiRegionDataset. got it running and updated test but data update has many mysterious changes.

--- tests/libs/datasets/sources/cdc_testing_dataset_test.py
+++ tests/libs/datasets/sources/cdc_testing_dataset_test.py
@@ -1,33 +1,24 @@
-import io
+import dataclasses
 
 import pandas as pd
-from covidactnow.datapublic import common_df
+from covidactnow.datapublic.common_fields import CommonFields
 
 from libs.datasets.sources import cdc_testing_dataset
+from tests import test_helpers
 
 
 def test_remove_trailing_zeros():
 
-    data_buf = io.StringIO(
-        "fips,date,test_positivity_7d\n"
-        f"48112,2020-08-17,0.5\n"
-        f"48112,2020-08-18,0.6\n"
-        f"48112,2020-08-19,0.0\n"
-        f"48113,2020-08-16,0.0\n"
-        f"48113,2020-08-17,0.0\n"
-        f"48113,2020-08-18,0.0\n"
+    ds_in = test_helpers.build_default_region_dataset(
+        {CommonFields.TEST_POSITIVITY_7D: [0.5, 0.6, 0, 0, 0, 0]}
     )
-    data = common_df.read_csv(data_buf, set_index=False)
-    results = cdc_testing_dataset.remove_trailing_zeros(data)
 
-    expected_buf = io.StringIO(
-        "fips,date,test_positivity_7d\n"
-        f"48112,2020-08-17,0.5\n"
-        f"48112,2020-08-18,0.6\n"
-        f"48112,2020-08-19,\n"
-        f"48113,2020-08-16,\n"
-        f"48113,2020-08-17,\n"
-        f"48113,2020-08-18,\n"
+    ds_out = dataclasses.replace(
+        ds_in, timeseries=cdc_testing_dataset.remove_trailing_zeros(ds_in.timeseries)
     )
-    expected = common_df.read_csv(expected_buf, set_index=False)
-    pd.testing.assert_frame_equal(expected.sort_index(axis=1), results.sort_index(axis=1))
+
+    ds_expected = test_helpers.build_default_region_dataset(
+        {CommonFields.TEST_POSITIVITY_7D: [0.5, 0.6]}
+    )
+
+    test_helpers.assert_dataset_like(ds_out, ds_expected, drop_na_dates=True)
