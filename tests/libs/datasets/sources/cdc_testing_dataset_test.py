fb02f1bdaf379e372fbac6cf8dbefd1e3e17adc2
Tom Brown <tomgith4@thecap.org>
2021-03-08 07:50:52 -0800

    fix test

--- tests/libs/datasets/sources/cdc_testing_dataset_test.py
+++ tests/libs/datasets/sources/cdc_testing_dataset_test.py
@@ -5,21 +5,19 @@ from libs.datasets.sources import cdc_testing_dataset
 from tests import test_helpers
 
 
-def test_remove_trailing_zeros():
+def test_cdc_testing_modify_dataset():
     region_dc_county = pipeline.Region.from_fips("11001")
     region_dc_state = pipeline.Region.from_state("DC")
-    region_sf = pipeline.Region.from_fips("06075")
+    region_other = pipeline.Region.from_fips("06075")
 
-    # Other data, in the test to make sure it isn't dropped
-    other_regions_data = {
-        region_sf: {CommonFields.TEST_POSITIVITY_7D: [10, 20, 30, 40, 50, 60]},
-    }
     dc_data_before_filter = {
         CommonFields.TEST_POSITIVITY_7D: [50, 60, 0, 0, 0, 0],
         CommonFields.CASES: [1, 2, 3, 4, 5, 6],
     }
+    # Check that test positivity is converted from percent to ratio in other regions.
+    other_data_before_filter = {CommonFields.TEST_POSITIVITY_7D: [10, 20, 30, 40, 50, 60]}
     ds_in = test_helpers.build_dataset(
-        {region_dc_county: dc_data_before_filter, **other_regions_data,}
+        {region_dc_county: dc_data_before_filter, region_other: other_data_before_filter}
     )
 
     ds_out = cdc_testing_dataset.modify_dataset(ds_in)
@@ -28,11 +26,12 @@ def test_remove_trailing_zeros():
         CommonFields.TEST_POSITIVITY_7D: [0.5, 0.6, None, None, None, None],
         CommonFields.CASES: [1, 2, 3, 4, 5, 6],
     }
+    other_data_after_filter = {CommonFields.TEST_POSITIVITY_7D: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]}
     ds_expected = test_helpers.build_dataset(
         {
             region_dc_county: dc_data_after_filter,
             region_dc_state: dc_data_after_filter,
-            **other_regions_data,
+            region_other: other_data_after_filter,
         }
     )
 
