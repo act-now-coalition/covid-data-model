67689c0d40ae24f058ef3bc7c4b0d7329d9ba71f
Chris Kelly <ghop02@gmail.com>
2021-03-11 00:26:38 -0500

    fix check for vacc eligibility data

--- libs/build_api_v2.py
+++ libs/build_api_v2.py
@@ -104,7 +104,7 @@ def build_region_summary(
 
     actuals = _build_actuals(latest_values)
     vaccine_eligibility = None
-    if CommonFields.VACCINE_ELIGIBILITY_DATA in latest_values:
+    if latest_values[CommonFields.VACCINE_ELIGIBILITY_DATA] is not None:
         vaccine_eligibility = _build_vaccine_eligibility(
             latest_values[CommonFields.VACCINE_ELIGIBILITY_DATA]
         )
