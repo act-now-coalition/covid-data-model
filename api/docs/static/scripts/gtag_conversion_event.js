849db7c1e8eeab6a00b068666fa5d006b9654b6d
Chris Kelly <ghop02@gmail.com>
2021-03-24 17:59:32 -0500

    Add Google conversion event + hubspot pixel

--- api/docs/static/scripts/gtag_conversion_event.js
+++ api/docs/static/scripts/gtag_conversion_event.js
@@ -1,4 +1,17 @@
-// Track page view conversion event. 
+// Track page view conversion event.
 // Default gtag plugin doesn't support conversion events - this
 // script is included in head to track conversion events.
 gtag("event", "conversion", { send_to: "AW-527465415/lRyqCM2a9-kBEMf3wfsB" });
+
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof url != "undefined") {
+      window.location = url;
+    }
+  };
+  gtag("event", "conversion", {
+    send_to: "AW-527465415/TkWKCNqspv0BEMf3wfsB",
+    event_callback: callback,
+  });
+  return false;
+}
