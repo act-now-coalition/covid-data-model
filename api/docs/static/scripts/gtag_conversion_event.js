d685be047ac601f644e25b34b1f7ac9c056fea64
Chris Kelly <ghop02@gmail.com>
2021-03-24 19:35:09 -0500

    Modify tracking function to not include url logic

--- api/docs/static/scripts/gtag_conversion_event.js
+++ api/docs/static/scripts/gtag_conversion_event.js
@@ -3,15 +3,10 @@
 // script is included in head to track conversion events.
 gtag("event", "conversion", { send_to: "AW-527465415/lRyqCM2a9-kBEMf3wfsB" });
 
-function gtag_report_conversion(url) {
-  var callback = function () {
-    if (typeof url != "undefined") {
-      window.location = url;
-    }
-  };
+// Track API Signup
+function gtag_report_conversion() {
   gtag("event", "conversion", {
     send_to: "AW-527465415/TkWKCNqspv0BEMf3wfsB",
-    event_callback: callback,
   });
   return false;
 }
