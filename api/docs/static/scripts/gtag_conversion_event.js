ed413b53c48dce5f74f08c877e7ab2277565299d
Chris Kelly <ghop02@gmail.com>
2021-03-24 19:36:55 -0500

    Add Google conversion event + hubspot pixel (#1041)
    * Add Google conversion event + hubspot pixel
    
    * Modify tracking function to not include url logic

--- api/docs/static/scripts/gtag_conversion_event.js
+++ api/docs/static/scripts/gtag_conversion_event.js
@@ -1,4 +1,12 @@
-// Track page view conversion event. 
+// Track page view conversion event.
 // Default gtag plugin doesn't support conversion events - this
 // script is included in head to track conversion events.
 gtag("event", "conversion", { send_to: "AW-527465415/lRyqCM2a9-kBEMf3wfsB" });
+
+// Track API Signup
+function gtag_report_conversion() {
+  gtag("event", "conversion", {
+    send_to: "AW-527465415/TkWKCNqspv0BEMf3wfsB",
+  });
+  return false;
+}
