13acd6035594829478bdb62ec915e754bba52416
Tom Brown <tomgith4@thecap.org>
2021-03-29 13:50:12 -0700

    fix test

--- tests/pyseir/utils_test.py
+++ tests/pyseir/utils_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from libs.pipeline import Region
@@ -5,17 +6,22 @@ from pyseir.utils import RunArtifact
 from pyseir.utils import get_run_artifact_path
 
 
-def test():
-    output_dir = "/t"
-    with unittest.mock.patch("pyseir.utils.OUTPUT_DIR", output_dir):
+def test(tmp_path):
+    with unittest.mock.patch("pyseir.utils.OUTPUT_DIR", str(tmp_path)):
 
         path = get_run_artifact_path(Region.from_state("TX"), RunArtifact.RT_INFERENCE_REPORT)
-        assert path == "/t/pyseir/state_summaries/reports/Rt_results__Texas__48.pdf"
+        assert path == os.path.join(
+            tmp_path, "pyseir/state_summaries/reports/Rt_results__Texas__48.pdf"
+        )
 
         path = get_run_artifact_path(Region.from_fips("48301"), RunArtifact.RT_SMOOTHING_REPORT)
-        assert path == "/t/pyseir/Texas/reports/Rt_smoothing__Texas__Loving County__48301.pdf"
+        assert path == os.path.join(
+            tmp_path, "pyseir/Texas/reports/Rt_smoothing__Texas__Loving County__48301.pdf"
+        )
 
         path = get_run_artifact_path(
             Region.from_cbsa_code("10100"), RunArtifact.RT_INFERENCE_REPORT
         )
-        assert path == "/t/pyseir/state_summaries/reports/Rt_results__CBSA__10100.pdf"
+        assert path == os.path.join(
+            tmp_path, "pyseir/state_summaries/reports/Rt_results__CBSA__10100.pdf"
+        )
