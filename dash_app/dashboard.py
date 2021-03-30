a53e826c3565c5fe22e96a6b37de5bbfcf96ef29
Tom Brown <tom@covidactnow.org>
2021-03-30 09:29:56 -0700

    Interactive pivot table on data dashboard (#1055)
    This PR adds https://github.com/plotly/dash-pivottable to our data dashboard. This makes it really easy to interactively make most of the other plots on the dashboard and many, many more.

--- dash_app/dashboard.py
+++ dash_app/dashboard.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_pivottable
 import dash_table
 import git
 import more_itertools
@@ -100,6 +101,9 @@ def init(server):
 
     region_df = region_table(per_timeseries_stats, ds)
 
+    df = per_timeseries_stats.stats.reset_index()
+    pivottable_data = [df.columns.tolist()] + df.values.tolist()
+
     dash_app.layout = html.Div(
         children=[
             html.H1(children="CAN Data Pipeline Dashboard"),
@@ -113,6 +117,8 @@ def init(server):
             dash_table_from_data_frame(
                 agg_level_and_distribution.has_timeseries, id="distribution_timeseries_count",
             ),
+            html.H2("Interactive pivot table"),
+            dash_pivottable.PivotTable(id="pivot_table", data=pivottable_data),
             html.H2("Source URLs"),
             dash_table_from_data_frame(
                 source_url_value_counts, id="source_url_counts", page_size=8
