7eab8cef5ab53cb8041953316bfcf931fe15de3d
Tom Brown <tomgith4@thecap.org>
2021-04-21 15:05:18 -0700

    rm ObservationsToDrop and add manual_filter.Action because future change will reuse the selection stuff but not drop observations.

--- libs/datasets/manual_filter.py
+++ libs/datasets/manual_filter.py
@@ -1,11 +1,11 @@
 import datetime
+import enum
 from typing import List
 from typing import Optional
 from typing import Tuple
 
 import pydantic
 import structlog
-from covidactnow.datapublic import common_fields
 from covidactnow.datapublic.common_fields import CommonFields
 from covidactnow.datapublic.common_fields import PdFields
 import pandas as pd
@@ -20,32 +20,23 @@ _logger = structlog.getLogger()
 
 
 # The manual filter config is defined with pydantic instead of standard dataclasses to get support
-# for parsing from json, automatic conversion of fields to the expected type and fields that default
-# to None. pydantic reserves the field name 'fields'.
+# for parsing from json, automatic conversion of fields to the expected type and avoiding the
+# dreaded "TypeError: non-default argument ...". pydantic reserves the field name 'fields'.
 
 
-class ObservationsToDrop(pydantic.BaseModel):
-    start_date: datetime.date
-    drop_fields: Optional[List[CommonFields]] = None
-    drop_field_group: Optional[common_fields.FieldGroup] = None
-    internal_note: str
-    public_note: str
-
-    @property
-    def drop_field_list(self) -> List[CommonFields]:
-        if self.drop_fields is not None:
-            assert self.drop_field_group is None
-            return self.drop_fields
-        else:
-            # self.drop_fields is None
-            assert self.drop_field_group is not None
-            return common_fields.FIELD_GROUP_TO_LIST_FIELDS[self.drop_field_group]
+class Action(enum.Enum):
+    DROP_OBSERVATIONS = enum.auto()
+    ANNOTATE = enum.auto()
 
 
 class Filter(pydantic.BaseModel):
     regions_included: List[RegionMaskOrRegion]
     regions_excluded: Optional[List[RegionMaskOrRegion]] = None
-    observations_to_drop: ObservationsToDrop
+    fields_included: List[CommonFields]
+    action: Action
+    start_date: Optional[datetime.date] = None
+    internal_note: str
+    public_note: str
 
 
 class Config(pydantic.BaseModel):
@@ -57,12 +48,11 @@ CONFIG = Config(
         Filter(
             regions_included=[RegionMask(AggregationLevel.COUNTY, states=["OK"])],
             regions_excluded=[Region.from_fips("40109"), Region.from_fips("40143")],
-            observations_to_drop=ObservationsToDrop(
-                start_date="2021-03-15",
-                drop_fields=[CommonFields.CASES, CommonFields.DEATHS],
-                internal_note="https://trello.com/c/HdAKfp49/1139",
-                public_note="Something broke with the OK county data.",
-            ),
+            fields_included=[CommonFields.CASES, CommonFields.DEATHS],
+            action=Action.ANNOTATE,
+            start_date="2021-03-15",
+            internal_note="https://trello.com/c/HdAKfp49/1139",
+            public_note="Something broke with the OK county data.",
         )
     ]
 )
@@ -95,18 +85,18 @@ def _filter_by_date(
 
 
 def drop_observations(
-    dataset: timeseries.MultiRegionDataset, config: ObservationsToDrop
+    dataset: timeseries.MultiRegionDataset, filter_: Filter
 ) -> timeseries.MultiRegionDataset:
     """Drops observations according to `config` from every region in `dataset`."""
     ts_selected_fields, ts_not_selected_fields = _partition_by_fields(
-        dataset.timeseries_bucketed_wide_dates, config.drop_field_list
+        dataset.timeseries_bucketed_wide_dates, filter_.fields_included
     )
 
     ts_filtered, ts_no_real_values_to_drop = _filter_by_date(
-        ts_selected_fields, drop_start_date=config.start_date
+        ts_selected_fields, drop_start_date=filter_.start_date
     )
 
-    tag = taglib.KnownIssue(date=config.start_date, disclaimer=config.public_note)
+    tag = taglib.KnownIssue(date=filter_.start_date, disclaimer=filter_.public_note)
 
     new_tags = taglib.TagCollection()
     new_tags.add_by_index(tag, index=ts_filtered.index)
@@ -127,7 +117,7 @@ def run(
             # TODO(tom): Find a cleaner way to refer to a filter in logs.
             _logger.info("No locations matched", regions=str(filter_.regions_included))
             continue
-        filtered_dataset = drop_observations(filtered_dataset, filter_.observations_to_drop)
+        filtered_dataset = drop_observations(filtered_dataset, filter_)
 
         dataset = filtered_dataset.append_regions(passed_dataset)
 
--- tests/libs/datasets/manual_filter_test.py
+++ tests/libs/datasets/manual_filter_test.py
@@ -1,3 +1,4 @@
+from covidactnow.datapublic import common_fields
 from covidactnow.datapublic.common_fields import CommonFields
 from covidactnow.datapublic.common_fields import DemographicBucket
 from covidactnow.datapublic.common_fields import FieldGroup
@@ -20,23 +21,23 @@ TEST_CONFIG = manual_filter.Config.parse_obj(
                     Region.from_fips("49013"),
                     Region.from_fips("49047"),
                 ],
-                "observations_to_drop": {
-                    "start_date": "2021-02-12",
-                    "drop_fields": [CommonFields.CASES, CommonFields.DEATHS],
-                    "internal_note": "https://trello.com/c/aj7ep7S7/1130",
-                    "public_note": "The TriCounty Health Department is focusing on vaccinations "
-                    "and we have not found a new source of case counts.",
-                },
+                "start_date": "2021-02-12",
+                "fields_included": [CommonFields.CASES, CommonFields.DEATHS],
+                "internal_note": "https://trello.com/c/aj7ep7S7/1130",
+                "public_note": "The TriCounty Health Department is focusing on vaccinations "
+                "and we have not found a new source of case counts.",
+                "action": manual_filter.Action.DROP_OBSERVATIONS,
             },
             {
                 "regions_included": [RegionMask(AggregationLevel.COUNTY, states=["OK"])],
                 "regions_excluded": [Region.from_fips("40109"), Region.from_fips("40143")],
-                "observations_to_drop": {
-                    "start_date": "2021-03-15",
-                    "drop_field_group": FieldGroup.CASES_DEATHS,
-                    "internal_note": "https://trello.com/c/HdAKfp49/1139",
-                    "public_note": "Something broke with the OK county data.",
-                },
+                "start_date": "2021-03-15",
+                "fields_included": common_fields.FIELD_GROUP_TO_LIST_FIELDS[
+                    FieldGroup.CASES_DEATHS
+                ],
+                "internal_note": "https://trello.com/c/HdAKfp49/1139",
+                "public_note": "Something broke with the OK county data.",
+                "action": manual_filter.Action.DROP_OBSERVATIONS,
             },
         ]
     }
@@ -55,7 +56,7 @@ def test_manual_filter():
     tag_expected = test_helpers.make_tag(
         taglib.TagType.KNOWN_ISSUE,
         date="2021-02-12",
-        disclaimer=TEST_CONFIG.filters[0].observations_to_drop.public_note,
+        disclaimer=TEST_CONFIG.filters[0].public_note,
     )
     ds_expected = test_helpers.build_dataset(
         {
@@ -81,7 +82,7 @@ def test_manual_filter_region_excluded():
     tag_expected = test_helpers.make_tag(
         taglib.TagType.KNOWN_ISSUE,
         date="2021-03-15",
-        disclaimer=TEST_CONFIG.filters[1].observations_to_drop.public_note,
+        disclaimer=TEST_CONFIG.filters[1].public_note,
     )
     ds_expected = test_helpers.build_dataset(
         {
@@ -110,7 +111,7 @@ def test_manual_filter_field_groups():
     tag_expected = test_helpers.make_tag(
         taglib.TagType.KNOWN_ISSUE,
         date="2021-03-15",
-        disclaimer=TEST_CONFIG.filters[1].observations_to_drop.public_note,
+        disclaimer=TEST_CONFIG.filters[1].public_note,
     )
     ds_expected = test_helpers.build_default_region_dataset(
         {CommonFields.DEATHS: TimeseriesLiteral([1], annotation=[tag_expected]), **other_data},
@@ -141,7 +142,7 @@ def test_manual_filter_per_bucket_tag():
     tag_expected = test_helpers.make_tag(
         taglib.TagType.KNOWN_ISSUE,
         date="2021-03-15",
-        disclaimer=TEST_CONFIG.filters[1].observations_to_drop.public_note,
+        disclaimer=TEST_CONFIG.filters[1].public_note,
     )
     ds_expected = test_helpers.build_default_region_dataset(
         {
