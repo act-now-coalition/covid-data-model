45aa7b4f65c273e86fd9d5c6dc97ed137cf9eaee
Tom Brown <tomgith4@thecap.org>
2021-05-07 08:40:22 -0700

    some renaming and tests pass.

--- tests/cli/data_cli_test.py
+++ tests/cli/data_cli_test.py
@@ -23,7 +23,7 @@ def test_run_aggregate_to_country():
     region_il = Region.from_state("IL")
     region_us = Region.from_iso1("us")
 
-    others_metrics = {
+    states_timeseries = {
         region_il: {
             CommonFields.ALL_BED_TYPICAL_OCCUPANCY_RATE: [0.5, 0.5],
             CommonFields.CASES: [1, 2],
@@ -33,31 +33,40 @@ def test_run_aggregate_to_country():
             CommonFields.CASES: [0, 3],
         },
     }
-    others_static = {
+    states_static = {
         r: {CommonFields.POPULATION: 100, CommonFields.MAX_BED_COUNT: 10, CommonFields.ICU_BEDS: 2}
         for r in [region_il, region_tx]
     }
 
     ds_in = test_helpers.build_dataset(
         {
-            **others_metrics,
+            **states_timeseries,
             # region_us input intentionally not the sum of states to check that the input is preserved.
             region_us: {CommonFields.CASES: [2, 6]},
         },
-        static_by_region_then_field_name=others_static,
+        static_by_region_then_field_name=states_static,
     )
     ds_out = data.run_aggregate_to_country(ds_in)
 
     ds_expected = test_helpers.build_dataset(
         {
-            **others_metrics,
+            **states_timeseries,
             # region_us input intentionally not the sum of states to check that the input is preserved.
             region_us: {
                 CommonFields.CASES: [2, 6],
                 CommonFields.ALL_BED_TYPICAL_OCCUPANCY_RATE: [0.6, 0.6],
             },
         },
-        static_by_region_then_field_name=others_static,
+        static_by_region_then_field_name={
+            **states_static,
+            **{
+                region_us: {
+                    CommonFields.POPULATION: 200,
+                    CommonFields.MAX_BED_COUNT: 20,
+                    CommonFields.ICU_BEDS: 4,
+                }
+            },
+        },
     )
 
     test_helpers.assert_dataset_like(ds_out, ds_expected)
