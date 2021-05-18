act-now-coalition/covid-data-model

commit 08fe29e1834a0ceedcf18c88ac29d77293133355

add drop_observations after today UTC (#1113)
This PR addresses https://trello.com/c/UKqdcj0z/1345-pipeline-block-data-for-future-dates

* Adds function `timeseries.drop_observations(..., after=)` and tag `drop_future_observation` to record where it did something
* Use this function to drop data that is more recent than today, UTC
* Clean up print_stats calls so "combined" is right after the combined data is produced and adding "manual filter" stats

### Tested

Running `./run.py data update` with covid-data-public manually tweaked to have a date in the future.

libs/datasets/taglib.py
@@ -59,6 +59,7 @@ class TagType(GetByValueMixin, ValueAsStrMixin, str, enum.Enum):
     KNOWN_ISSUE = "known_issue"
     KNOWN_ISSUE_NO_DATE = "known_issue_no_date"
     DERIVED = "derived"
+    DROP_FUTURE_OBSERVATION = "drop_future_observation"
 
     PROVENANCE = PdFields.PROVENANCE
     SOURCE_URL = "source_url"
@@ -328,6 +329,22 @@ class Derived(TagInTimeseries):
         return json.dumps(d, separators=(",", ":"))
 
 
+@dataclass(frozen=True)
+class DropFutureObservation(TagInTimeseries):
+    TAG_TYPE = TagType.DROP_FUTURE_OBSERVATION
+    after: datetime.date
+
+    @classmethod
+    def make_instance(cls, *, content: str) -> "TagInTimeseries":
+        content_parsed = json.loads(content)
+        return cls(after=datetime.date.fromisoformat(content_parsed["after"]),)
+
+    @property
+    def content(self) -> str:
+        d = {"after": self.after.isoformat()}
+        return json.dumps(d, separators=(",", ":"))
+
+
 TAG_TYPE_TO_CLASS = {
     TagType.CUMULATIVE_TAIL_TRUNCATED: CumulativeTailTruncated,
     TagType.CUMULATIVE_LONG_TAIL_TRUNCATED: CumulativeLongTailTruncated,
@@ -338,6 +355,7 @@ TAG_TYPE_TO_CLASS = {
     TagType.KNOWN_ISSUE: KnownIssue,
     TagType.KNOWN_ISSUE_NO_DATE: KnownIssueNoDate,
     TagType.DERIVED: Derived,
+    TagType.DROP_FUTURE_OBSERVATION: DropFutureObservation,
 }
 
 

cli/data.py
@@ -1,4 +1,5 @@
 import dataclasses
+import datetime
 from typing import List
 from typing import Mapping
 from typing import Optional
@@ -111,6 +112,7 @@ def update(
         timeseries_field_datasets, static_field_datasets
     )
     _logger.info("Finished combining datasets")
+    multiregion_dataset.print_stats("combined")
 
     # Apply manual overrides (currently only removing timeseries) before aggregation so we don't
     # need to remove CBSAs because they don't exist yet.
@@ -125,8 +127,12 @@ def update(
         dataset_utils.MANUAL_FILTER_REMOVED_WIDE_DATES_CSV_PATH,
         dataset_utils.MANUAL_FILTER_REMOVED_STATIC_CSV_PATH,
     )
+    multiregion_dataset.print_stats("manual filter")
 
-    multiregion_dataset.print_stats("combined")
+    multiregion_dataset = timeseries.drop_observations(
+        multiregion_dataset, after=datetime.datetime.utcnow().date()
+    )
+
     multiregion_dataset = outlier_detection.drop_tail_positivity_outliers(multiregion_dataset)
     multiregion_dataset.print_stats("drop_tail")
     # Filter for stalled cumulative values before deriving NEW_CASES from CASES.

libs/datasets/timeseries.py
@@ -1392,6 +1392,20 @@ def drop_regions_without_population(
     return dataset.get_locations_subset(location_id_with_population)
 
 
+def drop_observations(
+    dataset_in: MultiRegionDataset, *, after: datetime.date
+) -> MultiRegionDataset:
+    wide_dates_df = dataset_in.timeseries_bucketed_wide_dates
+    after_columns_mask = wide_dates_df.columns > pd.to_datetime(after)
+    after_notna_index_mask = wide_dates_df.loc[:, after_columns_mask].notna().any(axis="columns")
+    after_notna_index = wide_dates_df.loc[after_notna_index_mask, :].index
+    wide_dates_not_after_df = wide_dates_df.loc[:, ~after_columns_mask]
+    tag = taglib.DropFutureObservation(after=after)
+    return dataset_in.replace_timeseries_wide_dates([wide_dates_not_after_df]).add_tag_to_subset(
+        tag, after_notna_index
+    )
+
+
 class DatasetName(str):
     """Human readable name for a dataset. In the future this may be an enum, for now it
     provides some type safety."""

tests/libs/datasets/timeseries_test.py
@@ -2021,3 +2021,33 @@ def test_delta_timeseries_removed():
     ds_expected = test_helpers.build_dataset({region_la: {CommonFields.CASES: {age_40s: [1, 2]}}})
 
     test_helpers.assert_dataset_like(ds_out, ds_expected)
+
+
+def test_drop_observations_after():
+    age_40s = DemographicBucket("age:40-49")
+    ds_in = test_helpers.build_default_region_dataset(
+        {
+            CommonFields.CASES: {DemographicBucket.ALL: [5, 10], age_40s: [1, 2, 3]},
+            # Check that observation is dropped even when not a True value (ie 0).
+            CommonFields.DEATHS: [0, 0, 0],
+            # Check what happens when there are no real valued observations after dropping,
+            # though the behaviour probably doesn't matter.
+            CommonFields.ICU_BEDS: [None, None, 10],
+        }
+    )
+
+    ds_out = timeseries.drop_observations(ds_in, after=datetime.date(2020, 4, 2))
+
+    tag = test_helpers.make_tag(taglib.TagType.DROP_FUTURE_OBSERVATION, after="2020-04-02")
+    ds_expected = test_helpers.build_default_region_dataset(
+        {
+            CommonFields.CASES: {
+                DemographicBucket.ALL: [5, 10],
+                age_40s: TimeseriesLiteral([1, 2], annotation=[tag]),
+            },
+            CommonFields.DEATHS: TimeseriesLiteral([0, 0], annotation=[tag]),
+            CommonFields.ICU_BEDS: TimeseriesLiteral([], annotation=[tag]),
+        }
+    )
+
+    test_helpers.assert_dataset_like(ds_out, ds_expected)

tests/test_helpers.py
@@ -104,6 +104,9 @@ def make_tag(
         kwargs["date"] = pd.to_datetime(kwargs.get("date", "2020-04-02"))
     elif tag_type is taglib.TagType.KNOWN_ISSUE:
         kwargs["date"] = pd.to_datetime(kwargs.get("date", "2020-04-02")).date()
+    elif tag_type is taglib.TagType.DROP_FUTURE_OBSERVATION:
+        # make_tag does not have a default `after`; force tests to provide it explicitly.
+        kwargs["after"] = pd.to_datetime(kwargs["after"]).date()
 
     return taglib.TAG_TYPE_TO_CLASS[tag_type](**kwargs)
 

commit afe36632034a682ea1dcf914fcf49ac0d50f4896

touched_subset only returns time series with dropped observations (#1115)
This PR is last remaining part of https://trello.com/c/1p0uTkYH/1300-pipeline-generate-csv-of-blocked-metrics-and-make-it-easy-for-data-dashboard-to-read-it

Follow up on https://github.com/covid-projections/covid-data-model/pull/1108#issuecomment-842694398 to only put timeseries that actually had observations dropped in data/manual_filter_removed-wide-dates.csv

This also fixes a test breakage introduced by 08fe29e1834.

### Tested

Ran `./run.py data update` with main c76029ee merged in and covid-data-public in sync. Changes only seen in data/manual_filter_removed-wide-dates.csv. They are removal of CA county cases, CT state tests, IL county vaccine, NY state and county tests. This matches what is `blocked: false` in data/region-overrides.json

libs/datasets/manual_filter.py
@@ -153,23 +153,41 @@ def touched_subset(
     ds_in: timeseries.MultiRegionDataset, ds_out: timeseries.MultiRegionDataset
 ) -> timeseries.MultiRegionDataset:
     """Given the input and output of `run`, returns the time series from ds_in that have tags in
-    ds_out added by `run` combined with tags of these time series copied from ds_out."""
-    # Expected tags do not appear in the input
+    ds_out added by `run` and dropped observations, combined with tags of these time series
+    copied from ds_out."""
+    # Check that tags added by `run` do not appear in the input of `run`.
     assert ds_in.tag.index.unique(taglib.TagField.TYPE).intersection(_EXPECTED_TYPES).empty
     tag_mask = ds_out.tag.index.get_level_values(taglib.TagField.TYPE).isin(_EXPECTED_TYPES)
-    # touch_index identifies the subset of time series rows that have an _EXPECTED_TYPES tag.
-    touch_index = ds_out.tag.index[tag_mask].droplevel(taglib.TagField.TYPE).unique()
+    # ts_has_tag_index identifies the subset of time series rows that have an _EXPECTED_TYPES tag.
+    ts_has_tag_index = ds_out.tag.index[tag_mask].droplevel(taglib.TagField.TYPE).unique()
     wide_dates_in_df = ds_in.timeseries_bucketed_wide_dates
-    assert wide_dates_in_df.index.names == touch_index.names
-    assert touch_index.isin(wide_dates_in_df.index).all()
-    # Get all ds_in.tag associated with any time series in touch_index
-    tag_in = ds_in.tag.loc[ds_in.tag.index.droplevel(taglib.TagField.TYPE).isin(touch_index)]
-    # Get only the ds_out.tag where the type is _EXPECTED_TYPES. These are the tags added by
-    # `run` and not in ds_in.tag.
-    tag_out = ds_out.tag.loc[tag_mask]
+    assert wide_dates_in_df.index.names == ts_has_tag_index.names
+    assert ts_has_tag_index.isin(wide_dates_in_df.index).all()
+    # For time series identified by ts_has_tag_index find the number of observations per time
+    # series in ds_in and ds_out.
+    in_observations_by_ts = wide_dates_in_df.reindex(ts_has_tag_index).notna().sum(axis="columns")
+    out_observations_by_ts = (
+        ds_out.timeseries_bucketed_wide_dates.reindex(ts_has_tag_index).notna().sum(axis="columns")
+    )
+    # Make an index which identifies time series that had a decrease in number of observations
+    # from ds_in to ds_out.
+    ts_lost_observation_mask = out_observations_by_ts < in_observations_by_ts
+    ts_lost_observation_index = in_observations_by_ts.loc[ts_lost_observation_mask].index
+
+    def tag_xs(tag: pd.Series, ts_index: pd.MultiIndex) -> pd.Series:
+        """Return the cross-section of `tag` that has index labels in `ts_index`."""
+        tag_ts_index = tag.index.droplevel([taglib.TagField.TYPE])
+        assert tag_ts_index.names == ts_index.names
+        return tag.loc[tag_ts_index.isin(ts_index)]
+
+    # Get all ds_in.tag associated with any time series in ts_lost_observation_index.
+    tag_in = tag_xs(ds_in.tag, ts_lost_observation_index)
+    # Get only the ds_out.tag where the type is _EXPECTED_TYPES and observations were lost. This
+    # avoids duplicating tags in tag_in.
+    tag_out = tag_xs(ds_out.tag.loc[tag_mask], ts_lost_observation_index)
     return (
         timeseries.MultiRegionDataset.from_timeseries_wide_dates_df(
-            wide_dates_in_df.reindex(touch_index), bucketed=True
+            wide_dates_in_df.reindex(ts_lost_observation_index), bucketed=True
         )
         .append_tag_df(tag_in.reset_index())
         .append_tag_df(tag_out.reset_index())

libs/datasets/taglib.py
@@ -60,6 +60,8 @@ class TagType(GetByValueMixin, ValueAsStrMixin, str, enum.Enum):
     KNOWN_ISSUE_NO_DATE = "known_issue_no_date"
     DERIVED = "derived"
     DROP_FUTURE_OBSERVATION = "drop_future_observation"
+    # When adding a TagType remember to update timeseries_stats.StatName so it is displayed in
+    # the dashboard.
 
     PROVENANCE = PdFields.PROVENANCE
     SOURCE_URL = "source_url"

libs/qa/timeseries_stats.py
@@ -43,6 +43,7 @@ class StatName(ValueAsStrMixin, str, enum.Enum):
     KNOWN_ISSUE = TagType.KNOWN_ISSUE
     KNOWN_ISSUE_NO_DATE = TagType.KNOWN_ISSUE_NO_DATE
     DERIVED = TagType.DERIVED
+    DROP_FUTURE_OBSERVATION = TagType.DROP_FUTURE_OBSERVATION
     PROVENANCE = TagType.PROVENANCE
     SOURCE_URL = TagType.SOURCE_URL
     SOURCE = TagType.SOURCE

tests/libs/datasets/manual_filter_test.py
@@ -383,3 +383,54 @@ def test_touched_subset():
         }
     )
     test_helpers.assert_dataset_like(ds_touched, ds_touched_expected)
+
+
+def test_touched_subset_only_observation_drops():
+    # (Ab)use the demographic buckets as a way to identify time series.
+    bucket_no_issue = DemographicBucket("b1")
+    bucket_has_tag = DemographicBucket("b2")
+    # Make 6 time series. half will get a KNOWN_ISSUE tag, half won't.
+    ds_in = test_helpers.build_default_region_dataset(
+        {
+            CommonFields.CASES: {bucket_no_issue: [1, 2], bucket_has_tag: [3, 4]},
+            CommonFields.DEATHS: {bucket_no_issue: [5, 6], bucket_has_tag: [7, 8]},
+            CommonFields.ICU_BEDS: {bucket_no_issue: [9, 1], bucket_has_tag: [2, 3]},
+        }
+    )
+    known_issue = test_helpers.make_tag(
+        taglib.TagType.KNOWN_ISSUE, disclaimer="foo", date="2021-04-01"
+    )
+    # ds_out is a mock of what is returned by `manual_filter.run`.
+    ds_out = test_helpers.build_default_region_dataset(
+        {
+            # Drop all observations of CASES.
+            CommonFields.CASES: {
+                bucket_no_issue: [None, None],
+                bucket_has_tag: TimeseriesLiteral([None, None], annotation=[known_issue]),
+            },
+            # Drop one observation of each DEATHS time series.
+            CommonFields.DEATHS: {
+                bucket_no_issue: [5, None],
+                bucket_has_tag: TimeseriesLiteral([7, None], annotation=[known_issue]),
+            },
+            # Drop no observations of ICU_BEDS.
+            CommonFields.ICU_BEDS: {
+                bucket_no_issue: [9, 1],
+                bucket_has_tag: TimeseriesLiteral([2, 3], annotation=[known_issue]),
+            },
+        }
+    )
+    ds_touched = manual_filter.touched_subset(ds_in, ds_out)
+    ds_touched_expected = test_helpers.build_default_region_dataset(
+        {
+            # Only time series that had the tag and at least one dropped observation are put in
+            # the touched dataset.
+            CommonFields.CASES: {
+                bucket_has_tag: TimeseriesLiteral([3, 4], annotation=[known_issue]),
+            },
+            CommonFields.DEATHS: {
+                bucket_has_tag: TimeseriesLiteral([7, 8], annotation=[known_issue]),
+            },
+        }
+    )
+    test_helpers.assert_dataset_like(ds_touched, ds_touched_expected)