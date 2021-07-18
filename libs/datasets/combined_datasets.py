5e12d3828322800fed5ad1c9ab55b91209a2f949
Michael Lehenbauer <mikelehen@gmail.com>
2021-07-18 10:30:06 -0700

    Tweak exclusions.

--- libs/datasets/combined_datasets.py
+++ libs/datasets/combined_datasets.py
@@ -179,7 +179,7 @@ CDCVaccinesStatesAndNationDataset = datasource_regions(
     CDCVaccinesDataset, [RegionMask(AggregationLevel.STATE), RegionMask(AggregationLevel.COUNTRY)]
 )
 
-CDC_EXCLUSIONS = RegionMask(states=["IL", "KS", "NM", "ND", "VT", "WV", "GA", "VA"])
+CDC_EXCLUSIONS = RegionMask(states=["GA", "IL", "NM", "ND", "SD", "VA", "VT", "WV"])
 CDCVaccineDatasetWithoutExceptions = datasource_regions(CDCVaccinesDataset2, exclude=CDC_EXCLUSIONS)
 
 # Excludes FL counties for vaccine fields. See
