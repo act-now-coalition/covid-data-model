a92283d05c3be1d529298ccd1a411e502961b09f
Chris Kelly <ghop02@gmail.com>
2021-08-25 15:14:03 -0400

    Try to fetch

--- tools/push-data-update.sh
+++ tools/push-data-update.sh
@@ -4,5 +4,7 @@ now=$(date)
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add -A data
-git commit -m "Updating saved datasets at $now"
+echo ${GITHUB_REF}
+git commit --allow-empty -m "Updating saved datasets at $now"
+git fetch
 git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
