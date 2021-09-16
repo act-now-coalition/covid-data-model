ae1790e27894c5ff804138d6acec5bffef1e41fc
Michael Lehenbauer <mikelehen@gmail.com>
2021-09-16 12:00:48 -0700

    Improve comment about 'unused' values.

--- libs/pipelines/api_v2_pipeline.py
+++ libs/pipelines/api_v2_pipeline.py
@@ -267,9 +267,11 @@ def _model_to_dict(data: dict):
         if isinstance(value, pydantic.BaseModel):
             value = _model_to_dict(value.__dict__)
 
-            # We always add an "unused" entry that will be used for any "unused" columns
-            # in the CSV output (when we remove an API column we replace it with
-            # "unused" to preserve column ordering).
+            # When converting API model objects to dicts, we always add an
+            # "unused" entry to the dict. This allows us to use "unused" as a
+            # column in our CSV output to have a blank column. This is handy
+            # since when we remove an API column we replace it with "unused" to
+            # preserve column ordering).
             results["unused"] = None
 
         if isinstance(value, list):
