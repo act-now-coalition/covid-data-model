9cd10d8a75f51c771601a903d5e1988f30129d6f
Sean McClure <sean.mcclure17@gmail.com>
2021-11-29 19:53:37 -0500

    reduce processes from 64 to 32

--- libs/parallel_utils.py
+++ libs/parallel_utils.py
@@ -33,9 +33,9 @@ def parallel_map(func: Callable[[T], R], iterable: Iterable[T]) -> Iterable[R]:
     if USE_MULTIPROCESSING:
         # Setting maxtasksperchild to one ensures that we minimize memory usage over time by creating
         # a new child for every task. Addresses OOMs we saw on highly parallel build machine.
-        # But that might not be enough. Also make sure we don't spawn more than 64 processes (the
+        # But that might not be enough. Also make sure we don't spawn more than 32 processes (the
         # build machine is 96-core)
-        processes = min(os.cpu_count(), 64)
+        processes = min(os.cpu_count(), 32)
         with multiprocessing.Pool(maxtasksperchild=1, processes=processes) as pool:
             # Always return an iterator to make sure the return type is consistent.
             return iter(pool.map(func, iterable))
