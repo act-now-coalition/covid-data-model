660c832f4b7ef43e4a6b5423900edab491639f77
Michael Lehenbauer <mikelehen@gmail.com>
2021-11-29 18:44:19 -0800

    Make data update pull right before pushing in case other changes were committed.

--- tools/push-data-update.sh
+++ tools/push-data-update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do a fresh pull in case any changes have been pushed to the branch.
+git pull "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
+
 now=$(date)
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
