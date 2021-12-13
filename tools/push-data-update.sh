16acdc4476df658d64f9e681c2bec3784650647a
Sean McClure <sean.mcclure17@gmail.com>
2021-12-13 12:59:37 -0500

    add back pull, verbose git logging

--- tools/push-data-update.sh
+++ tools/push-data-update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Do a fresh pull in case any changes have been pushed to the branch.
-# git pull "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
+git pull "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
 
 now=$(date)
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add -A data
 git commit -m "Updating saved datasets at $now"
-git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
+GIT_CURL_VERBOSE=1 GIT_TRACE=1 git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:${GITHUB_REF}
