07b40c2364961a82aaa33083c0f228677209d128
Sean McClure <sean.mcclure17@gmail.com>
2022-01-06 14:50:39 -0500

    update tests to reflect changes

--- tests/libs/datasets/vaccine_backfill_test.py
+++ tests/libs/datasets/vaccine_backfill_test.py
@@ -12,6 +12,7 @@ from libs.datasets import vaccine_backfills
 def test_derive_vaccine_pct():
     region_tx = Region.from_state("TX")
     region_sf = Region.from_fips("06075")
+    region_ma = Region.from_state("MA")
     # TX has metrics that will be transformed to a percent of the population. Include some other
     # data to make sure it is not dropped.
     tx_timeseries_in = {
@@ -23,18 +24,30 @@ def test_derive_vaccine_pct():
         CommonFields.VACCINATIONS_COMPLETED: [None, 1_000],
         CommonFields.CASES: TimeseriesLiteral([1, 2], provenance=["caseprov"]),
     }
+
+    # MA metrics will be transformed and capped at 95% after exceeding 95,000 doses
+    ma_timeseries_in = {
+        CommonFields.VACCINATIONS_INITIATED: TimeseriesLiteral(
+            [94_000, 96_000],
+            annotation=[test_helpers.make_tag(date="2020-04-01")],
+            provenance=["prov1"],
+        ),
+        CommonFields.CASES: TimeseriesLiteral([1, 2], provenance=["caseprov"]),
+    }
+
     # SF does not have any vaccination metrics that can be transformed to a percentage so for it
-    # the input and output are the same.
+    # the input and output are the same, except 97% is capped to 95%.
     sf_timeseries = {
-        CommonFields.VACCINATIONS_COMPLETED_PCT: [0.1, 1],
+        CommonFields.VACCINATIONS_COMPLETED_PCT: [10, 97],
     }
     static_data_map = {
         region_tx: {CommonFields.POPULATION: 100_000},
         region_sf: {CommonFields.POPULATION: 10_000},
+        region_ma: {CommonFields.POPULATION: 100_000},
     }
 
     ds_in = test_helpers.build_dataset(
-        {region_tx: tx_timeseries_in, region_sf: sf_timeseries},
+        {region_tx: tx_timeseries_in, region_sf: sf_timeseries, region_ma: ma_timeseries_in},
         static_by_region_then_field_name=static_data_map,
     )
 
@@ -47,11 +60,11 @@ def test_derive_vaccine_pct():
                 CommonFields.VACCINATIONS_INITIATED_PCT: [1, 2],
                 CommonFields.VACCINATIONS_COMPLETED_PCT: [None, 1],
             },
-            region_sf: sf_timeseries,
+            region_ma: {**ma_timeseries_in, CommonFields.VACCINATIONS_INITIATED_PCT: [94, 95],},
+            region_sf: {CommonFields.VACCINATIONS_COMPLETED_PCT: [10, 95],},
         },
         static_by_region_then_field_name=static_data_map,
     )
-
     test_helpers.assert_dataset_like(ds_out, ds_expected)
 
 
