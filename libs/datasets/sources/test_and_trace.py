6a16359cdb13d9a1404b2dc7259f4c9f8bb3778f
Sean McClure <sean.mcclure17@gmail.com>
2022-01-26 16:05:33 -0500

    add date of note

--- libs/datasets/sources/test_and_trace.py
+++ libs/datasets/sources/test_and_trace.py
@@ -5,8 +5,8 @@ from libs.datasets import data_source
 class TestAndTraceData(data_source.DataSource):
     SOURCE_TYPE = "TestAndTrace"
 
-    # This data is no longer updating. We keep it in the data/
-    # directory so that this dataset builds successfully, but
+    # 1/26/2022: This data is no longer updating. We keep it in the
+    # data/ directory so that this dataset builds successfully, but
     # nothing is included in the API because the data is (very) stale.
     COMMON_DF_CSV_PATH = "misc/stale_test_and_trace_data.csv"
 
