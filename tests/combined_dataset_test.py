9a1a2e17f9611a4375972dd5f171719a1ed4ef1c
Sean McClure <sean.mcclure17@gmail.com>
2022-04-12 15:12:28 -0400

    Cleanup and fix DC test

--- tests/combined_dataset_test.py
+++ tests/combined_dataset_test.py
@@ -38,14 +38,14 @@ def test_combined_county_has_some_data(fips):
     region_data = combined_datasets.load_us_timeseries_dataset(
         load_demographics=False
     ).get_one_region(region)
-    # assert region_data.data[CommonFields.POSITIVE_TESTS].all()
-    # assert region_data.data[CommonFields.NEGATIVE_TESTS].all()
+    assert region_data.data[CommonFields.POSITIVE_TESTS].all()
+    assert region_data.data[CommonFields.NEGATIVE_TESTS].all()
     assert region_data.latest[CommonFields.DEATHS] > 1
 
 
 # Check some counties picked arbitrarily: (Orange County, CA)/06059 and (Harris County, TX)/48201
 @pytest.mark.slow
-@pytest.mark.parametrize("fips", ("06059", "48201"))
+@pytest.mark.parametrize("fips", ["06059", "48201"])
 def test_combined_county_has_some_timeseries_data(fips):
     region = Region.from_fips(fips)
     latest = combined_datasets.load_us_timeseries_dataset(load_demographics=False).get_one_region(
--- tests/pyseir_end_to_end_test.py
+++ tests/pyseir_end_to_end_test.py
@@ -36,20 +36,20 @@ def test_pyseir_end_to_end_california(tmp_path):
 
 @pytest.mark.filterwarnings("error", "ignore::RuntimeWarning")
 @pytest.mark.slow
-@pytest.mark.skip(reason="Github action runner runs OOM when loading full dataset")
 def test_pyseir_end_to_end_dc(tmp_path):
     # Runs over a single state which tests state filtering + running over more than
     # a single fips.
     with unittest.mock.patch("pyseir.utils.OUTPUT_DIR", str(tmp_path)):
-        regions_dataset = combined_datasets.load_us_timeseries_dataset(load_demographics=False)
+        regions_dataset = combined_datasets.load_us_timeseries_dataset(
+            load_demographics=False
+        ).get_subset(state="DC")
         regions = [one_region for _, one_region in regions_dataset.iter_one_regions()]
         region_pipelines: List[OneRegionPipeline] = list(
             parallel_utils.parallel_map(OneRegionPipeline.run, regions)
         )
         # Checking to make sure that build all for states properly filters and only
         # returns DC data
-        print(region_pipelines)
-        assert len(region_pipelines) == 4
+        assert len(region_pipelines) == 2
 
         model_output = pyseir.run.PyseirOutputDatasets.from_pipeline_output(region_pipelines)
         # TODO(tom): Work out why these have only one region where there are two regions in the
